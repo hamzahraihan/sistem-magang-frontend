5256bfde6ab5dce19996ccc522bd12a3fc97331c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-02-27 21:17:37 +0700

    Add LogoutButton component

---
+++ src/components/SidebarL/LogoutButton.jsx
@@ -0,0 +1,9 @@
+const LogoutButton = () => {
+  return (
+    <div className="text-center gap-3 p-4 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-300 hover:bg-primaryColor cursor-pointer mt-auto lg:w-full w-fit">
+      <p className="font-bold">Log Out</p>
+    </div>
+  );
+};
+
+export default LogoutButton;
