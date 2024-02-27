a5c22a3130a11feb4827e2eb753f5cfb37808b6e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-02-27 21:17:57 +0700

    Add search functionality and category selection to SidebarR component

--- src/components/SidebarR/index.jsx
+++ src/components/SidebarR/index.jsx
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+import Searchbar from './Searchbar';
+import PrimaryButton from '../PrimaryButton';
+import CatagoriesSelect from './CatagoriesSelect';
+
 const SidebarR = () => {
-  return <div className="border border-red-700 ">SidebarR</div>;
+  const [search, setSearch] = useState({
+    input: '',
+    category_name: '',
+  });
+  console.log('🚀 ~ SidebarR ~ search:', search);
+
+  return (
+    <div className="sticky lg:flex md:flex sm:flex flex-col gap-10 top-5 lg:items-start h-[90vh] hidden">
+      <form className="flex flex-col gap-10 w-full">
+        <Searchbar search={search} setSearch={setSearch} />
+        <CatagoriesSelect search={search} setSearch={setSearch} />
+        <PrimaryButton cari={'Cari'} />
+      </form>
+    </div>
+  );
 };
 
 export default SidebarR;
