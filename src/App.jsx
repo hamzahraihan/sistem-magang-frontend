2fc3389a9218f084fea052b991f790d61af69680
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-02-27 21:17:05 +0700

    Update grid layout and add PrimeReactProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import SidebarL from './components/SidebarL/index.jsx';
 import SidebarR from './components/SidebarR/index.jsx';
+import { PrimeReactProvider } from 'primereact/api';
 
 function App() {
   return (
-    <div className="grid grid-cols-3">
-      <SidebarL />
-      <Outlet />
-      <SidebarR />
+    <div className="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-12 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative">
+      <PrimeReactProvider>
+        <SidebarL />
+        <Outlet />
+        <SidebarR />
+      </PrimeReactProvider>
     </div>
   );
 }
