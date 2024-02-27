592301f4800fd8eafa5f5a71413f28dfcb77e04f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-02-27 21:17:32 +0700

    Refactor SidebarL component to include profile, navigation, and logout buttons

--- src/components/SidebarL/index.jsx
+++ src/components/SidebarL/index.jsx
@@ -1,5 +1,15 @@
+import LogoutButton from './LogoutButton';
+import NavigationButton from './NavigationButton';
+import ProfileButton from './ProfileButton';
+
 const SidebarL = () => {
-  return <div className="border border-red-700">SidebarL</div>;
+  return (
+    <div className="sticky lg:flex md:flex sm:flex flex-col gap-10 top-5 lg:items-start items-center h-[90vh] hidden">
+      <ProfileButton />
+      <NavigationButton />
+      <LogoutButton />
+    </div>
+  );
 };
 
 export default SidebarL;
