4a73dd8afda5621deab60869d8ff0666d7bd1413
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-02 21:32:25 +0700

    Update ProfileButton styling

--- src/components/SidebarL/ProfileButton.jsx
+++ src/components/SidebarL/ProfileButton.jsx
@@ -2,7 +2,7 @@ import { ArrowIcon } from '../Icons';
 
 const ProfileButton = () => {
   return (
-    <div className="flex flex-row gap-5 p-5 items-center lg:border rounded-[32px] border-neutral-200 border-0 cursor-pointer lg:w-full w-fit">
+    <div className="flex flex-row gap-5 p-4 items-center lg:border rounded-[32px] border-neutral-200 border-0 cursor-pointer lg:w-full w-fit">
       <div className="rounded-full bg-slate-400 animate-pulse  h-10 w-10"></div>
       <div className="flex-col lg:flex md:hidden sm:hidden hidden">
         <p className="font-bold text-md">Hamzah Raihan</p>
