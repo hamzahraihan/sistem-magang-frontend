ce91929e53f3e90786f8f8c2d921dcd48721b445
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-02 21:32:14 +0700

    Update SidebarL component styles

--- src/components/SidebarL/index.jsx
+++ src/components/SidebarL/index.jsx
@@ -4,7 +4,7 @@ import ProfileButton from './ProfileButton';
 
 const SidebarL = () => {
   return (
-    <div className="sticky lg:flex md:flex sm:flex flex-col gap-10 top-5 lg:items-start items-center h-[90vh] hidden">
+    <div className="sticky lg:flex md:flex sm:flex flex-col gap-4 top-5 lg:items-start items-center h-[90vh] hidden">
       <ProfileButton />
       <NavigationButton />
       <LogoutButton />
