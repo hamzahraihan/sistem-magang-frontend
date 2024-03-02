eb6526d9e9be812bde77a6016d09755d00e708a1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-02 21:33:03 +0700

    Update font size for "Informasi" heading

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import CardPost from './CardPost';
 const Home = () => {
   return (
     <div className="flex flex-col gap-10 lg:col-span-2 md:col-span-2 col-span-2 ">
-      <p className="font-bold  lg:text-5xl text-3xl">Informasi</p>
+      <p className="font-bold lg:text-2xl text-2xl">Informasi</p>
       <CardPost />
       <CardPost />
       <CardPost />
