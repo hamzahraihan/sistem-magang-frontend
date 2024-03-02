cfa1cb642f0887186539a0beee05d1adad465141
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-02 21:33:08 +0700

    Add FormLogin component for login functionality

---
+++ src/pages/Login/FormLogin.jsx
@@ -0,0 +1,40 @@
+import { useParams } from 'react-router-dom';
+import { EmailIcon, PasswordIcon } from '../../components/Icons';
+import PrimaryButton from '../../components/PrimaryButton';
+import { useRoleContext } from '../../hooks/useRoleContext';
+
+const FormLogin = () => {
+  const { role } = useRoleContext();
+  const { roleUrl } = useParams();
+
+  return (
+    <div className="flex flex-col h-full gap-4 items-center">
+      <div className="flex flex-col w-60 text-center gap-2">
+        <p className="font-bold">Masuk dengan akun {role.roleChoice ? role.roleChoice : roleUrl}</p>
+        <p className="text-xs text-gray-400">Silahkan isi data dirimu untuk membuat akun magang mandiri</p>
+      </div>
+
+      <form className="flex flex-col gap-3 w-full h-full">
+        <div className="flex flex-col relative">
+          <label htmlFor="email">Email</label>
+          <div className="absolute inset-y-11 start-0 flex items-center ps-3 pointer-events-none">
+            <EmailIcon />
+          </div>
+          <input type="email" name="email" id="email" className="w-full ps-10 border border-gray-200 rounded-xl p-3 text-xs" placeholder="Email..." required />
+        </div>
+        <div className="flex flex-col relative">
+          <label htmlFor="password">Password</label>
+          <div className="absolute inset-y-11 start-0 flex items-center ps-3 pointer-events-none">
+            <PasswordIcon />
+          </div>
+          <input type="password" name="password" id="password" className="w-full ps-10 border border-gray-200 rounded-xl p-3 text-xs" placeholder="Password..." required />
+        </div>
+        <div className="mt-auto">
+          <PrimaryButton text={'Login'} />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FormLogin;
