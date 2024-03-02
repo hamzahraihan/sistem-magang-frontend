48e8170d4b7b1b21c639d0f8537483c2452a5012
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-02 21:31:57 +0700

    Add console log and update button styling

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types';
+import { PlusIcon } from './Icons';
 
 const PrimaryButton = (props) => {
+  console.log('🚀 ~ PrimaryButton ~ props:', props);
   return (
-    <div className="text-center gap-3 p-4 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-300 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
-      <p className="font-bold">{props.cari}</p>
+    <div className="text-center gap-3 p-3 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-300 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
+      <p className={props.text == 'Buat Post' ? 'flex gap-2 items-center justify-center' : 'text-xs'}>
+        {props.text == 'Buat Post' && <PlusIcon />}
+        {props.text}
+      </p>
     </div>
   );
 };
 
 PrimaryButton.propTypes = {
-  cari: PropTypes.string,
+  text: PropTypes.string,
 };
 
 export default PrimaryButton;
