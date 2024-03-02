314cb0556b8da9b6aed88264955dcc087f88a3a2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-02 21:32:01 +0700

    Add SelectRole component

---
+++ src/components/SelectRole.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import { ConsoleIcon, LecturerIcon, UserIcon } from './Icons';
+import PropTypes from 'prop-types';
+import { useRoleContext } from '../hooks/useRoleContext';
+
+const SelectRole = (props) => {
+  console.log('🚀 ~ SelectRole ~ props:', props);
+
+  let roleIcon;
+  let content;
+  if (props.role == 'Mahasiswa') {
+    roleIcon = <UserIcon />;
+    content = `Untuk pengguna dengan status sebagai ${props.role} yang berkeinginan ikut program magang mandiri`;
+  } else if (props.role == 'Dosen') {
+    roleIcon = <LecturerIcon />;
+    content = `Untuk pengguna dengan status sebagai ${props.role} yang akan membimbing mahasiswa saat magang mandiri`;
+  } else {
+    roleIcon = <ConsoleIcon />;
+    content = `Untuk pengguna dengan status ${props.role}`;
+  }
+
+  const { handleRole } = useRoleContext();
+
+  return (
+    <Link to={`/login/${props.role}`} className="group flex flex-col gap-2 items-center border border-gray-300 rounded-[32px] hover:bg-primaryColor transition-all p-4 w-full cursor-pointer" onClick={() => handleRole(props.role)}>
+      <div className="rounded-[20px] p-4 bg-primaryColor">{roleIcon}</div>
+      <div className="flex flex-col gap-2 w-72 text-center group-hover:text-gray-600">
+        <p className="font-bold">{props.role}</p>
+        <p className="text-xs text-gray-400 group-hover:text-black">{content}</p>
+      </div>
+    </Link>
+  );
+};
+
+SelectRole.propTypes = {
+  role: PropTypes.string,
+};
+
+export default SelectRole;
