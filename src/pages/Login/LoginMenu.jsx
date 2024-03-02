bf8acbd94fe82f4f135c2b2425aad648f8a2da83
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-02 21:33:21 +0700

    Add LoginMenu component

---
+++ src/pages/Login/LoginMenu.jsx
@@ -0,0 +1,15 @@
+import InternshipLp from '../../assets/svg/Internship-bro.svg';
+
+const LoginMenu = () => {
+  return (
+    <div className="lg:flex flex-col gap-10 justify-center items-center border border-gray-300 h-full p-10 rounded-[64px] hidden">
+      <img className="w-80" src={InternshipLp} alt="" />
+      <div className="flex flex-col gap-2 text-center w-72 text-xs">
+        <p className="font-bold text-base">Magang Mandiri</p>
+        <p className="text-gray-400">Mulailah Berkontribusi pada Beragam Proyek yang Sesuai dengan Minatmu dan Berikan Dampak Nyata melalui program Magang Mandiri</p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginMenu;
