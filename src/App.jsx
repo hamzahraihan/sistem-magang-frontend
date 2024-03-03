515276d4271329706bc0c28310e086f959a13cb3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-03 14:34:41 +0700

    Remove SidebarR component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import SidebarL from './components/SidebarL/index.jsx';
-import SidebarR from './components/SidebarR/index.jsx';
 import { PrimeReactProvider } from 'primereact/api';
 import UserProvider from './context/UserContext.jsx';
 
 function App() {
   return (
-    <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-12 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative">
+    <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-5 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative">
       <PrimeReactProvider>
         <UserProvider>
           <SidebarL />
           <Outlet />
-          <SidebarR />
         </UserProvider>
       </PrimeReactProvider>
     </div>
