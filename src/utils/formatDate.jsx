93940ddf9633bfe4d23ed98152b43a8c8e6d3e77
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-03 14:36:28 +0700

    Add formatDate utility function

---
+++ src/utils/formatDate.jsx
@@ -0,0 +1,12 @@
+export const formatDate = (date) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  };
+
+  return new Date(date).toLocaleDateString('id-ID', options);
+};
