766dcb379fcb140cb63f2093c9c2f75901032051
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-03 14:35:40 +0700

    Add Searchbar component to SidebarHome

---
+++ src/pages/Home/SidebarHome/Searchbar.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { SearchIcon } from '../../../components/Icons';
+
+const Searchbar = ({ search, setSearch }) => {
+  return (
+    <div className="w-full relative">
+      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <SearchIcon />
+      </div>
+      <input
+        type="search"
+        name="input"
+        id="default-search"
+        className="w-full p-4 ps-10 text-gray-900 border border-gray-300 rounded-[20px]"
+        placeholder="Cari..."
+        onChange={(e) => setSearch({ ...search, [e.target.name]: e.target.value })}
+        required
+      />
+    </div>
+  );
+};
+
+Searchbar.propTypes = {
+  search: PropTypes.object,
+  setSearch: PropTypes.func,
+};
+
+export default Searchbar;
