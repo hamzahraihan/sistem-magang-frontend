1fd1c6082d53ebf2a1c0cfc1d023f91a8e3c6d1c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-03 14:35:26 +0700

    Refactor Home component layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,18 @@
 import CardPost from './CardPost';
+import SidebarHome from './SidebarHome';
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-10 lg:col-span-2 md:col-span-2 col-span-2 ">
-      <p className="font-bold lg:text-2xl text-2xl">Informasi</p>
-      <CardPost />
-      <CardPost />
-      <CardPost />
+    <div className="col-span-3">
+      <div className="grid grid-cols-3 gap-5">
+        <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
+          <p className="font-bold lg:text-2xl text-2xl">Informasi</p>
+          <CardPost />
+          <CardPost />
+          <CardPost />
+        </div>
+        <SidebarHome />
+      </div>
     </div>
   );
 };
