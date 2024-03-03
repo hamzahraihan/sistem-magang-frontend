7eb0862f0b2c8f87bd5db3b92093b3baa103ee36
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-03 14:35:00 +0700

    Update LogoutButton styling

--- src/components/SidebarL/LogoutButton.jsx
+++ src/components/SidebarL/LogoutButton.jsx
@@ -1,6 +1,6 @@
 const LogoutButton = () => {
   return (
-    <div className="text-center gap-3 p-4 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-300 hover:bg-primaryColor cursor-pointer mt-auto lg:w-full w-fit">
+    <div className="text-center gap-3 p-4 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-150 hover:bg-primaryColor cursor-pointer mt-auto lg:w-full w-fit">
       <p className="font-bold">Log Out</p>
     </div>
   );
