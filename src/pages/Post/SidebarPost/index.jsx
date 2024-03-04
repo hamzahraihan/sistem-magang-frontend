212dd1d8152edc1c9fdb10dd60dfc432bf8d5b1e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-04 23:54:24 +0700

    Refactor SidebarPost component

--- src/pages/Post/SidebarPost/index.jsx
+++ src/pages/Post/SidebarPost/index.jsx
@@ -2,7 +2,7 @@ import SidebarCard from './SidebarCard';
 
 const SidebarPost = () => {
   return (
-    <div className="lg:flex md:flex sm:flex flex-col top-5 lg:items-start h-[90vh] hidden">
+    <div className="lg:flex flex-col top-5 lg:items-start h-[90vh] hidden">
       <div className="flex flex-col items-center w-full gap-5">
         <h1 className="text-xl font-bold text-start w-full">Tentang Author</h1>
         <div className="h-28 w-28 bg-slate-500 animate-pulse rounded-full"></div>
