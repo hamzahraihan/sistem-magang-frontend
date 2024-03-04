3378893204b8c5c555ca5196f7f0781bc626a9fb
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-04 23:54:37 +0700

    Remove hour and minute formatting options from formatDate function

--- src/utils/formatDate.jsx
+++ src/utils/formatDate.jsx
@@ -3,9 +3,6 @@ export const formatDate = (date) => {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: false,
   };
 
   return new Date(date).toLocaleDateString('id-ID', options);
