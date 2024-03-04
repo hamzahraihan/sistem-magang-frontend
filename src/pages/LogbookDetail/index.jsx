58e032f8fbd732c611fc2fdb7935b606abe4f7b1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-05 01:24:20 +0700

    Update LogbookDetail component with state prop

--- src/pages/LogbookDetail/index.jsx
+++ src/pages/LogbookDetail/index.jsx
@@ -8,13 +8,13 @@ const LogbookDetail = () => {
   console.log('🚀 ~ LogbookDetail ~ state:', state);
 
   return (
-    <div className="col-span-3">
+    <div className="col-span-3 pb-10">
       <div className="grid grid-cols-3 gap-5">
         <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
           <Link to="/logbook" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
             <ArrowIcon />
           </Link>
-          <LogbookWeek />
+          <LogbookWeek state={state} />
         </div>
         <SidebarLogbookDetail />
       </div>
