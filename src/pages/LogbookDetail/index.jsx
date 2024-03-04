49b205c38f5076788224088ca7992490f840b294
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-04 23:54:05 +0700

    Add LogbookDetail component

---
+++ src/pages/LogbookDetail/index.jsx
@@ -0,0 +1,25 @@
+import { Link, useLocation } from 'react-router-dom';
+import { ArrowIcon } from '../../components/Icons';
+import SidebarLogbookDetail from './SidebarLogbookDetail';
+import LogbookWeek from './LogbookWeek';
+
+const LogbookDetail = () => {
+  const { state } = useLocation();
+  console.log('🚀 ~ LogbookDetail ~ state:', state);
+
+  return (
+    <div className="col-span-3">
+      <div className="grid grid-cols-3 gap-5">
+        <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
+          <Link to="/logbook" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+            <ArrowIcon />
+          </Link>
+          <LogbookWeek />
+        </div>
+        <SidebarLogbookDetail />
+      </div>
+    </div>
+  );
+};
+
+export default LogbookDetail;
