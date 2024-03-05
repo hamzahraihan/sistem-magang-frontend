a260ef0098e801a840afbcc881f313e3b27fcbd6
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-05 21:18:41 +0700

    Fix input border in CommentBar component

--- src/pages/Post/CommentBar.jsx
+++ src/pages/Post/CommentBar.jsx
@@ -6,7 +6,7 @@ const CommentBar = () => {
       <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
         <div className="h-10 w-10 bg-slate-500 animate-pulse rounded-full"></div>
       </div>
-      <input type="text" name="post" id="post" className="w-full ps-16 focus:outline-none rounded-xl p-5 me-3 text-xs ring-0" placeholder="Buat komentar..." required />
+      <input type="text" name="post" id="post" className="w-full ps-16 border-0 focus:ring-0 focus:outline-none rounded-xl p-5 me-3 text-xs ring-0" placeholder="Buat komentar..." required />
       <button className="flex items-center justify-center bg-primaryColor rounded-md w-20 hover:bg-hoverColor active:bg-activeColor transition-all">
         <ArrowIcon />
       </button>
