25a3af0961c3d65693acfbd10a066e1745932ea1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-05 21:17:36 +0700

    Update PrimaryButton styling

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -4,7 +4,7 @@ import { PlusIcon } from './Icons';
 const PrimaryButton = (props) => {
   console.log('🚀 ~ PrimaryButton ~ props:', props);
   return (
-    <div className="text-center gap-3 p-3 items-center border rounded-3xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
+    <div className="text-center gap-3 p-3 items-center border rounded-xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
       <p className={props.text == 'Buat Post' ? 'flex gap-2 items-center justify-center' : 'text-xs'}>
         {props.text == 'Buat Post' && <PlusIcon />}
         {props.text}
