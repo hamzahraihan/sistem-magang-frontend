0e5a7ee2b57a7be2ed73da7c26804a5789f9b3fd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-05 21:18:04 +0700

    Update Logbook Sidebar content

--- src/pages/Logbook/SidebarLogbook/index.jsx
+++ src/pages/Logbook/SidebarLogbook/index.jsx
@@ -2,13 +2,10 @@ const SidebarLogbook = () => {
   return (
     <div className="lg:flex flex-col top-5 lg:items-start h-[90vh] hidden">
       <div className="flex flex-col items-center w-full gap-5">
-        <h1 className="text-xl font-bold text-start w-full">Tentang Author</h1>
-        <div className="h-28 w-28 bg-slate-500 animate-pulse rounded-full"></div>
-        <div className="text-center">
-          <p className="text-base font-bold">Hamzah</p>
-          <p className="text-xs text-neutral-500 pb-2">Hamzah@gmail.com</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto perferendis </p>
-        </div>
+        <h1 className="text-xl font-bold text-start w-full">Logbook</h1>
+        <p className="text-neutral-500">
+          Mahasiswa wajib mengisi Logbook/Laporan Kegiatan di bawah pengawasan dosen pembimbing selama kegiatan berlangsung. Hasil evaluasi dan catatan akhir dari Mentor diperlukan untuk mendapatkan nilai magang mandiri
+        </p>
       </div>
     </div>
   );
