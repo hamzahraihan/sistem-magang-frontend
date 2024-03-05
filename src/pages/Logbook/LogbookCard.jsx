b15712d34e50466dae3e272218bd5da0c9d373dc
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-05 21:17:59 +0700

    Add console.log statement and fix dayName indexing

--- src/pages/Logbook/LogbookCard.jsx
+++ src/pages/Logbook/LogbookCard.jsx
@@ -7,6 +7,7 @@ import { formatDate } from '../../utils/formatDate';
 
 const LogbookCard = (props) => {
   const { index, weekLog } = props;
+  console.log('🚀 ~ LogbookCard ~ weekLog:', weekLog);
 
   return (
     <Link to={`/logbook/${formatDate(weekLog[0])}`} state={weekLog} className="border border-neutral-200 rounded-[32px] lg:p-9 md:p-4 sm:p-4 p-4">
@@ -14,7 +15,7 @@ const LogbookCard = (props) => {
         <div>
           <div className="flex items-center xm:text-base text-md font-bold gap-1">
             <p>
-              {weekLog[0].getDate()} {monthName[weekLog[0].getMonth()].slice(0, 3)} {weekLog[0].getFullYear()}
+              {weekLog[0].getDate()} {monthName[weekLog[0].getMonth()].slice(0, 3)}
             </p>
             <p>
               {weekLog[4] && '-'} {weekLog[4]?.getDate()} {weekLog[4] && monthName[weekLog[4].getMonth()].slice(0, 3)} {weekLog[4]?.getFullYear()}
@@ -25,9 +26,10 @@ const LogbookCard = (props) => {
         </div>
 
         <div className="lg:flex md:flex sm:flex gap-3 min-[490px]:flex hidden">
-          {dayName.slice(1, 6).map((item, index) => (
+          {weekLog.slice(0, 5).map((item, index) => (
             <div key={index} className="flex justify-center items-center h-10 w-10 border border-neutral-200 rounded-full">
-              <p>{dayName[weekLog[index].getDay()][0]}</p>
+              {console.log(item)}
+              <p>{dayName[item?.getDay()][0]}</p>
             </div>
           ))}
         </div>
