7cb4eba0464382a76bde1a59ef57336d842fe8ce
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-05 21:18:50 +0700

    Add UploadIntern page

---
+++ src/pages/UploadIntern/index.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import { ArrowIcon } from '../../components/Icons';
+import SidebarReport from './SidebarReport';
+import FormUploadReport from './FormUploadReport';
+
+const UploadIntern = () => {
+  return (
+    <div className="col-span-3 pb-10">
+      <div className="grid grid-cols-3 gap-5 ">
+        <div className="lg:order-first flex flex-col gap-4 lg:col-span-2 col-span-3 order-last">
+          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+            <ArrowIcon />
+          </Link>
+          <h1 className="text-xl font-bold">Upload Berkas</h1>
+          <FormUploadReport />
+        </div>
+        <SidebarReport />
+      </div>
+    </div>
+  );
+};
+
+export default UploadIntern;
