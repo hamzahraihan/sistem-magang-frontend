3fd1f39659cc092b51264417055c247c33f16733
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-06 15:44:34 +0700

    Refactor import statement and add padding bottom to Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,11 @@
-import CardPost from './CardPost';
+import CardPost from '../../components/CardPost';
 import SidebarHome from './SidebarHome';
 
 const Home = () => {
   return (
     <div className="col-span-3">
       <div className="grid grid-cols-3 gap-5">
-        <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
+        <div className="flex flex-col gap-4 lg:col-span-2 col-span-3 pb-10">
           <p className="font-bold lg:text-2xl text-2xl">Informasi</p>
           <CardPost />
           <CardPost />
