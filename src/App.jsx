121fb2c4468328cefb3241d950e8b70ace4b0d92
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-06 15:43:52 +0700

    Update grid gap in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import UserProvider from './context/UserContext.jsx';
 
 function App() {
   return (
-    <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-5 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative">
+    <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-5 md:gap-2 sm:gap-2 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative">
       <PrimeReactProvider>
         <UserProvider>
           <SidebarL />
