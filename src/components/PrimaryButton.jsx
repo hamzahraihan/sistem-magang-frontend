d704db1e5bca5b4d63e295dd0c038281c6a2d40b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-06 15:44:13 +0700

    Refactor PrimaryButton component to use react-router-dom

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,15 +1,28 @@
 import PropTypes from 'prop-types';
 import { PlusIcon } from './Icons';
+import { Link } from 'react-router-dom';
 
 const PrimaryButton = (props) => {
-  console.log('🚀 ~ PrimaryButton ~ props:', props);
   return (
-    <div className="text-center gap-3 p-3 items-center border rounded-xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
-      <p className={props.text == 'Buat Post' ? 'flex gap-2 items-center justify-center' : 'text-xs'}>
-        {props.text == 'Buat Post' && <PlusIcon />}
-        {props.text}
-      </p>
-    </div>
+    <>
+      {props.text == 'Buat Post' ? (
+        <Link to="/create-post">
+          <div className="text-center gap-3 p-3 items-center border rounded-xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
+            <p className={props.text == 'Buat Post' ? 'flex gap-2 items-center justify-center text-xs' : 'text-xs'}>
+              {props.text == 'Buat Post' && <PlusIcon />}
+              {props.text}
+            </p>
+          </div>
+        </Link>
+      ) : (
+        <button className="text-center gap-3 p-3 items-center border rounded-xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
+          <p className={props.text == 'Buat Post' ? 'flex gap-2 items-center justify-center text-xs' : 'text-xs'}>
+            {props.text == 'Buat Post' && <PlusIcon />}
+            {props.text}
+          </p>
+        </button>
+      )}
+    </>
   );
 };
 
