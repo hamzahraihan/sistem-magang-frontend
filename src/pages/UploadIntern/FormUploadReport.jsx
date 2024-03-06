eeb4f5fa170d4784b6dedd8a25b2a5b6e44aa2f3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-06 15:45:06 +0700

    Add ReactQuill for rich text editing

--- src/pages/UploadIntern/FormUploadReport.jsx
+++ src/pages/UploadIntern/FormUploadReport.jsx
@@ -1,13 +1,17 @@
 import PrimaryButton from '../../components/PrimaryButton';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
 
 const FormUploadReport = () => {
+  const [value, setValue] = useState('');
   return (
     <form className="flex flex-col gap-2 text-base">
       <label htmlFor="title">Judul</label>
       <input className="rounded-lg border border-gray-300" type="text" name="title" id="title" placeholder="Masukkan judul laporan akhir " />
 
       <label htmlFor="title">Keterangan laporan</label>
-      <input className="rounded-lg border border-gray-300" type="text" name="title" id="title" placeholder="Masukkan judul laporan akhir " />
+      <ReactQuill theme="snow" value={value} onChange={setValue} className="pb-10" />
 
       <label htmlFor="file_url">Upload File</label>
       <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer  focus:outline-none " id="file_url" type="file" />
