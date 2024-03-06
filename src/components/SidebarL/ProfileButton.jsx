79cb72244a3d90d3775b9d6c1a3fc8d26d917780
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-06 15:44:23 +0700

    Add Link component to ProfileButton.jsx

--- src/components/SidebarL/ProfileButton.jsx
+++ src/components/SidebarL/ProfileButton.jsx
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom';
 import { ArrowIcon } from '../Icons';
 
 const ProfileButton = () => {
   return (
-    <div className="flex flex-row gap-5 p-4 items-center lg:border rounded-[32px] border-neutral-200 border-0 cursor-pointer lg:w-full w-fit">
+    <Link to="/profile" className="flex flex-row gap-5 p-4 items-center lg:border rounded-[32px] border-neutral-200 border-0 cursor-pointer lg:w-full w-fit">
       <div className="rounded-full bg-slate-400 animate-pulse  h-10 w-10"></div>
       <div className="flex-col lg:flex md:hidden sm:hidden hidden">
         <p className="font-bold text-md">Hamzah Raihan</p>
@@ -11,7 +12,7 @@ const ProfileButton = () => {
       <div className="lg:flex hidden ms-auto">
         <ArrowIcon />
       </div>
-    </div>
+    </Link>
   );
 };
 
