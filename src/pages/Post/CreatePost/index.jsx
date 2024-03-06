022b02d31631da8c0c56bfe2d19503ca4b695133
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-06 15:44:48 +0700

    Add CreatePost component

---
+++ src/pages/Post/CreatePost/index.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import FormCreatePost from './FormCreatePost';
+import { ArrowIcon } from '../../../components/Icons';
+import SidebarCreatePost from './SidebarCreatePost';
+
+const CreatePost = () => {
+  return (
+    <div className="col-span-3 pb-10">
+      <form className="grid grid-cols-3 gap-5 ">
+        <div className="lg:order-first flex flex-col gap-4 lg:col-span-2 col-span-3 order-last">
+          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+            <ArrowIcon />
+          </Link>
+          <h1 className="text-xl font-bold">Upload Berkas</h1>
+          <FormCreatePost />
+        </div>
+        <SidebarCreatePost />
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
