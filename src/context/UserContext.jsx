25cbc082710e76875b687e13e06c1ba797f2bb68
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-10 16:14:32 +0700

    Refactor UserProvider component and add handleLogin function

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,16 +1,67 @@
-import { createContext, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { createContext, useEffect, useReducer, useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 export const UserContext = createContext(null);
 
+export const UserDispatch = createContext(null);
+
 export const UserProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
   const [role, setRole] = useState({ roleChoice: '' });
+  console.log('🚀 ~ UserProvider ~ role:', role.roleChoice.toLowerCase());
+  const [token, setToken] = useState('');
+  const [user, dispatch] = useReducer(UserReducer, []);
+
+  const { roleUrl } = useParams();
 
   const handleRole = (choosenRole) => {
     setRole({ ...role, roleChoice: choosenRole });
   };
 
-  return <UserContext.Provider value={{ role, handleRole }}>{children}</UserContext.Provider>;
+  useEffect(() => {
+    setRole({ ...role, roleChoice: roleUrl });
+  }, [roleUrl]);
+
+  useEffect(() => {}, []);
+
+  const handleLogin = async (email, password) => {
+    console.log('🚀 ~ handleLogin ~ email:', email);
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/account-verification`, {
+        email,
+        role: role.roleChoice.toLowerCase(),
+        password,
+      });
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        role,
+        handleRole,
+        handleLogin,
+      }}
+    >
+      <UserDispatch.Provider value={dispatch}>{children}</UserDispatch.Provider>
+    </UserContext.Provider>
+  );
+};
+
+const UserReducer = (user, action) => {
+  switch (action.type) {
+    case 'SET_USERDATA':
+      return action.userPayload;
+    case 'SET_TOKEN':
+      return action.token;
+  }
 };
 
 UserProvider.propTypes = {
