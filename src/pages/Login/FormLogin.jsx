be992c429de2e2799a8fd699afbf85b85db605de
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-10 16:14:44 +0700

    Add form submission functionality to FormLogin component

--- src/pages/Login/FormLogin.jsx
+++ src/pages/Login/FormLogin.jsx
@@ -2,10 +2,19 @@ import { useParams } from 'react-router-dom';
 import { EmailIcon, PasswordIcon } from '../../components/Icons';
 import PrimaryButton from '../../components/PrimaryButton';
 import { useRoleContext } from '../../hooks/useRoleContext';
+import { useState } from 'react';
 
 const FormLogin = () => {
-  const { role } = useRoleContext();
+  const [userInput, setUserInput] = useState({ email: '', password: '' });
+  console.log('🚀 ~ FormLogin ~ userInput:', userInput);
+  const { role, handleLogin } = useRoleContext();
+
   const { roleUrl } = useParams();
+
+  const handleSubmitLoginData = (e) => {
+    e.preventDefault();
+    handleLogin(userInput.email, userInput.password);
+  };
 
   return (
     <div className="flex flex-col h-full gap-4 items-center">
@@ -14,20 +23,38 @@ const FormLogin = () => {
         <p className="text-xs text-gray-400">Silahkan isi data dirimu untuk membuat akun magang mandiri</p>
       </div>
 
-      <form className="flex flex-col gap-3 w-full h-full">
+      <form className="flex flex-col gap-3 w-full h-full" onSubmit={handleSubmitLoginData}>
         <div className="flex flex-col relative">
           <label htmlFor="email">Email</label>
           <div className="absolute inset-y-11 start-0 flex items-center ps-3 pointer-events-none">
             <EmailIcon />
           </div>
-          <input type="email" name="email" id="email" className="w-full ps-10 border border-gray-200 rounded-xl p-3 text-xs" placeholder="Email..." required />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="w-full ps-10 border border-gray-200 rounded-xl p-3 text-xs"
+            placeholder="Email..."
+            value={userInput.email}
+            onChange={(e) => setUserInput({ ...userInput, [e.target.name]: e.target.value })}
+            required
+          />
         </div>
         <div className="flex flex-col relative">
           <label htmlFor="password">Password</label>
           <div className="absolute inset-y-11 start-0 flex items-center ps-3 pointer-events-none">
             <PasswordIcon />
           </div>
-          <input type="password" name="password" id="password" className="w-full ps-10 border border-gray-200 rounded-xl p-3 text-xs" placeholder="Password..." required />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="w-full ps-10 border border-gray-200 rounded-xl p-3 text-xs"
+            placeholder="Password..."
+            value={userInput.password}
+            onChange={(e) => setUserInput({ ...userInput, [e.target.name]: e.target.value })}
+            required
+          />
         </div>
         <div className="mt-auto">
           <PrimaryButton text={'Login'} />
