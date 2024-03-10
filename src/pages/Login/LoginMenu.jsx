277666f7e8047f59057feb81d98afc14f1fe2fb1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-10 23:42:30 +0700

    Add user authentication check in LoginMenu component

--- src/pages/Login/LoginMenu.jsx
+++ src/pages/Login/LoginMenu.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import InternshipLp from '../../assets/svg/Internship-bro.svg';
+import { useUserContext } from '../../hooks/useUserContext';
+import { useNavigate } from 'react-router-dom';
 
 const LoginMenu = () => {
+  const { user } = useUserContext();
+  console.log('🚀 ~ LoginMenu ~ user:', user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user && user.length === 0 && user == undefined) {
+      navigate('/');
+    }
+  }, [navigate, user]);
   return (
     <div className="lg:flex flex-col gap-10 justify-center items-center border border-gray-300 h-full p-10 rounded-[64px] hidden">
       <img className="w-80" src={InternshipLp} alt="" />
