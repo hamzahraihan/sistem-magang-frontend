eaf62e083db84cf4517eed71a86efc3c4674f852
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-11 20:11:06 +0700

    Add user information to Profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -2,8 +2,12 @@ import { Link } from 'react-router-dom';
 import { ArrowIcon } from '../../components/Icons';
 import SidebarProfile from './SidebarProfile';
 import CardPost from '../../components/CardPost';
+import { useUserContext } from '../../hooks/useUserContext';
+import ProfilePlaceholder from './Placeholder';
 
 const Profile = () => {
+  const { loading, userInformation } = useUserContext();
+  console.log('🚀 ~ Profile ~ userInformation:', userInformation);
   return (
     <div className="col-span-3 pb-10">
       <div className="grid grid-cols-3 gap-5 ">
@@ -11,12 +15,20 @@ const Profile = () => {
           <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
             <ArrowIcon />
           </Link>
-          <h1 className="text-xl font-bold">Profile</h1>
-          <div className="flex flex-col gap-2 justify-center items-center m-auto">
-            <div className="border border-neutral-400 h-52 w-52  bg-slate-500 animate-pulse rounded-full"></div>
-            <p className="font-bold text-base rounded-xl bg-activeColor py-2 px-4 w-fit">Hamzah Raihan</p>
-            <p className="text-neutral-400">hamzahraihan@gmail.com</p>
-          </div>
+          {loading ? (
+            <ProfilePlaceholder />
+          ) : (
+            <>
+              <h1 className="text-xl font-bold">Profile</h1>
+              <div className="flex flex-col gap-2 justify-center items-center m-auto">
+                <img className="border border-neutral-400 h-52 w-52  bg-slate-500 animate-pulse rounded-full" alt="profile-pict"></img>
+                <p className="font-bold text-base rounded-xl bg-activeColor py-2 px-4 w-fit">
+                  {userInformation.first_name} {userInformation.last_name}
+                </p>
+                <p className="text-neutral-400">{userInformation.email}</p>
+              </div>
+            </>
+          )}
           <p className="text-xl font-bold">Post Terbaru</p>
           <CardPost />
         </div>
