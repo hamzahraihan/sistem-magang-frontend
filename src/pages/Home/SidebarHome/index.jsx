8eb8b89ac083aeeffb0cdcf46992e4046236f542
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-12 14:50:57 +0700

    Refactor SidebarHome component

--- src/pages/Home/SidebarHome/index.jsx
+++ src/pages/Home/SidebarHome/index.jsx
@@ -1,21 +1,13 @@
-import { useState } from 'react';
 import Searchbar from './Searchbar';
 import PrimaryButton from '../../../components/PrimaryButton';
 import CatagoriesSelect from './CatagoriesSelect';
 
 const SidebarHome = () => {
-  const [search, setSearch] = useState({
-    input: '',
-    category_name: '',
-  });
-  console.log('🚀 ~ SidebarHome ~ search:', search);
-
   return (
     <div className="sticky lg:flex flex-col top-5 lg:items-start h-[90vh] hidden">
       <form className="flex flex-col gap-4 w-full">
-        <Searchbar search={search} setSearch={setSearch} />
-        <CatagoriesSelect search={search} setSearch={setSearch} />
-        <PrimaryButton text={'Cari'} />
+        <Searchbar />
+        <CatagoriesSelect />
         <PrimaryButton text={'Buat Post'} />
       </form>
     </div>
