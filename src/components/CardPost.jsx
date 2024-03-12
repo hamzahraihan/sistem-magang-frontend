f38d5728a4de73ed09724c2edd2dea35d9d8e08f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-12 14:50:17 +0700

    Add dynamic data and PropTypes to CardPost component

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -1,32 +1,42 @@
 import { Link } from 'react-router-dom';
 import { ClockIcon, TagIcon } from './Icons';
+import { formatDate } from '../utils/formatDate';
+import PropTypes from 'prop-types';
 
-const CardPost = () => {
+const CardPost = (props) => {
+  const { post } = props;
+
   return (
     <div className="border border-neutral-200 rounded-[32px]">
-      <Link to="/detail-post">
+      <Link to={`/detail-post/${post.post_id}`}>
         <div className="bg-slate-300 animate-pulse h-48 rounded-se-[32px] rounded-ss-[32px] "></div>
       </Link>
       <div className="flex flex-col gap-3 p-7">
         <div className="flex gap-2">
           <p className="flex gap-1 items-center text-neutral-700 text-sm">
             <ClockIcon />
-            19 November 2023
+            {formatDate(post.updatedAt)}
           </p>
           <p className="flex gap-1 items-center text-neutral-700 text-sm">
             <TagIcon />
-            Magang
+            {post.category_name}
           </p>
         </div>
-        <p className="text-base font-bold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore atque</p>
-        <p className="line-clamp-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquid itaque obcaecati accusamus numquam. Perferendis recusandae voluptas quidem, optio omnis repudiandae et, hic sint perspiciatis aliquam repellendus! Nam
-          quisquam esse eveniet nesciunt explicabo aliquid voluptates sunt dicta alias ea? Possimus quam corporis aperiam necessitatibus suscipit commodi ipsum nemo delectus modi.
+        <p className="text-base font-bold">{post.title}</p>
+        <p className="line-clamp-3">{post.description}</p>
+
+        <p className="text-neutral-400 underline">
+          {post.Mahasiswa?.first_name}
+          {post.Dosen?.first_name}
+          {post.Admin?.first_name}
         </p>
-        <p className="text-neutral-400 underline">Hamzah</p>
       </div>
     </div>
   );
+};
+
+CardPost.propTypes = {
+  post: PropTypes.object,
 };
 
 export default CardPost;
