e933813b938165915f321f5d3bb9d6e54687a26c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-12 14:50:11 +0700

    Add PostProvider to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Outlet } from 'react-router-dom';
 import SidebarL from './components/SidebarL/index.jsx';
 import { PrimeReactProvider } from 'primereact/api';
 import UserProvider from './context/UserContext.jsx';
+import PostProvider from './context/PostContext.jsx';
 
 function App() {
   return (
     <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-5 md:gap-2 sm:gap-2 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative">
       <PrimeReactProvider>
         <UserProvider>
-          <SidebarL />
-          <Outlet />
+          <PostProvider>
+            <SidebarL />
+            <Outlet />
+          </PostProvider>
         </UserProvider>
       </PrimeReactProvider>
     </div>
