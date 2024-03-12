f8d5fdd3acbcd9e0070f3cb7b8ef1b848c20f67a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-12 14:50:53 +0700

    Refactor CatagoriesSelect component to use usePostContext hook

--- src/pages/Home/SidebarHome/CatagoriesSelect.jsx
+++ src/pages/Home/SidebarHome/CatagoriesSelect.jsx
@@ -1,6 +1,7 @@
-import PropTypes from 'prop-types';
+import { usePostContext } from '../../../hooks/usePostContext';
 
-const CatagoriesSelect = ({ search, setSearch }) => {
+const CatagoriesSelect = () => {
+  const { search, setSearch } = usePostContext();
   return (
     <select id="large" name="category_name" className="text-xs block w-full p-4 text-gray-900 border border-gray-300 rounded-[20px]" onChange={(e) => setSearch({ ...search, [e.target.name]: e.target.value })}>
       <option defaultValue={true}>Pilih Kategori</option>
@@ -10,11 +11,6 @@ const CatagoriesSelect = ({ search, setSearch }) => {
       <option value="bertanya">Bertanya</option>
     </select>
   );
-};
-
-CatagoriesSelect.propTypes = {
-  search: PropTypes.object,
-  setSearch: PropTypes.func,
 };
 
 export default CatagoriesSelect;
