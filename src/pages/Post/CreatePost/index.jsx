0851d5575d3e82ac5f5bc08bd1269dfa935e3f81
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-13 23:37:27 +0700

    Update handleImageUpload to handleCreatePost in CreatePost component

--- src/pages/Post/CreatePost/index.jsx
+++ src/pages/Post/CreatePost/index.jsx
@@ -5,16 +5,16 @@ import SidebarCreatePost from './SidebarCreatePost';
 import { usePostContext } from '../../../hooks/usePostContext';
 
 const CreatePost = () => {
-  const { handleImageUpload } = usePostContext();
+  const { handleCreatePost } = usePostContext();
 
   return (
     <div className="col-span-3 pb-10">
-      <form className="grid grid-cols-3 gap-5 " onSubmit={handleImageUpload}>
+      <form className="grid grid-cols-3 gap-5 " onSubmit={handleCreatePost}>
         <div className="lg:order-first flex flex-col gap-4 lg:col-span-2 col-span-3 order-last">
           <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
             <ArrowIcon />
           </Link>
-          <h1 className="text-xl font-bold">Upload Berkas</h1>
+          <h1 className="text-xl font-bold">Buat post baru</h1>
           <FormCreatePost />
         </div>
         <SidebarCreatePost />
