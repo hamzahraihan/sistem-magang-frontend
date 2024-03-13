12e9ec02905e6b90ae01ba3461c9939502ba14e7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-13 15:14:34 +0700

    Add image background to CardPost component

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -6,29 +6,33 @@ import PropTypes from 'prop-types';
 const CardPost = (props) => {
   const { post } = props;
 
+  const imageBackground = {
+    backgroundImage: 'url("https://drive.google.com/thumbnail?id=1mUa6TjOJqy-NQBsat4-cUU_38Tq0ZIPB")',
+  };
+
   return (
     <div className="border border-neutral-200 rounded-[32px]">
-      <Link to={`/detail-post/${post.post_id}`}>
-        <div className="bg-slate-300 animate-pulse h-48 rounded-se-[32px] rounded-ss-[32px] "></div>
+      <Link to={`/detail-post/${post?.post_id}`}>
+        <div className="h-48 rounded-se-[32px] rounded-ss-[32px] bg-cover bg-no-repeat bg-center" style={imageBackground}></div>
       </Link>
       <div className="flex flex-col gap-3 p-7">
         <div className="flex gap-2">
           <p className="flex gap-1 items-center text-neutral-700 text-sm">
             <ClockIcon />
-            {formatDate(post.updatedAt)}
+            {formatDate(post?.updatedAt)}
           </p>
           <p className="flex gap-1 items-center text-neutral-700 text-sm">
             <TagIcon />
-            {post.category_name}
+            {post?.category_name}
           </p>
         </div>
-        <p className="text-base font-bold">{post.title}</p>
-        <p className="line-clamp-3">{post.description}</p>
+        <p className="text-base font-bold">{post?.title}</p>
+        <p className="line-clamp-3">{post?.description}</p>
 
         <p className="text-neutral-400 underline">
-          {post.Mahasiswa?.first_name}
-          {post.Dosen?.first_name}
-          {post.Admin?.first_name}
+          {post?.Mahasiswa?.first_name}
+          {post?.Dosen?.first_name}
+          {post?.Admin?.first_name}
         </p>
       </div>
     </div>
