c2716ca79096ed7cc27b737b42bd699bc4145d3f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-13 23:37:21 +0700

    Refactor FormCreatePost component to use postInputData state

--- src/pages/Post/CreatePost/FormCreatePost.jsx
+++ src/pages/Post/CreatePost/FormCreatePost.jsx
@@ -1,23 +1,36 @@
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
-import { useState } from 'react';
 import { usePostContext } from '../../../hooks/usePostContext';
 
 const FormCreatePost = () => {
-  const [value, setValue] = useState('');
-
-  const { imageInputRef } = usePostContext();
+  const { postInputData, setPostInputData, imageInputRef } = usePostContext();
+  console.log('🚀 ~ FormCreatePost ~ postInputData:', postInputData);
 
   return (
     <div className="flex flex-col gap-2 text-base">
       <label htmlFor="title">Judul</label>
-      <input className="rounded-lg border border-gray-300" type="text" name="title" id="title" placeholder="Masukkan judul untuk postingan mu " required />
+      <input
+        className="rounded-lg border border-gray-300"
+        type="text"
+        name="title"
+        id="title"
+        placeholder="Masukkan judul untuk postingan mu "
+        required
+        value={postInputData.title}
+        onChange={(e) => setPostInputData({ ...postInputData, [e.target.name]: e.target.value })}
+      />
 
       <label htmlFor="desc">Deskripsi Post</label>
-      <ReactQuill theme="snow" value={value} onChange={setValue} className="pb-10" />
+      <ReactQuill theme="snow" id="description" value={postInputData.description} onChange={(content) => setPostInputData({ ...postInputData, description: content })} className="pb-10" />
 
       <label htmlFor="category_name">Kategori</label>
-      <select id="large" name="category_name" className="text-base block w-full p-4 text-gray-900 border border-gray-300 rounded-lg">
+      <select
+        id="large"
+        name="category_name"
+        className="text-base block w-full p-4 text-gray-900 border border-gray-300 rounded-lg "
+        value={postInputData.category_name}
+        onChange={(e) => setPostInputData({ ...postInputData, [e.target.name]: e.target.value })}
+      >
         <option defaultValue="magang">Pilih Kategori</option>
         <option value="magang">Magang</option>
         <option value="diskusi">Diskusi</option>
