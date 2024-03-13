afcfc0e9cc0126b06fc3f925d4fb8d6ae7ba5510
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-13 15:14:38 +0700

    Add CardPostPlaceholder component

---
+++ src/components/Placeholder/CardPostPlaceholder.jsx
@@ -0,0 +1,47 @@
+const CardPostPlaceholder = () => {
+  return (
+    <>
+      <div className="border border-neutral-200 rounded-[32px]">
+        <div className="bg-slate-300 animate-pulse h-48 rounded-se-[32px] rounded-ss-[32px] bg-cover bg-no-repeat bg-center"></div>
+        <div className="flex flex-col gap-3 p-7">
+          <div className="flex gap-2">
+            <p className="flex gap-1 bg-slate-400 h-2 w-20 rounded-md animate-pulse items-center text-neutral-700 text-sm"></p>
+            <p className="flex gap-1 bg-slate-400 h-2 w-20 rounded-md animate-pulse items-center text-neutral-700 text-sm"></p>
+          </div>
+          <p className="bg-slate-600 h-4 w-full rounded-md animate-pulse "></p>
+          <p className="bg-slate-600 h-4 w-full rounded-md animate-pulse "></p>
+
+          <p className="text-neutral-400 underline"></p>
+        </div>
+      </div>
+      <div className="border border-neutral-200 rounded-[32px]">
+        <div className="bg-slate-300 animate-pulse h-48 rounded-se-[32px] rounded-ss-[32px] bg-cover bg-no-repeat bg-center"></div>
+        <div className="flex flex-col gap-3 p-7">
+          <div className="flex gap-2">
+            <p className="flex gap-1 bg-slate-400 h-2 w-20 rounded-md animate-pulse items-center text-neutral-700 text-sm"></p>
+            <p className="flex gap-1 bg-slate-400 h-2 w-20 rounded-md animate-pulse items-center text-neutral-700 text-sm"></p>
+          </div>
+          <p className="bg-slate-600 h-4 w-full rounded-md animate-pulse "></p>
+          <p className="bg-slate-600 h-4 w-full rounded-md animate-pulse "></p>
+
+          <p className="text-neutral-400 underline"></p>
+        </div>
+      </div>
+      <div className="border border-neutral-200 rounded-[32px]">
+        <div className="bg-slate-300 animate-pulse h-48 rounded-se-[32px] rounded-ss-[32px] bg-cover bg-no-repeat bg-center"></div>
+        <div className="flex flex-col gap-3 p-7">
+          <div className="flex gap-2">
+            <p className="flex gap-1 bg-slate-400 h-2 w-20 rounded-md animate-pulse items-center text-neutral-700 text-sm"></p>
+            <p className="flex gap-1 bg-slate-400 h-2 w-20 rounded-md animate-pulse items-center text-neutral-700 text-sm"></p>
+          </div>
+          <p className="bg-slate-600 h-4 w-full rounded-md animate-pulse "></p>
+          <p className="bg-slate-600 h-4 w-full rounded-md animate-pulse "></p>
+
+          <p className="text-neutral-400 underline"></p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardPostPlaceholder;
