14de005d4be690736de390f78eaa1b3edb2d160c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-13 23:36:58 +0700

    Refactor getPost function to handle search and category filtering

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,24 +5,32 @@ import SidebarHome from './SidebarHome';
 
 const Home = () => {
   const { loadingPost, post, searchParams } = usePostContext();
+  console.log('🚀 ~ Home ~ post:', post);
 
   const searchInput = searchParams.get('search');
   const categoryInput = searchParams.get('category_name');
 
-  const getPost = post.filter((item) => {
-    const titleMatch = item.title.toLowerCase().includes(searchInput?.toLowerCase());
+  const getPost = (searchInput, categoryInput) => {
+    if (!searchInput && !categoryInput) {
+      return post;
+    }
 
-    const categoryMatch = item.category_name.toLowerCase().includes(categoryInput?.toLowerCase());
+    return post.filter((item) => {
+      const titleMatch = searchInput ? item.title.toLowerCase().includes(searchInput.toLowerCase()) : true;
+      const categoryMatch = categoryInput ? item.category_name.toLowerCase().includes(categoryInput.toLowerCase()) : true;
+      return titleMatch && categoryMatch;
+    });
+  };
 
-    return titleMatch && categoryMatch;
-  });
+  const filteredPost = getPost(searchInput, categoryInput);
+  console.log('🚀 ~ Home ~ filteredPost:', filteredPost);
 
   return (
     <div className="col-span-3">
       <div className="grid grid-cols-3 gap-5">
         <div className="flex flex-col gap-4 lg:col-span-2 col-span-3 pb-10">
           <p className="font-bold lg:text-2xl text-2xl">Informasi</p>
-          {loadingPost ? <CardPostPlaceholder /> : getPost.length === 0 ? <div>Tidak ditemukan</div> : getPost.map((item) => <CardPost key={item.post_id} post={item} />)}
+          {loadingPost ? <CardPostPlaceholder /> : filteredPost.length === 0 ? <div>Tidak ditemukan</div> : filteredPost.map((item) => <CardPost key={item.post_id} post={item} />)}
         </div>
         <SidebarHome />
       </div>
