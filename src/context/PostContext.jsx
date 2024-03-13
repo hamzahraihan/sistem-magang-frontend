13c4918764c1c3b8ba7a01a842d43598ba5049c5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-13 15:14:43 +0700

    Add useParams hook and handle loadingPost state

--- src/context/PostContext.jsx
+++ src/context/PostContext.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { createContext, useEffect, useReducer, useRef, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 export const PostContext = createContext(null);
 
@@ -11,14 +11,22 @@ export const PostProvider = ({ children }) => {
   const [search, setSearch] = useState({ search: '', category_name: '' });
   const [post, dispatch] = useReducer(PostReducer, []);
   const [loadingPost, setLoadingPost] = useState(false);
+  console.log('🚀 ~ PostProvider ~ loadingPost:', loadingPost);
   const [searchParams, setSearchParams] = useSearchParams({ search: '', category_name: '' });
 
   const imageInputRef = useRef(null);
+
+  const { id } = useParams();
 
   useEffect(() => {
     const getAllPost = async () => {
       setLoadingPost(true);
       try {
+        if (id) {
+          const { data } = await axios.get(`${import.meta.env.VITE_BASE_URL}/post/user/${id}`);
+          setLoadingPost(false);
+          return dispatch({ type: 'SET_POST_BY_USER', payload: data.result });
+        }
         const { data } = await axios.get(`${import.meta.env.VITE_BASE_URL}/post`);
         dispatch({ type: 'SET_POST_DATA', payload: data.result });
         setLoadingPost(false);
@@ -27,19 +35,15 @@ export const PostProvider = ({ children }) => {
       }
     };
     getAllPost();
-  }, []);
+  }, [id]);
 
   const handleImageUpload = async (e) => {
     e.preventDefault();
     const image = imageInputRef.current.files[0];
-    console.log('🚀 ~ handleImageUpload ~ images:', image);
 
     if (image) {
       const formData = new FormData();
-
       formData.append('image', image);
-      console.log('🚀 ~ handleImageUpload ~ formData:', formData.get('image'));
-
       try {
         const { data } = await axios.post(`${import.meta.env.VITE_BASE_URL}/post/create-post`, formData, {
           headers: {
@@ -63,6 +67,8 @@ export const PostProvider = ({ children }) => {
 const PostReducer = (post, action) => {
   switch (action.type) {
     case 'SET_POST_DATA':
+      return action.payload;
+    case 'SET_POST_BY_USER':
       return action.payload;
     case 'DELETE_POST_BY_ID':
       return action.payload;
