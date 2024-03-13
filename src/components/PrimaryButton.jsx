4c01fa64d34fb3182edf290d3ea107b3ab0d4db5
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-13 23:36:33 +0700

    Update button border radius

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -7,7 +7,7 @@ const PrimaryButton = (props) => {
     <>
       {props.text == 'Buat Post' ? (
         <Link to="/create-post">
-          <div className="text-center gap-3 p-3 items-center border rounded-xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
+          <div className="text-center gap-3 p-3 items-center border rounded-2xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full">
             <p className={props.text == 'Buat Post' ? 'flex gap-2 items-center justify-center text-xs' : 'text-xs'}>
               {props.text == 'Buat Post' && <PlusIcon />}
               {props.text}
@@ -17,7 +17,7 @@ const PrimaryButton = (props) => {
       ) : (
         <button
           type={props.type}
-          className="text-center gap-3 p-3 items-center border rounded-xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full disabled:bg-primaryColor/50 "
+          className="text-center gap-3 p-3 items-center border rounded-2xl border-neutral-200 transition-all duration-150 bg-primaryColor active:bg-activeColor cursor-pointer mt-auto w-full disabled:bg-primaryColor/50 "
           disabled={props.loading}
         >
           <p className={props.text == 'Buat Post' ? 'flex gap-2 items-center justify-center text-xs' : 'text-xs'}>
