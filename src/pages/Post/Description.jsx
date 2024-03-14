5e04c7a8835d356ca6167101ede63d7c3e0bc0c7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-15 00:21:10 +0700

    Refactor post description rendering

--- src/pages/Post/Description.jsx
+++ src/pages/Post/Description.jsx
@@ -3,17 +3,12 @@ import PostDate from './PostDate';
 
 const Description = () => {
   const { postById } = usePostContext();
-  console.log('🚀 ~ Description ~ post:', postById);
   return (
     <div className="flex flex-col gap-2 ">
       <PostDate />
       <div className="flex flex-col gap-2 pb-2">
-        {postById.map((item) => (
-          <>
-            <h1 className="text-xl font-bold leading-8">{item.title}</h1>
-            <p className="text-base" dangerouslySetInnerHTML={{ __html: item.description }} />
-          </>
-        ))}
+        <h1 className="text-xl font-bold leading-8">{postById[0]?.title}</h1>
+        <p className="text-base" dangerouslySetInnerHTML={{ __html: postById[0]?.description }} />
       </div>
       <div className="border-t border-slate-300 w-full"></div>
     </div>
