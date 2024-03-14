debc7f8a05a00c5f223fb0b492cd4686635472ab
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-15 00:21:14 +0700

    Add usePostContext hook and update image background

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -3,8 +3,13 @@ import { ArrowIcon } from '../../components/Icons';
 import Description from './Description';
 import CommentSection from './CommentSection';
 import SidebarPost from './SidebarPost';
+import { usePostContext } from '../../hooks/usePostContext';
 
 const DetailPost = () => {
+  const { loadingPost, postById } = usePostContext();
+  const imageBackground = {
+    backgroundImage: `url(https://drive.google.com/thumbnail?id=${postById[0]?.image}&sz=w1000)`,
+  };
   return (
     <div className="col-span-3">
       <div className="grid grid-cols-3 gap-5">
@@ -12,7 +17,16 @@ const DetailPost = () => {
           <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
             <ArrowIcon />
           </Link>
-          <div className="bg-slate-300 animate-pulse h-96 rounded-[32px] "></div>
+          {loadingPost ? (
+            <div className="bg-slate-400 animate-pulse h-96 rounded-[32px] bg-cover bg-center bg-no-repeat"></div>
+          ) : (
+            postById[0]?.image && (
+              <a href={`https://drive.google.com/uc?export=preview&id=${postById[0].image}`} target="_blank">
+                <div className="h-96 rounded-[32px] bg-cover bg-center bg-no-repeat" style={imageBackground}></div>
+              </a>
+            )
+          )}
+
           <Description />
           <CommentSection />
         </div>
