7a2b01845607862e6ca8a224134decc7c229c40e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-15 00:20:37 +0700

    Add user check for LogoutButton in SidebarL

--- src/components/SidebarL/index.jsx
+++ src/components/SidebarL/index.jsx
@@ -1,13 +1,15 @@
+import { useUserContext } from '../../hooks/useUserContext';
 import LogoutButton from './LogoutButton';
 import NavigationButton from './NavigationButton';
 import ProfileButton from './ProfileButton';
 
 const SidebarL = () => {
+  const { user } = useUserContext();
   return (
     <div className="sticky lg:flex md:flex sm:flex flex-col gap-4 top-5 lg:items-start items-center h-screen pb-10 hidden">
       <ProfileButton />
       <NavigationButton />
-      <LogoutButton />
+      {user.length > 0 && <LogoutButton />}
     </div>
   );
 };
