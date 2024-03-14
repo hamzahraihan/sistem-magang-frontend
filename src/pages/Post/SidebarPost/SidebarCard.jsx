61654942c05af06ba8bc10e0df45efbde86ab628
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-15 00:21:24 +0700

    Add post details and format date in SidebarCard component

--- src/pages/Post/SidebarPost/SidebarCard.jsx
+++ src/pages/Post/SidebarPost/SidebarCard.jsx
@@ -1,28 +1,42 @@
 import { Link } from 'react-router-dom';
 import { ClockIcon, TagIcon } from '../../../components/Icons';
+import { formatDate } from '../../../utils/formatDate';
+import PropTypes from 'prop-types';
 
-const SidebarCard = () => {
+const SidebarCard = (props) => {
+  const { post } = props;
+  const imageBackground = {
+    backgroundImage: 'url(https://drive.google.com/thumbnail?id=' + post.image + ')',
+  };
   return (
     <div className="border border-neutral-200 rounded-[32px]">
-      <Link to="/detail-post">
-        <div className="bg-slate-300 animate-pulse h-32 rounded-se-[32px] rounded-ss-[32px] "></div>
+      <Link to={`/detail-post/${post.post_id}`}>
+        <div className="bg-no-repeat bg-cover bg-center h-32 rounded-se-[32px] rounded-ss-[32px]" style={imageBackground}></div>
       </Link>
       <div className="flex flex-col gap-3 p-7">
         <div className="flex gap-2">
           <p className="flex gap-1 items-center text-neutral-700 text-xs">
             <ClockIcon />
-            19 November 2023
+            {formatDate(post.updatedAt)}
           </p>
           <p className="flex gap-1 items-center text-neutral-700 text-xs">
             <TagIcon />
-            Magang
+            {post.category_name}
           </p>
         </div>
-        <p className="text-sm font-bold line-clamp-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore atque</p>
-        <p className="text-neutral-400 underline">Hamzah</p>
+        <p className="text-sm font-bold line-clamp-2">{post.title}</p>
+        <p className="text-neutral-400 underline">
+          {post?.Mahasiswa?.first_name}
+          {post?.Dosen?.first_name}
+          {post?.Admin?.first_name}
+        </p>
       </div>
     </div>
   );
+};
+
+SidebarCard.propTypes = {
+  post: PropTypes.object,
 };
 
 export default SidebarCard;
