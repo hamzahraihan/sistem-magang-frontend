0ba765a63e62cce5048aa3ae4f3b1edeff75dc8a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-15 00:21:00 +0700

    Update Login page layout to use full height

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,7 +4,7 @@ import UserProvider from '../../context/UserContext';
 
 const index = () => {
   return (
-    <div className="flex justify-center items-center h-screen p-10">
+    <div className="flex justify-center items-center h-full p-10">
       <div className="grid lg:grid-cols-2 grid-cols-1 h-[600px] gap-10 w-[1000px]">
         <UserProvider>
           <LoginMenu />
