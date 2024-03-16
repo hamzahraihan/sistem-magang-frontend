6dc35cf71b6c5712c7362c487ecbc0e98f08ccdd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-16 21:41:04 +0700

    Add CreateInternship page

---
+++ src/pages/Internship/CreateInternship/index.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import { ArrowIcon } from '../../../components/Icons';
+import FormCreateInternship from './FormCreateInternship';
+import SidebarCreateIntenship from './SidebarCreateInternship';
+
+const CreateInternship = () => {
+  return (
+    <form className="grid grid-cols-3 gap-5">
+      <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
+        <div className="flex flex-col gap-2 text-base ">
+          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+            <ArrowIcon />
+          </Link>
+          <FormCreateInternship />
+        </div>
+      </div>
+      <SidebarCreateIntenship />
+    </form>
+  );
+};
+
+export default CreateInternship;
