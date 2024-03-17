5d547ac3fb0c4e586d62edfea0b2c03c82d1cac4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-17 21:24:21 +0700

    Refactor ProfileButton component to use userLoggedInData

--- src/components/SidebarL/ProfileButton.jsx
+++ src/components/SidebarL/ProfileButton.jsx
@@ -1,24 +1,22 @@
 import { Link } from 'react-router-dom';
 import { ArrowIcon } from '../Icons';
 import { useUserContext } from '../../hooks/useUserContext';
-import { useEffect, useState } from 'react';
 
 const ProfileButton = () => {
-  const [userData, setUserData] = useState({});
-  const { user } = useUserContext();
-
-  useEffect(() => {
-    setUserData(user[0]);
-  }, [user]);
+  const { userLoggedInData } = useUserContext();
+  console.log('🚀 ~ ProfileButton ~ userLoggedInData:', userLoggedInData);
 
   return (
-    <Link to={userData ? `/profile/${userData.role}/${userData.id}` : '/login'} className="flex flex-row lg:gap-5  p-4 items-center lg:border rounded-[32px] border-neutral-200 border-0 cursor-pointer lg:w-full w-fit">
+    <Link
+      to={userLoggedInData ? `/profile/${userLoggedInData?.role}/${userLoggedInData?.id}` : '/login'}
+      className="flex flex-row lg:gap-5  p-4 items-center lg:border rounded-[32px] border-neutral-200 border-0 cursor-pointer lg:w-full w-fit"
+    >
       <div className="rounded-full bg-slate-400 animate-pulse  h-10 w-10 flex-shrink-0"></div>
       <div className="flex-col lg:flex md:hidden sm:hidden hidden">
         <p className="font-bold text-md">
-          {userData ? userData.first_name : 'Belum login'} {userData ? userData.last_name : ''}
+          {userLoggedInData ? userLoggedInData?.first_name : 'Silahkan login'} {userLoggedInData ? userLoggedInData?.last_name : ''}
         </p>
-        <p className="text-neutral-400 text-[10px] min-[1000px]:line-clamp-1">{userData ? userData.email : ''}</p>
+        <p className="text-neutral-400 text-[10px] min-[1000px]:line-clamp-1">{userLoggedInData ? userLoggedInData?.email : ''}</p>
       </div>
       <div className="xl:flex lg:hidden hidden ms-auto">
         <ArrowIcon />
