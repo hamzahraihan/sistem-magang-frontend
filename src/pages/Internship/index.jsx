706100e11d32b33b993b60cfb4f9b2c4ba7ed529
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-17 21:24:51 +0700

    Update Internship component styling

--- src/pages/Internship/index.jsx
+++ src/pages/Internship/index.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const Internship = () => {
   return (
-    <div className="col-span-3">
+    <div className="col-span-3 pb-10">
       <Outlet />
     </div>
   );
