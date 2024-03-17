27aacc27e4d07312f592dd4c35378952d76827c4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-17 21:24:17 +0700

    Add handleLogout functionality to LogoutButton component

--- src/components/SidebarL/LogoutButton.jsx
+++ src/components/SidebarL/LogoutButton.jsx
@@ -1,8 +1,11 @@
+import { useUserContext } from '../../hooks/useUserContext';
+
 const LogoutButton = () => {
+  const { handleLogout } = useUserContext();
   return (
-    <div className="text-center gap-3 p-4 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-150 hover:bg-primaryColor cursor-pointer mt-auto lg:w-full w-fit">
+    <button className="text-center gap-3 p-4 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-150 hover:bg-primaryColor cursor-pointer mt-auto lg:w-full w-fit" onClick={handleLogout}>
       <p className="font-bold">Log Out</p>
-    </div>
+    </button>
   );
 };
 
