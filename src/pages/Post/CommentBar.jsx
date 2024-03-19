a5736ca33e4fbf8d91efaf1982079e32f7986551
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-19 15:29:34 +0700

    Add text color to comment button

--- src/pages/Post/CommentBar.jsx
+++ src/pages/Post/CommentBar.jsx
@@ -7,7 +7,7 @@ const CommentBar = () => {
         <div className="h-10 w-10 bg-slate-500 animate-pulse rounded-full"></div>
       </div>
       <input type="text" name="post" id="post" className="w-full ps-16 border-0 bg-slate-100 focus:ring-0 focus:outline-none rounded-xl p-5 me-3 text-xs ring-0" placeholder="Buat komentar..." required />
-      <button className="flex items-center justify-center bg-primaryColor rounded-md w-20 hover:bg-hoverColor active:bg-activeColor transition-all">
+      <button className="flex items-center justify-center bg-primaryColor rounded-md w-20 hover:bg-hoverColor active:bg-activeColor text-white transition-all">
         <ArrowIcon />
       </button>
     </div>
