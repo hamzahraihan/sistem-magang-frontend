79e2dfd32e675f3580327d6a48d111e7f4e503ac
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-19 15:29:16 +0700

    Add InternshipPlaceholder component

---
+++ src/components/Placeholder/InternshipPlaceholder.jsx
@@ -0,0 +1,48 @@
+const InternshipPlaceholder = () => {
+  return (
+    <>
+      <div className="flex border items-center border-gray-200 rounded-xl p-4 transition-all">
+        <div className="flex flex-1 gap-1 flex-col">
+          <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
+          <p className="bg-gray-600 animate-pulse h-3 w-96 rounded-lg"></p>
+          <div className="flex gap-2">
+            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+          </div>
+        </div>
+      </div>
+      <div className="flex border items-center border-gray-200 rounded-xl p-4 transition-all">
+        <div className="flex flex-1 gap-1 flex-col">
+          <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
+          <p className="bg-gray-600 animate-pulse h-3 w-96 rounded-lg"></p>
+          <div className="flex gap-2">
+            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+          </div>
+        </div>
+      </div>
+      <div className="flex border items-center border-gray-200 rounded-xl p-4 transition-all">
+        <div className="flex flex-1 gap-1 flex-col">
+          <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
+          <p className="bg-gray-600 animate-pulse h-3 w-96 rounded-lg"></p>
+          <div className="flex gap-2">
+            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+          </div>
+        </div>
+      </div>
+      <div className="flex border items-center border-gray-200 rounded-xl p-4 transition-all">
+        <div className="flex flex-1 gap-1 flex-col">
+          <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
+          <p className="bg-gray-600 animate-pulse h-3 w-96 rounded-lg"></p>
+          <div className="flex gap-2">
+            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InternshipPlaceholder;
