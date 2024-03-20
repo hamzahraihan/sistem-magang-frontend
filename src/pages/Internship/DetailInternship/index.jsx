829646b8d934a4b378533980efd43ae94bd273ff
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-20 14:15:39 +0700

    Add InternshipDocs component for displaying internship documents

--- src/pages/Internship/DetailInternship/index.jsx
+++ src/pages/Internship/DetailInternship/index.jsx
@@ -5,6 +5,7 @@ import InternshipNotFound from '../InternshipNotFound';
 import SidebarDetailInternship from './SidebarDetailInternship';
 import { formatDate } from '../../../utils/formatDate';
 import { capitalizeFirstLetter } from '../../../utils/capitalizeFirstLetter';
+import InternshipDocs from './InternshipDocs';
 
 const DetailInternship = () => {
   const { loadingDetail, internshipByID } = useInternshipContext();
@@ -24,39 +25,30 @@ const DetailInternship = () => {
           <div className="flex flex-col gap-5">
             <div className="flex flex-col gap-1">
               <h1 className="font-bold text-2xl">{capitalizeFirstLetter(internshipByID.instance)}</h1>
-              <div className="flex flex-col gap-1">
+              <div className="flex flex-col gap-1 text-sm">
                 <p className="text-gray-400">Periode magang</p>
                 <p className="font-bold">
                   {formatDate(internshipByID.start_intern)} - {formatDate(internshipByID.end_intern)}
                 </p>
               </div>
-              <div className="flex flex-col gap-1">
+              <div className="flex flex-col gap-1 text-sm">
                 <p className="text-gray-400">Lokasi</p>
                 <p className="font-bold">{internshipByID.location}</p>
               </div>
-              <div className="flex flex-col gap-1">
+              <div className="flex flex-col gap-1 text-sm">
                 <p className="text-gray-400">Deskripsi Perusahaan</p>
                 <p className="font-bold">{internshipByID.description}</p>
               </div>
-              <div className="flex flex-col gap-1">
+              <div className="flex flex-col gap-1 text-sm">
                 <p className="text-gray-400">Tipe Magang</p>
                 <p className="font-bold">{internshipByID.type}</p>
               </div>
             </div>
             <div className="flex flex-col gap-2">
-              <h1 className="font-bold text-sm">Dokumen Persyaratan Magang</h1>
-              <div className="flex flex-col gap-1">
-                <h1 className="text-sm">Surat bersedia dosen magang</h1>
-                <iframe className="w-full h-96 rounded-xl" src={`https://drive.google.com/file/d/${internshipByID.lecture_agreement}/preview`} title="doc-1"></iframe>
-              </div>
-              <div className="flex flex-col gap-1">
-                <h1 className="text-sm">Surat magang dari kampus</h1>
-                <iframe className="w-full h-96 rounded-xl" src={`https://drive.google.com/file/d/${internshipByID.campus_approval}/preview`} title="doc-1"></iframe>
-              </div>
-              <div className="flex flex-col gap-1">
-                <h1 className="text-sm">Surat magang dari perusahaan</h1>
-                <iframe className="w-full h-96 rounded-xl" src={`https://drive.google.com/file/d/${internshipByID.intern_agreement}/preview`} title="doc-1"></iframe>
-              </div>
+              <h1 className="font-bold text-base">Dokumen Persyaratan Magang</h1>
+              <InternshipDocs text="Surat bersedia dosen magang" docsID={internshipByID.lecture_agreement} />
+              <InternshipDocs text="Surat magang dari kampus" docsID={internshipByID.campus_approval} />
+              <InternshipDocs text="Surat magang dari perusahaan" docsID={internshipByID.intern_agreement} />
             </div>
           </div>
         )}
