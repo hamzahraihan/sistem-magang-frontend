a964064dbaab31f8a25ec4668d9cfa01e48365f0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-20 21:42:41 +0700

    Add ModalInternshipDocs component

---
+++ src/pages/Internship/DetailInternship/ModalInternshipDocs.jsx
@@ -0,0 +1,33 @@
+import { Modal } from 'flowbite-react';
+import InternshipDocs from './InternshipDocs';
+
+const ModalInternshipDocs = ({ id, isOpen, closeModal, modalType }) => {
+  let modalTitle, modalContent;
+
+  switch (modalType) {
+    case 'lecture_docu':
+      modalTitle = 'Surat bersedia dosen magang';
+      modalContent = id.lecture_agreement;
+      break;
+    case 'campus_docu':
+      modalTitle = 'Surat magang dari kampus';
+      modalContent = id.campus_approval;
+      break;
+    case 'instance_docu':
+      modalTitle = 'Surat magang dari perusahaan';
+      modalContent = id.intern_agreement;
+      break;
+  }
+  return (
+    <Modal position="center" size="6xl" dismissible show={isOpen} onClose={closeModal}>
+      <Modal.Header>
+        <h1 className="text-sm py-2 px-3 bg-hoverColor w-fit rounded-lg text-gray-50">{modalTitle}</h1>
+      </Modal.Header>
+      <Modal.Body>
+        <InternshipDocs docsID={modalContent} />
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default ModalInternshipDocs;
