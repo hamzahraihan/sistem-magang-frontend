05f49d8b536b1695ba8a1910b4aeecee319b3f1b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-20 21:41:50 +0700

    Remove PrimeReactProvider from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import SidebarL from './components/SidebarL/index.jsx';
-import { PrimeReactProvider } from 'primereact/api';
 import UserProvider from './context/UserContext.jsx';
 import PostProvider from './context/PostContext.jsx';
 import { InternshipProvider } from './context/InternshipContext.jsx';
@@ -8,16 +7,14 @@ import { InternshipProvider } from './context/InternshipContext.jsx';
 function App() {
   return (
     <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-5 md:gap-2 sm:gap-2 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative">
-      <PrimeReactProvider>
-        <UserProvider>
-          <PostProvider>
-            <InternshipProvider>
-              <SidebarL />
-              <Outlet />
-            </InternshipProvider>
-          </PostProvider>
-        </UserProvider>
-      </PrimeReactProvider>
+      <UserProvider>
+        <PostProvider>
+          <InternshipProvider>
+            <SidebarL />
+            <Outlet />
+          </InternshipProvider>
+        </PostProvider>
+      </UserProvider>
     </div>
   );
 }
