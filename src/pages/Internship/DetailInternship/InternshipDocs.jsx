acb7d189205c28cf0bc7f89fbf44388997d8aa93
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-20 21:42:35 +0700

    Refactor InternshipDocs component to remove unused prop

--- src/pages/Internship/DetailInternship/InternshipDocs.jsx
+++ src/pages/Internship/DetailInternship/InternshipDocs.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types';
 
 const InternshipDocs = (props) => {
-  const { docsID, text } = props;
+  const { docsID } = props;
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-sm py-2 px-3 bg-hoverColor w-fit rounded-lg text-gray-50">{text}</h1>
-      <iframe className="w-full h-96 rounded-xl" src={`https://drive.google.com/file/d/${docsID}/preview`} title="doc-1"></iframe>
+      <iframe className="w-full h-[500px] rounded-xl" src={`https://drive.google.com/file/d/${docsID}/preview`} title="doc-1"></iframe>
     </div>
   );
 };
