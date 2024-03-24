e38ceafda0662f12a8595330756a32140b3ec742
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-24 15:41:24 +0700

    Update SidebarLogbookDetail component

--- src/pages/Logbook/LogbookDetail/SidebarLogbookDetail/index.jsx
+++ src/pages/Logbook/LogbookDetail/SidebarLogbookDetail/index.jsx
@@ -1,11 +1,11 @@
 import LogbookOverallCard from './LogbookOverallCard';
 
-const SidebarLogbookDetail = (props) => {
+const SidebarLogbookDetail = () => {
   return (
-    <div className="sticky flex flex-col lg:col-span-2 col-span-4 top-5 lg:items-start h-[90vh]">
+    <div className="sticky flex flex-col lg:col-span-1 col-span-4 top-5 lg:items-start h-[90vh]">
       <div className="flex flex-col items-center w-full gap-5">
         <h1 className="text-xl font-bold text-start w-full">Logbook Minguan</h1>
-        <LogbookOverallCard weekLog={props.state} />
+        <LogbookOverallCard />
       </div>
     </div>
   );
