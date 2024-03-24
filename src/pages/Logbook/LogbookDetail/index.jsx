dabb6edb839b0f18ee4b81acea6284aea2f7c867
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-24 15:41:14 +0700

    Refactor LogbookDetail component

--- src/pages/Logbook/LogbookDetail/index.jsx
+++ src/pages/Logbook/LogbookDetail/index.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SidebarLogbookDetail from './SidebarLogbookDetail';
 
 import { ArrowIcon } from '../../../components/Icons';
@@ -6,19 +6,18 @@ import LogbookWeek from './LogbookWeek';
 import { useLogbookContext } from '../../../hooks/useLogbookContext';
 
 const LogbookDetail = () => {
-  const { state } = useLocation();
   const { logbook } = useLogbookContext();
 
   return (
     <div className="col-span-3 pb-10">
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-3 gap-5">
         <div className="flex flex-col gap-4 lg:col-span-2 col-span-4">
           <Link to="/kegiatan-magang/logbook" state={{ internshipID: logbook.internship_id }} className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
             <ArrowIcon />
           </Link>
-          <LogbookWeek state={state} />
+          <LogbookWeek />
         </div>
-        <SidebarLogbookDetail state={state} />
+        <SidebarLogbookDetail />
       </div>
     </div>
   );
