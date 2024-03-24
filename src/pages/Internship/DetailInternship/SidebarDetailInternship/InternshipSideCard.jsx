e219be78a93a4f35be77c4c5f3c65a436e110037
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-24 15:40:48 +0700

    Add lodash import and update link in InternshipSideCard component

--- src/pages/Internship/DetailInternship/SidebarDetailInternship/InternshipSideCard.jsx
+++ src/pages/Internship/DetailInternship/SidebarDetailInternship/InternshipSideCard.jsx
@@ -3,6 +3,7 @@ import { useInternshipContext } from '../../../../hooks/useInternshipContext';
 import InternshipSVG from '../../../../assets/svg/Freelancer-bro.svg';
 import { formatDate } from '../../../../utils/formatDate';
 import { Spinner } from '../../../../components/Icons';
+import _ from 'lodash';
 
 const InternshipSideCard = () => {
   const { loading, internship } = useInternshipContext();
@@ -17,13 +18,13 @@ const InternshipSideCard = () => {
         <Spinner />
       ) : (
         internship.map((item) => (
-          <Link to={`/kegiatan-magang/detail`} state={{ internshipID: item.internship_id }} key={item.internship_id} className="flex flex-col gap-2 border border-neutral-200 rounded-[32px] h-full w-full">
+          <Link to={`/kegiatan-magang/detail/${item.internship_id}`} state={{ internshipID: item.internship_id }} key={item.internship_id} className="flex flex-col gap-2 border border-neutral-200 rounded-[32px] h-full w-full">
             <div className="h-28 bg-no-repeat bg-cover bg-center rounded-se-[32px] rounded-ss-[32px]" style={imageBackground}></div>
             <div className="flex flex-col gap-1 p-4 ">
               <div className="flex gap-2">
                 <p className="flex gap-1 items-center text-neutral-700 text-xs">Magang Mandiri</p>
               </div>
-              <p className="text-sm font-bold">Diskominfo</p>
+              <p className="text-sm font-bold">{_.capitalize(item.instance)}</p>
               <p className="text-neutral-400">
                 {formatDate(item.start_intern)} - {formatDate(item.end_intern)}
               </p>
