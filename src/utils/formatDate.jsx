5523894f1a9d5393eef0680338c17df2ade2f4b9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-24 15:41:31 +0700

    Add date formatting and weekday functions to formatDate.jsx

--- src/utils/formatDate.jsx
+++ src/utils/formatDate.jsx
@@ -1,6 +1,17 @@
 export const formatDate = (date) => {
   const options = {
     year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+
+  return new Date(date).toLocaleDateString('id-ID', options);
+};
+
+export const weekDay = (date) => {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
     month: 'long',
     day: 'numeric',
   };
