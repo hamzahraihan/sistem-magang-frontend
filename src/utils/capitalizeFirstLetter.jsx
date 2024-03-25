41b20edca406f48f794d44506c0d448cc1d59780
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-25 15:50:41 +0700

    Delete capitalizeFirstLetter utility function

--- src/utils/capitalizeFirstLetter.jsx
+++
@@ -1,7 +0,0 @@
-export const capitalizeFirstLetter = (string) => {
-  const firstLetter = string?.charAt(0);
-  const firstLetterCap = firstLetter?.toUpperCase();
-  const remainingLetters = string?.slice(1);
-  const capWord = firstLetterCap + remainingLetters;
-  return capWord;
-};
