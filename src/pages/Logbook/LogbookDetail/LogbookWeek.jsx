0d8f7cd773c4e6332bd518fe6b40b9a733021965
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-26 15:22:38 +0700

    Fix styling issue in LogbookWeek component

--- src/pages/Logbook/LogbookDetail/LogbookWeek.jsx
+++ src/pages/Logbook/LogbookDetail/LogbookWeek.jsx
@@ -14,7 +14,7 @@ const LogbookWeek = () => {
         daily.slice(0, 5).map((item) => (
           <div key={item.date_intern} className="flex flex-col justify-center border border-neutral-300 rounded-[32px]">
             <div className="flex border-b items-center gap-2 border-neutral-200 rounded-tl-[32px] rounded-tr-[32px] p-4 font-bold">
-              <div className={`h-10 w-10 rounded-full ${item.isComplete && 'bg-hoverColor'} border border-neutral-200`}></div>
+              <div className={`h-10 w-10 rounded-full ${item.isComplete && 'bg-hoverColor border-none'} border border-neutral-200`}></div>
               <div className="flex flex-col">
                 <p>{weekDay(item.date_intern)}</p>
               </div>
