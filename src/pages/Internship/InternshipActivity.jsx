b1de73869ac8fe7d9449180fd2001f5d7fe41c39
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-26 15:20:45 +0700

    Add lodash import and capitalize instance name

--- src/pages/Internship/InternshipActivity.jsx
+++ src/pages/Internship/InternshipActivity.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { ArrowIcon } from '../../components/Icons';
 import InternshipPlaceholder from '../../components/Placeholder/InternshipPlaceholder';
 import useFetchInternship from '../../features/internship/useFetchInternship';
@@ -23,12 +24,12 @@ const InternshipActivity = () => {
             <Link
               to={`/kegiatan-magang/detail/${item.internship_id}`}
               state={{ internshipID: item.internship_id }}
-              className="flex border items-center border-gray-200 rounded-xl p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white transition-all"
+              className="flex border items-center border-gray-200 rounded-xl p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white hover:border-white transition-all"
               key={item.internship_id}
             >
               <div className="flex flex-1 flex-col">
                 <p className="text-bold">Magang Mandiri</p>
-                <p className="font-bold text-base">{item.instance}</p>
+                <p className="font-bold text-base">{_.capitalize(item.instance)}</p>
                 <p>
                   Periode Magang: {formatDate(item.start_intern)} - {formatDate(item.end_intern)}
                 </p>
