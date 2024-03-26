18ef619f6bb1d1428b5b6bf0f522456e5aba9179
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-27 00:22:18 +0700

    Add LogbookDailyContext component

---
+++ src/context/LogbookDailyContext.jsx
@@ -0,0 +1,76 @@
+import { createContext, useReducer, useState } from 'react';
+import PropTypes from 'prop-types';
+import { TOKEN } from '../constant/key';
+import axios from 'axios';
+
+export const LogbookDailyContext = createContext(null);
+
+export const LogbookDailyDispatch = createContext(null);
+
+const LogbookDailyProvider = ({ children }) => {
+  const [logbookDaily, dispatch] = useReducer(LogbookDailyReducer, []);
+
+  const [loadingUpdate, setLoadingUpdate] = useState(false);
+
+  const token = localStorage.getItem(TOKEN);
+
+  const editLogbook = async ({ logday_id, log_description }) => {
+    setLoadingUpdate(true);
+    try {
+      const data = await axios.put(
+        `${import.meta.env.VITE_BASE_URL}/logbook/daily/edit/${logday_id}`,
+        {
+          log_description,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(data);
+      setLoadingUpdate(false);
+    } catch (error) {
+      console.error(error);
+      setLoadingUpdate(false);
+    }
+  };
+
+  return (
+    <LogbookDailyContext.Provider
+      value={{
+        logbookDaily,
+        editLogbook,
+        loadingUpdate,
+      }}
+    >
+      <LogbookDailyDispatch.Provider value={dispatch}>{children}</LogbookDailyDispatch.Provider>
+    </LogbookDailyContext.Provider>
+  );
+};
+
+const LogbookDailyReducer = (logbookDaily, action) => {
+  switch (action.type) {
+    case 'SET_DAILYLOG':
+      return action.payload;
+    case 'ADD_DAILYLOG':
+      return [...logbookDaily, action.payload];
+    case 'EDIT_DAILYLOG':
+      return logbookDaily.map((day) => {
+        if (day.logday_id == action.payload.logday_id) {
+          return action.payload;
+        } else {
+          return day;
+        }
+      });
+    default:
+      break;
+  }
+};
+
+LogbookDailyProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default LogbookDailyProvider;
