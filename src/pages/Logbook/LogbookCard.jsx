4ec3e59c998dd373f626fdac9aa0f499b2e12310
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-27 17:21:37 +0700

    Refactor LogbookCard component to remove unused code and update Link URL

--- src/pages/Logbook/LogbookCard.jsx
+++ src/pages/Logbook/LogbookCard.jsx
@@ -1,16 +1,16 @@
 import { ArrowIcon } from '../../components/Icons';
 import PropTypes from 'prop-types';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { formatDate, weekDay } from '../../utils/formatDate';
 import { useUserContext } from '../../hooks/useUserContext';
 
 const LogbookCard = ({ week }) => {
   const { Logdailies } = week;
+
   const { userLoggedInData } = useUserContext();
-  const { id } = useParams();
 
   return (
-    <Link to={`/kegiatan-magang/logbook/aktivitas/${userLoggedInData.id}/${week.logbook_id}`} state={{ id: id }} className="border border-neutral-200 rounded-[32px] lg:p-9 md:p-4 sm:p-4 p-4">
+    <Link to={`/kegiatan-magang/logbook/aktivitas/${week.internship_id}/${userLoggedInData.id}/${week.logbook_id}`} state={{ id: week.logbook_id }} className="border border-neutral-200 rounded-[32px] lg:p-9 md:p-4 sm:p-4 p-4">
       <div className="flex justify-between items-center gap-2">
         <div>
           <div className="flex items-center xm:text-base text-md font-bold gap-1">
