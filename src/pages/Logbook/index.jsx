8db819a8dba870b18843c23237a6adb44005944c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-27 17:21:32 +0700

    Update Logbook component to use internship_id instead of id

--- src/pages/Logbook/index.jsx
+++ src/pages/Logbook/index.jsx
@@ -10,13 +10,13 @@ import useFetchWeeklyLogbook from '../../features/logbook/useFetchWeeklyLogbook'
 const Logbook = () => {
   const { weeks, loading } = useFetchWeeklyLogbook();
   const { loading: loadingDate, logbook } = useFetchLogbook();
-  const { id } = useParams();
+  const { internship_id } = useParams();
 
   return (
     <div className="col-span-3 pb-10">
       <div className="grid grid-cols-3 gap-5">
         <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
-          <Link to={`/kegiatan-magang/detail/${id}`} className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+          <Link to={`/kegiatan-magang/detail/${internship_id}`} className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
             <ArrowIcon />
           </Link>
           <p className="text-base font-bold">
