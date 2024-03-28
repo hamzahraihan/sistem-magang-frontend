f8a06c7b00ce6909bf9e91d0204e12f5e68671a0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-28 20:12:15 +0700

    Add useState and handleUpdateWeeklyLog to LogbookWeeklyActivityContext

--- src/context/LogbookWeelyActivityContext.jsx
+++ src/context/LogbookWeelyActivityContext.jsx
@@ -1,5 +1,7 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { TOKEN } from '../constant/key';
 
 export const LogbookWeeklyActivityContext = createContext(null);
 
@@ -7,9 +9,36 @@ export const LogbookWeeklyActivityDispatch = createContext(null);
 
 const LogbookWeeklyActivityProvider = ({ children }) => {
   const [weeklyActivity, dispatch] = useReducer(LogbookWeeklyActivityReducer, []);
+  const [loadingUpdate, setLoadingUpdate] = useState(false);
+  const token = localStorage.getItem(TOKEN);
+
+  const handleUpdateWeeklyLog = async ({ logbook_id, log_description, status }) => {
+    setLoadingUpdate(true);
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_BASE_URL}/logbook/weekly/edit/${logbook_id}`,
+        {
+          logbook_id,
+          log_description,
+          status,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response);
+      setLoadingUpdate(false);
+    } catch (error) {
+      console.error(error);
+      setLoadingUpdate(false);
+    }
+  };
 
   return (
-    <LogbookWeeklyActivityContext.Provider value={{ weeklyActivity }}>
+    <LogbookWeeklyActivityContext.Provider value={{ weeklyActivity, handleUpdateWeeklyLog, loadingUpdate }}>
       <LogbookWeeklyActivityDispatch.Provider value={dispatch}>{children}</LogbookWeeklyActivityDispatch.Provider>
     </LogbookWeeklyActivityContext.Provider>
   );
@@ -22,7 +51,7 @@ const LogbookWeeklyActivityReducer = (weeklyActivity, action) => {
     case 'ADD_WEEK_ACTIVITY':
       return action.payload;
     case 'EDIT_WEEK_ACTIVITY':
-      return;
+      return action.payload;
   }
 };
 LogbookWeeklyActivityProvider.propTypes = {
