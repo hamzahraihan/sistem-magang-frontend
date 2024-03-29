b6b7006259145d6e1a490053e46f1d09baad5383
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-29 15:51:36 +0700

    Refactor FormUploadReport component

--- src/pages/UploadIntern/FormUploadReport.jsx
+++ src/pages/UploadIntern/FormUploadReport.jsx
@@ -1,38 +1,16 @@
 import PrimaryButton from '../../components/PrimaryButton';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { useRef, useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+import { useReportInternContext } from '../../hooks/useReportInternContext';
 
 const FormUploadReport = () => {
   const [value, setValue] = useState('');
 
-  const fileInputRef = useRef(null);
-
-  const handleFileUpload = async (e) => {
-    e.preventDefault();
-    const files = fileInputRef.current.files;
-    console.log('🚀 ~ handleFileUpload ~ files:', files);
-
-    if (files.length > 0) {
-      const formData = new FormData();
-
-      for (const file of files) {
-        formData.append('files', file);
-        console.log(formData.get('files'));
-      }
-
-      try {
-        const { data } = await axios.post(`${import.meta.env.VITE_BASE_URL}/report-intern/upload-report`, formData);
-        console.log('file uploaded', data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  };
+  const { fileInputRef } = useReportInternContext();
 
   return (
-    <form className="flex flex-col gap-2 text-base" onSubmit={handleFileUpload}>
+    <div className="flex flex-col gap-2 text-base">
       <label htmlFor="title">Judul</label>
       <input className="rounded-lg border border-gray-300" type="text" name="title" id="title" placeholder="Masukkan judul laporan akhir " />
 
@@ -44,7 +22,7 @@ const FormUploadReport = () => {
       <div className="lg:hidden block pt-4">
         <PrimaryButton type="submit" text={'Kirim laporan'} />
       </div>
-    </form>
+    </div>
   );
 };
 
