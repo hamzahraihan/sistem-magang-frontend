5c16d69dfcbf6e89312a8e94bd22c850f06e7e2a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-29 15:22:17 +0700

    Add toast notification for unauthorized access

--- src/features/internship/useFetchInternship.jsx
+++ src/features/internship/useFetchInternship.jsx
@@ -3,6 +3,7 @@ import { useInternshipContext, useInternshipDispatch } from '../../hooks/useInte
 import { getInternshipByUser } from '../../constant/api';
 import { useUserContext } from '../../hooks/useUserContext';
 import { TOKEN } from '../../constant/key';
+import toast from 'react-hot-toast';
 
 const useFetchInternship = () => {
   const [loading, setLoading] = useState(false);
@@ -16,10 +17,13 @@ const useFetchInternship = () => {
     const getUserInternship = async () => {
       setLoading(true);
       try {
-        const data = await getInternshipByUser(userLoggedInData?.id, token);
+        const { data } = await getInternshipByUser(userLoggedInData?.id, token);
         setLoading(false);
         dispatch({ type: 'SET_INTERNSHIP_DATA', payload: data });
       } catch (error) {
+        if (error.response.status == 403) {
+          toast.error('Kamu belum login');
+        }
         console.error(error);
       }
     };
