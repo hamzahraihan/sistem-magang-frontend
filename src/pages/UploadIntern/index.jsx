1857d3f2ca8fcd7011c4e440e9c45d454066f345
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-29 15:51:39 +0700

    Add handleFileUpload function to form onSubmit

--- src/pages/UploadIntern/index.jsx
+++ src/pages/UploadIntern/index.jsx
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom';
 import { ArrowIcon } from '../../components/Icons';
 import SidebarReport from './SidebarReport';
 import FormUploadReport from './FormUploadReport';
+import { useReportInternContext } from '../../hooks/useReportInternContext';
 
 const UploadIntern = () => {
+  const { handleFileUpload } = useReportInternContext();
+
   return (
     <div className="col-span-3 pb-10">
-      <form className="grid grid-cols-3 gap-5">
+      <form className="grid grid-cols-3 gap-5" onSubmit={handleFileUpload}>
         <div className="lg:order-first flex flex-col gap-4 lg:col-span-2 col-span-3 order-last">
           <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
             <ArrowIcon />
