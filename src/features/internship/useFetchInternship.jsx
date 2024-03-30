3591821f7668e185270e767f457f436aae659bca
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-30 13:08:56 +0700

    Refactor fetching internship data

--- src/features/internship/useFetchInternship.jsx
+++ src/features/internship/useFetchInternship.jsx
@@ -2,22 +2,20 @@ import { useEffect, useState } from 'react';
 import { useInternshipContext, useInternshipDispatch } from '../../hooks/useInternshipContext';
 import { getInternshipByUser } from '../../constant/api';
 import { useUserContext } from '../../hooks/useUserContext';
-import { TOKEN } from '../../constant/key';
 import toast from 'react-hot-toast';
 
 const useFetchInternship = () => {
   const [loading, setLoading] = useState(false);
-  const { userLoggedInData } = useUserContext();
+  const { userLoggedInData, accessToken } = useUserContext();
+  console.log('🚀 ~ useFetchInternship ~ accessToken:', accessToken);
   const { internship } = useInternshipContext();
   const dispatch = useInternshipDispatch();
-
-  const token = localStorage.getItem(TOKEN);
 
   useEffect(() => {
     const getUserInternship = async () => {
       setLoading(true);
       try {
-        const { data } = await getInternshipByUser(userLoggedInData?.id, token);
+        const data = await getInternshipByUser(userLoggedInData?.id, accessToken);
         setLoading(false);
         dispatch({ type: 'SET_INTERNSHIP_DATA', payload: data });
       } catch (error) {
@@ -29,7 +27,7 @@ const useFetchInternship = () => {
       }
     };
     getUserInternship();
-  }, [dispatch, userLoggedInData, token]);
+  }, [dispatch, userLoggedInData, accessToken]);
 
   return { loading, internship };
 };
