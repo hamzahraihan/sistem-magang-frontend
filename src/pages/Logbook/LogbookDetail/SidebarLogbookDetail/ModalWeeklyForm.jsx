73ff3fbf55f5de244d1b3d496cc497ca07c75028
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-30 13:09:28 +0700

    Refactor ModalWeeklyForm component to use Formik and add validation

--- src/pages/Logbook/LogbookDetail/SidebarLogbookDetail/ModalWeeklyForm.jsx
+++ src/pages/Logbook/LogbookDetail/SidebarLogbookDetail/ModalWeeklyForm.jsx
@@ -1,25 +1,34 @@
 import { Modal, Textarea } from 'flowbite-react';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import PrimaryButton from '../../../../components/PrimaryButton';
 import { useLogbookWeeklyActivityContext, useLogbookWeeklyActivityDispatch } from '../../../../hooks/useLogbookWeeklyActivityContext';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 const ModalWeeklyForm = ({ isOpen, closeModal, id }) => {
-  const [logDescription, setLogDescription] = useState('');
-
   const { handleUpdateWeeklyLog, loadingUpdate } = useLogbookWeeklyActivityContext();
   const dispatch = useLogbookWeeklyActivityDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const update = {
+  const formik = useFormik({
+    initialValues: {
       logbook_id: id,
-      log_description: logDescription,
+      log_description: '',
       status: 'Belum disetujui',
-    };
-    dispatch({ type: 'EDIT_WEEK_ACTIVITY', payload: update });
-    handleUpdateWeeklyLog(update);
+    },
+    onSubmit: (values) => {
+      dispatch({ type: 'EDIT_WEEK_ACTIVITY', payload: values });
+      handleUpdateWeeklyLog(values);
+    },
+    validationSchema: yup.object().shape({
+      log_description: yup.string().required('Logbook wajib diisi'),
+    }),
+  });
+
+  const handleInputForm = (event) => {
+    const { target } = event;
+    formik.setFieldValue(target.name, target.value);
   };
+
   return (
     <Modal position="center" size="2xl" dismissible show={isOpen} onClose={closeModal}>
       <Modal.Header>
@@ -28,15 +37,14 @@ const ModalWeeklyForm = ({ isOpen, closeModal, id }) => {
         </div>
       </Modal.Header>
       <Modal.Body>
-        <form className="flex flex-col">
-          <Textarea className="rounded-lg h-52" type="text" name="title" id="title" placeholder="Buatkan kesimpulan dari hasil kegiatan harianmu" value={logDescription} onChange={(e) => setLogDescription(e.target.value)} required />
+        <form className="flex flex-col gap-4">
+          <Textarea className="rounded-lg h-52" type="text" name="log_description" id="log_description" placeholder="Buatkan kesimpulan dari hasil kegiatan harianmu" onChange={handleInputForm} required />
+          <p className="text-xs text-red-800">{formik.errors.log_description}</p>
+          <div className="w-40">
+            <PrimaryButton text="Kirim" onClick={formik.handleSubmit} loading={loadingUpdate} />
+          </div>
         </form>
       </Modal.Body>
-      <Modal.Footer>
-        <div className="w-40">
-          <PrimaryButton text="Kirim" onClick={handleSubmit} loading={loadingUpdate} />
-        </div>
-      </Modal.Footer>
     </Modal>
   );
 };
