c1b2bf50d25b40de3ca2e752db3d5870070c3e9b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-03-31 20:41:16 +0700

    Add InternshipList component

---
+++ src/pages/UploadIntern/InternshipList.jsx
@@ -0,0 +1,11 @@
+import InternshipCard from './InternshipCard';
+
+const InternshipList = () => {
+  return (
+    <div>
+      <InternshipCard />
+    </div>
+  );
+};
+
+export default InternshipList;
