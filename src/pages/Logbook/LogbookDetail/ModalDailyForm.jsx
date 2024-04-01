f50dacc833fe15eca2d7ceb8c0c3782eea7c3224
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-01 15:47:16 +0700

    Refactor ModalDailyForm component to use Formik and add validation

--- src/pages/Logbook/LogbookDetail/ModalDailyForm.jsx
+++ src/pages/Logbook/LogbookDetail/ModalDailyForm.jsx
@@ -1,24 +1,37 @@
 import { Modal, Textarea } from 'flowbite-react';
 import PropTypes from 'prop-types';
 import PrimaryButton from '../../../components/PrimaryButton';
-import { useState } from 'react';
 import { useLogbookDailyContext, useLogbookDailyDispatch } from '../../../hooks/useLogbookDailyContext';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 const ModalDailyForm = ({ isOpen, closeModal, logdayID, date }) => {
-  const [logDescription, setLogDescription] = useState('');
   const { editLogbook, loadingUpdate } = useLogbookDailyContext();
   const dispatch = useLogbookDailyDispatch();
-  const handleSubmit = (e) => {
-    e.preventDefault();
+
+  const handleCreateLogbook = (values) => {
     const update = {
       logday_id: logdayID,
-      log_description: logDescription,
+      log_description: values.log_description,
       isComplete: true,
       date_intern: date,
     };
     dispatch({ type: 'EDIT_DAILYLOG', payload: update });
     editLogbook(update);
   };
+
+  const formik = useFormik({
+    initialValues: {
+      log_description: '',
+    },
+    onSubmit: (values) => {
+      handleCreateLogbook(values);
+    },
+    validationSchema: yup.object().shape({
+      log_description: yup.string().required('Wajib diisi'),
+    }),
+  });
+
   return (
     <Modal position="center" size="2xl" dismissible show={isOpen} onClose={closeModal}>
       <Modal.Header>
@@ -27,15 +40,23 @@ const ModalDailyForm = ({ isOpen, closeModal, logdayID, date }) => {
         </div>
       </Modal.Header>
       <Modal.Body>
-        <form className="flex flex-col">
-          <Textarea className="rounded-lg h-52" type="text" name="title" id="title" placeholder="Ceritakan pengalaman magang mu hari ini " value={logDescription} onChange={(e) => setLogDescription(e.target.value)} required />
+        <form className="flex flex-col gap-2">
+          <Textarea
+            className="rounded-lg h-52"
+            type="text"
+            name="log_description"
+            id="title"
+            placeholder="Ceritakan pengalaman magang mu hari ini "
+            value={formik.values.log_description}
+            onChange={(e) => formik.setFieldValue(e.target.name, e.target.value)}
+            required
+          />
+          <p className="text-xs text-red-800">{formik.errors.log_description}</p>
+          <div className="w-40">
+            <PrimaryButton text="Kirim" onClick={formik.handleSubmit} loading={loadingUpdate} />
+          </div>
         </form>
       </Modal.Body>
-      <Modal.Footer>
-        <div className="w-40">
-          <PrimaryButton text="Kirim" onClick={handleSubmit} loading={loadingUpdate} />
-        </div>
-      </Modal.Footer>
     </Modal>
   );
 };
