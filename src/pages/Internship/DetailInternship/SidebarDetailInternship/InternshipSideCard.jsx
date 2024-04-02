b46918e02b26f9b32235b8b87fd77c3e17ea5405
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-02 14:40:19 +0700

    Refactor InternshipSideCard component

--- src/pages/Internship/DetailInternship/SidebarDetailInternship/InternshipSideCard.jsx
+++ src/pages/Internship/DetailInternship/SidebarDetailInternship/InternshipSideCard.jsx
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom';
 import InternshipSVG from '../../../../assets/svg/Freelancer-bro.svg';
 import { formatDate } from '../../../../utils/formatDate';
-import { Spinner } from '../../../../components/Icons';
 import _ from 'lodash';
 import useFetchInternship from '../../../../features/internship/useFetchInternship';
 
 const InternshipSideCard = () => {
-  const { loading, internship } = useFetchInternship();
+  const { internship } = useFetchInternship();
 
   const imageBackground = {
     backgroundImage: `url(${InternshipSVG})`,
@@ -14,30 +13,26 @@ const InternshipSideCard = () => {
 
   return (
     <>
-      {loading ? (
-        <Spinner />
-      ) : (
-        internship.map((item) => (
-          <Link
-            to={`/kegiatan-magang/detail/${item.internship_id}`}
-            state={{ internshipID: item.internship_id }}
-            key={item.internship_id}
-            className="flex flex-col gap-2 border border-neutral-200 rounded-[32px] h-full w-full"
-            onClick={window.scrollTo(0, 0)}
-          >
-            <div className="h-28 bg-no-repeat bg-cover bg-center rounded-se-[32px] rounded-ss-[32px]" style={imageBackground}></div>
-            <div className="flex flex-col gap-1 p-4 ">
-              <div className="flex gap-2">
-                <p className="flex gap-1 items-center text-neutral-700 text-xs">Magang Mandiri</p>
-              </div>
-              <p className="text-sm font-bold">{_.capitalize(item.instance)}</p>
-              <p className="text-neutral-400">
-                {formatDate(item.start_intern)} - {formatDate(item.end_intern)}
-              </p>
+      {internship.map((item) => (
+        <Link
+          to={`/kegiatan-magang/detail/${item.internship_id}`}
+          state={{ internshipID: item.internship_id }}
+          key={item.internship_id}
+          className="flex flex-col gap-2 border border-neutral-200 rounded-[32px] h-full w-full"
+          onClick={window.scrollTo(0, 0)}
+        >
+          <div className="h-28 bg-no-repeat bg-cover bg-center rounded-se-[32px] rounded-ss-[32px]" style={imageBackground}></div>
+          <div className="flex flex-col gap-1 p-4 ">
+            <div className="flex gap-2">
+              <p className="flex gap-1 items-center text-neutral-700 text-xs">Magang Mandiri</p>
             </div>
-          </Link>
-        ))
-      )}
+            <p className="text-sm font-bold">{_.capitalize(item.instance)}</p>
+            <p className="text-neutral-400">
+              {formatDate(item.start_intern)} - {formatDate(item.end_intern)}
+            </p>
+          </div>
+        </Link>
+      ))}
     </>
   );
 };
