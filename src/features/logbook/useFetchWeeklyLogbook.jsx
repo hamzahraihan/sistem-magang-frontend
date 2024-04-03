d4aa61d8580eca82976705a84f172b6f6639ed0d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-03 14:27:10 +0700

    Add token authentication and error handling

--- src/features/logbook/useFetchWeeklyLogbook.jsx
+++ src/features/logbook/useFetchWeeklyLogbook.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getWeeklyLogAPI } from '../../constant/api';
 import { useLogbookWeeklyContext, useLogbookWeeklyDispatch } from '../../hooks/useLogbookWeeklyContext';
+import { TOKEN } from '../../constant/key';
+import toast from 'react-hot-toast';
 
 const useFetchWeeklyLogbook = () => {
   const [loading, setLoading] = useState(false);
@@ -14,15 +16,20 @@ const useFetchWeeklyLogbook = () => {
     const signal = controller.signal;
     const getWeeklyLog = async () => {
       setLoading(true);
+      const token = localStorage.getItem(TOKEN);
       try {
-        const data = await getWeeklyLogAPI(internship_id, signal);
+        const data = await getWeeklyLogAPI(internship_id, token, signal);
         dispatch({ type: 'SET_WEEKLYLOG', payload: data });
         setLoading(false);
       } catch (error) {
         if (error.name == 'CanceledError') {
           console.error('Previous request was aborted');
         } else {
+          if (error.response.status === 403) {
+            toast.error('Anda tidak mempunyai hak akses. Pastikan Anda sudah login dengan akun dan role yang benar.');
+          }
           console.error(error);
+          setLoading(false);
         }
       }
     };
