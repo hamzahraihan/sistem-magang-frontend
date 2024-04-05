0b9807f1ca26931bb1346c88d02a31570a274b88
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-05 16:49:12 +0700

    Add InternshipList component

---
+++ src/pages/Internship/InternshipList.jsx
@@ -0,0 +1,11 @@
+import InternshipCard from './InternshipCard';
+
+const InternshipList = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <InternshipCard />
+    </div>
+  );
+};
+
+export default InternshipList;
