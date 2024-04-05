2f0d5c5a9b687779341e780163f3741eef96fe62
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-05 16:49:16 +0700

    Update styling and content in NotFound component

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col gap-3 justify-center items-center h-screen ">
+    <div className="flex flex-col gap-3 justify-center items-center h-screen p-2 text-center">
       <h1 className="text-9xl font-bold">Oops!</h1>
       <p className="text-base">Halaman yang kamu cari tidak ditemukan atau kamu belum login.</p>
       <Link to="/" className="bg-primaryColor rounded-[32px] py-2 px-4 shadow-md hover:bg-hoverColor text-white transition-all active:bg-activeColor">
