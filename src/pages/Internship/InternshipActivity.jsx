362524e90f0eb0dbff69370d7811610dc6632a6e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-05 16:49:05 +0700

    Refactor InternshipActivity component

--- src/pages/Internship/InternshipActivity.jsx
+++ src/pages/Internship/InternshipActivity.jsx
@@ -1,45 +1,19 @@
-import _ from 'lodash';
 import { ArrowIcon } from '../../components/Icons';
-import InternshipPlaceholder from '../../components/Placeholder/InternshipPlaceholder';
 import useFetchInternship from '../../features/internship/useFetchInternship';
-import { formatDate } from '../../utils/formatDate';
 import InternshipNotFound from './InternshipNotFound';
 import SidebarInternship from './SidebarInternship';
 import { Link } from 'react-router-dom';
+import InternshipList from './InternshipList';
 
 const InternshipActivity = () => {
-  const { loading, internship } = useFetchInternship();
+  const { internship } = useFetchInternship();
   return (
     <div className="grid grid-cols-3 gap-5">
       <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
         <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
           <ArrowIcon />
         </Link>
-        {loading ? (
-          <InternshipPlaceholder />
-        ) : internship.length === 0 ? (
-          <InternshipNotFound />
-        ) : (
-          internship.map((item) => (
-            <Link
-              to={`/kegiatan-magang/detail/${item.internship_id}`}
-              state={{ internshipID: item.internship_id }}
-              className="flex border items-center border-gray-200 rounded-xl p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white hover:border-white transition-all"
-              key={item.internship_id}
-            >
-              <div className="flex flex-1 flex-col">
-                <p className="text-bold">Magang Mandiri</p>
-                <p className="font-bold text-base">{_.capitalize(item.instance)}</p>
-                <p>
-                  Periode Magang: {formatDate(item.start_intern)} - {formatDate(item.end_intern)}
-                </p>
-              </div>
-              <div>
-                <ArrowIcon />
-              </div>
-            </Link>
-          ))
-        )}
+        {internship.length === 0 ? <InternshipNotFound /> : <InternshipList />}
       </div>
       <SidebarInternship />
     </div>
