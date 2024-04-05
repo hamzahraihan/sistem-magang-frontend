790bf1e3442ac665b2781352558b366f9da3a3f7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-05 16:48:41 +0700

    Add BottomNavMobile component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,11 @@ import LogbookWeeklyProvider from './context/LogbookWeeklyContext.jsx';
 import LogbookWeeklyActivityProvider from './context/LogbookWeelyActivityContext.jsx';
 import { Toaster } from 'react-hot-toast';
 import ReportInternProvider from './context/ReportInternContext.jsx';
+import BottomNavMobile from './components/BottomNavMobile/index.jsx';
 
 function App() {
   return (
-    <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-5 md:gap-2 sm:gap-2 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative">
+    <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-5 md:gap-2 sm:gap-2 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative lg:mb-0 md:mb-0 sm:mb-0 mb-14">
       <UserProvider>
         <PostProvider>
           <LogbookProvider>
@@ -23,6 +24,7 @@ function App() {
                     <ReportInternProvider>
                       <SidebarL />
                       <Outlet />
+                      <BottomNavMobile />
                       <Toaster position="top-center" />
                     </ReportInternProvider>
                   </InternshipProvider>
