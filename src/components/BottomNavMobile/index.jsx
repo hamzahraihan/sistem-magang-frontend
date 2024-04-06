b3d3bb54462ff20dd8c5a58261052f92bd168461
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-06 16:11:25 +0700

    Add BottomNavMobile component and handle profile menu in BottomNavMobile

--- src/components/BottomNavMobile/index.jsx
+++ src/components/BottomNavMobile/index.jsx
@@ -2,14 +2,24 @@ import { NavLink } from 'react-router-dom';
 import HomeIcon, { DocumentIcon, OfficeBrifcase, UploadIcon, UserIcon } from '../Icons';
 import { useUserContext } from '../../hooks/useUserContext';
 import _ from 'lodash';
+import { useState } from 'react';
 
 const BottomNavMobile = () => {
-  const { userLoggedInData } = useUserContext();
+  const [openProfile, setOpenProfile] = useState(false);
+  const { userLoggedInData, handleLogout } = useUserContext();
 
   const slugify = () => {
     const full_name = `${userLoggedInData.first_name} ${userLoggedInData.last_name}`;
     const slug_name = _.kebabCase(full_name);
     return slug_name;
+  };
+
+  const handleOpenMenu = () => {
+    if (openProfile) {
+      setOpenProfile(false);
+    } else if (!openProfile) {
+      setOpenProfile(true);
+    }
   };
 
   return (
@@ -23,9 +33,32 @@ const BottomNavMobile = () => {
           <OfficeBrifcase />
         </NavLink>
 
-        <NavLink to={userLoggedInData ? `/profile/${userLoggedInData?.role}/${_.kebabCase(slugify())}` : '/login'} state={{ userId: userLoggedInData?.id }}>
-          {userLoggedInData ? <div className="rounded-full bg-slate-400 animate-pulse  h-10 w-10 flex-shrink-0"></div> : <UserIcon />}
-        </NavLink>
+        {userLoggedInData ? (
+          <div className="relative">
+            <button type="button" className="rounded-full bg-slate-400 animate-pulse h-10 w-10" onClick={handleOpenMenu}></button>
+            {openProfile && (
+              <div className="absolute bottom-16 -right-[89px] border shadow-lg rounded-xl text-sm">
+                <div className="flex flex-col-reverse p-2 bg-white rounded-xl">
+                  <NavLink
+                    className=" hover:bg-gray-200 active:bg-gray-100 rounded-lg duration-150 p-2 w-52 text-center"
+                    to={userLoggedInData ? `/profile/${userLoggedInData?.role}/${_.kebabCase(slugify())}` : '/login'}
+                    state={{ userId: userLoggedInData?.id }}
+                    onClick={handleOpenMenu}
+                  >
+                    <p>Profil</p>
+                  </NavLink>
+                  <button className=" hover:bg-gray-200 active:bg-gray-100 rounded-lg duration-150 p-2 w-52 text-center" onClick={handleLogout}>
+                    <p>Log Out</p>
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        ) : (
+          <NavLink to="/login">
+            <UserIcon />
+          </NavLink>
+        )}
 
         <NavLink to="/report" className={({ isActive }) => (isActive ? 'bg-primaryColor hover:bg-hoverColor duration-150 ease-out text-white rounded-full p-4' : 'bg-white')}>
           <UploadIcon />
