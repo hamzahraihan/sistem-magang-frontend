687009eee593ddb5b7a070a237e424a9afefb5ac
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-06 16:12:10 +0700

    Update Link styling and import statements in SidebarInternship component

--- src/pages/Report/index.jsx
+++ src/pages/Report/index.jsx
@@ -12,7 +12,7 @@ const UploadIntern = () => {
     <div className="col-span-3 pb-10">
       <div className="grid grid-cols-3 gap-5">
         <div className="lg:order-first flex flex-col gap-4 lg:col-span-2 col-span-3 order-last">
-          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+          <Link to="/" className="lg:flex hidden items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
             <ArrowIcon />
           </Link>
           <h1 className="text-xl font-bold">Laporan Akhir Magang</h1>
