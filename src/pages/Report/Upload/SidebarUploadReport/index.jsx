6abdddfd3fce6b234a973d448deaa6cd4e23b38c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-06 16:12:28 +0700

    Update Link and import statements in SidebarUploadReport component

--- src/pages/Report/Upload/SidebarUploadReport/index.jsx
+++ src/pages/Report/Upload/SidebarUploadReport/index.jsx
@@ -1,10 +1,16 @@
+import { Link, useParams } from 'react-router-dom';
 import PrimaryButton from '../../../../components/PrimaryButton';
 import { useReportInternContext } from '../../../../hooks/useReportInternContext';
+import { ArrowIcon } from '../../../../components/Icons';
 
 const SidebarUploadReport = () => {
   const { loadingUpload } = useReportInternContext();
+  const { internship_id } = useParams();
   return (
-    <div className="lg:col-span-1 gap-4 col-span-3 flex flex-col top-5 lg:items-start  ">
+    <div className="lg:col-span-1 gap-4 col-span-3 flex flex-col top-5 lg:items-start ">
+      <Link to={`/kegiatan-magang/detail/${internship_id}`} className="flex lg:hidden items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+        <ArrowIcon />
+      </Link>
       <div className="lg:block w-full hidden">
         <PrimaryButton type="submit" text="Kirim Laporan" loading={loadingUpload} />
       </div>
