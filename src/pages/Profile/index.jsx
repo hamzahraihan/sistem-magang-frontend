06e060d58b59e565a415ec896288bb884dbcd55c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-07 14:19:00 +0700

    Update Profile page with new components and dependencies

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
-import { ArrowIcon } from '../../components/Icons';
-import SidebarProfile from './SidebarProfile';
+import { ArrowIcon, EditIcon } from '../../components/Icons';
 import CardPost from '../../components/CardPost';
 import ProfilePlaceholder from './Placeholder';
 import { usePostContext } from '../../hooks/usePostContext';
 import CardPostPlaceholder from '../../components/Placeholder/CardPostPlaceholder';
 import useFetchUserByID from '../../features/user/useFetchUserById';
 import _ from 'lodash';
+import SidebarProfile from './SidebarProfile';
+import Avvvatars from 'avvvatars-react';
+import { useUserContext } from '../../hooks/useUserContext';
+import StatusCard from './SidebarProfile/StatusCard';
 
 const Profile = () => {
   const { loading, userByID } = useFetchUserByID();
+  const { userLoggedInData } = useUserContext();
 
   const { loadingPostByUser, postByUser } = usePostContext();
 
@@ -26,18 +30,38 @@ const Profile = () => {
             <>
               <h1 className="text-xl font-bold">Profile</h1>
               <div className="flex flex-col gap-2 justify-center items-center m-auto">
-                {userByID.image ? (
+                {userByID?.image ? (
                   <img className="border border-neutral-400 h-52 w-52  bg-slate-500 animate-pulse rounded-full" alt="profile-pict"></img>
                 ) : (
-                  <div className="flex text-5xl font-bold bg-activeColor rounded-full h-52 w-52 items-center justify-center text-white">
-                    <p>{_.capitalize(userByID.first_name[0])}</p> <p>{_.capitalize(userByID.last_name[0])}</p>
-                  </div>
+                  <>
+                    {userByID?.first_name && userByID?.last_name && (
+                      <div className="relative">
+                        <Avvvatars value={userByID.first_name + userByID.last_name} displayValue={_.capitalize(userByID.first_name[0]) + _.capitalize(userByID.last_name[0])} size={200} />
+                        {userLoggedInData.id == userByID.mahasiswa_id && (
+                          <Link to="/" className="absolute top-1 right-0 flex items-center rounded-full bg-white p-3 hover:bg-gray-300 active:bg-gray-200 duration-150">
+                            <EditIcon />
+                          </Link>
+                        )}
+                      </div>
+                    )}
+                  </>
                 )}
 
-                <p className="font-bold text-base rounded-xl bg-activeColor py-2 px-4 w-fit text-white">
-                  {userByID.first_name} {userByID.last_name}
-                </p>
+                <div className="flex gap-2">
+                  <p className="font-bold text-base rounded-xl bg-activeColor py-2 px-4 w-fit text-white">
+                    {userByID.first_name} {userByID.last_name}
+                  </p>
+                </div>
                 <p className="text-neutral-400">{userByID.email}</p>
+              </div>
+              <div className="flex flex-col gap-2 text-base w-full lg:hidden">
+                {userByID && (
+                  <>
+                    <StatusCard type="angkatan" title="Angkatan" status={userByID.angkatan} />
+                    <StatusCard type="status" title="Status" status={userByID.status} />
+                    <StatusCard type="instance" title="Perusahaan/Instansi" status={userByID?.Internships?.[0]?.instance} />
+                  </>
+                )}
               </div>
             </>
           )}
