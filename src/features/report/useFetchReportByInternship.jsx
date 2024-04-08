a6e8f635b2175b10e2ac243267475cbd9991eb2a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-08 13:45:31 +0700

    Add toast error message for 403 status in useFetchReportByInternship.jsx

--- src/features/report/useFetchReportByInternship.jsx
+++ src/features/report/useFetchReportByInternship.jsx
@@ -3,6 +3,7 @@ import { getReportByInternshipIdAPI } from '../../constant/api';
 import { useReportInternContext, useReportInternDispatch } from '../../hooks/useReportInternContext';
 import { TOKEN } from '../../constant/key';
 import { useParams } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const useFetchReportByInternship = () => {
   const [loading, setLoading] = useState(false);
@@ -19,6 +20,9 @@ const useFetchReportByInternship = () => {
         dispatch({ type: 'SET_REPORT_DATA', payload: data });
         setLoading(false);
       } catch (error) {
+        if (error.response.status === 403) {
+          toast.error('Anda tidak mempunyai hak akses. Pastikan Anda sudah login dengan akun dan role yang benar.');
+        }
         console.error(error);
         setLoading(false);
       }
