780f4d0eee77b3c1a3dbd5caf9173a8b77a8196d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-08 13:45:15 +0700

    Fix error handling in useFetchLogbook.jsx

--- src/features/logbook/useFetchLogbook.jsx
+++ src/features/logbook/useFetchLogbook.jsx
@@ -26,6 +26,7 @@ const useFetchLogbook = () => {
           toast.error('Anda tidak mempunyai hak akses. Pastikan Anda sudah login dengan akun dan role yang benar.');
         }
         console.error(error);
+        setLoading(false);
       }
     };
     getInternshipLogbook();
