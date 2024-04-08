5a66670018e410a955e7b37e8a367d922e32be75
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-08 13:45:01 +0700

    Fix error handling in useFetchInternship.jsx

--- src/features/internship/useFetchInternship.jsx
+++ src/features/internship/useFetchInternship.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useInternshipContext, useInternshipDispatch } from '../../hooks/useInternshipContext';
 import { getInternshipByUser } from '../../constant/api';
 import { useUserContext } from '../../hooks/useUserContext';
-import toast from 'react-hot-toast';
 import { TOKEN } from '../../constant/key';
 
 const useFetchInternship = () => {
@@ -22,9 +21,6 @@ const useFetchInternship = () => {
         setLoading(false);
         dispatch({ type: 'SET_INTERNSHIP_DATA', payload: data });
       } catch (error) {
-        if (error.response.status == 403) {
-          toast.error('Kamu belum login');
-        }
         console.error(error);
         setLoading(false);
       }
