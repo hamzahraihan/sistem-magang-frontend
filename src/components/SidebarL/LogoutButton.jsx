748fd059e2546051cc619ddd5a5c4695df5bdb37
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-09 13:30:10 +0700

    Update styling and add hover effect to LogoutButton component in SidebarL/LogoutButton.jsx

--- src/components/SidebarL/LogoutButton.jsx
+++ src/components/SidebarL/LogoutButton.jsx
@@ -3,7 +3,10 @@ import { useUserContext } from '../../hooks/useUserContext';
 const LogoutButton = () => {
   const { handleLogout } = useUserContext();
   return (
-    <button className="text-center gap-3 p-4 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-150 hover:bg-primaryColor cursor-pointer mt-auto lg:w-full w-fit" onClick={handleLogout}>
+    <button
+      className="text-center gap-3 p-4 items-center border rounded-3xl border-neutral-200 transition-all ease-in-out duration-150 hover:bg-primaryColor hover:text-white cursor-pointer mt-auto lg:w-full w-fit bg-white"
+      onClick={handleLogout}
+    >
       <p className="font-bold">Log Out</p>
     </button>
   );
