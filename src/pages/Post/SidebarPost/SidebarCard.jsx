810d4ca4b88784d286bac00eaa7ace0096632810
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-09 13:31:39 +0700

    Update background color and image size in SidebarCard component

--- src/pages/Post/SidebarPost/SidebarCard.jsx
+++ src/pages/Post/SidebarPost/SidebarCard.jsx
@@ -10,11 +10,11 @@ const SidebarCard = (props) => {
     backgroundImage: 'url(https://drive.google.com/thumbnail?id=' + post.image + ')',
   };
   return (
-    <div className="border border-neutral-200 rounded-[32px]">
+    <div className="rounded-[32px]">
       <Link to={`/detail-post/${_.kebabCase(post?.title)}`} state={{ post_id: post?.post_id }}>
-        <div className="bg-no-repeat bg-cover bg-center h-32 rounded-se-[32px] rounded-ss-[32px]" style={imageBackground}></div>
+        <div className="bg-no-repeat bg-cover bg-center h-52 rounded-[32px]" style={imageBackground}></div>
       </Link>
-      <div className="flex flex-col gap-3 p-7">
+      <div className="flex flex-col p-3">
         <div className="flex gap-2">
           <p className="flex gap-1 items-center text-neutral-700 text-xs">
             <ClockIcon />
