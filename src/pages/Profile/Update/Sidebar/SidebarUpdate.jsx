4135e7ec16db237518d016f816f066b4416f0171
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-11 15:37:49 +0700

    Update SidebarUpdate component in Profile/Update page

--- src/pages/Profile/Update/Sidebar/SidebarUpdate.jsx
+++ src/pages/Profile/Update/Sidebar/SidebarUpdate.jsx
@@ -1,18 +1,25 @@
-import { Link } from 'react-router-dom';
-import { ArrowIcon } from '../../../../components/Icons';
 import PrimaryButton from '../../../../components/PrimaryButton';
+import PropTypes from 'prop-types';
 
-const SidebarUpdate = () => {
+const SidebarUpdate = ({ tab }) => {
   return (
-    <div className="lg:col-span-1 gap-4 col-span-3 lg:flex flex-col top-5 lg:items-start w-full">
-      <Link to="/" className="flex lg:hidden mb-2 items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
-        <ArrowIcon />
-      </Link>
-      <div className="lg:block w-full hidden">
-        <PrimaryButton type="submit" text="Ubah profil" />
-      </div>
+    <div className="lg:col-span-1 gap-4 col-span-3 lg:flex flex-col top-5 lg:items-start w-full hidden">
+      {tab == 'profile' && (
+        <div className="block w-full">
+          <PrimaryButton type="submit" text="Ubah profil" />
+        </div>
+      )}
+      {tab == 'password' && (
+        <div className="block w-full">
+          <PrimaryButton type="submit" text="Ubah password" />
+        </div>
+      )}
     </div>
   );
+};
+
+SidebarUpdate.propTypes = {
+  tab: PropTypes.string,
 };
 
 export default SidebarUpdate;
