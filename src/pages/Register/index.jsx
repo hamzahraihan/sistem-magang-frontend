22030baec684e04808c18fdf3be5e048338b23ec
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-11 15:37:54 +0700

    Update background color and add hover effect to ProfileButton component in SidebarL/ProfileButton.jsx

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -4,7 +4,7 @@ import RegisterMenu from './RegisterMenu';
 
 const index = () => {
   return (
-    <div className="flex justify-center items-center h-full p-10">
+    <div className="flex justify-center items-center h-full p-10 bg-gray-50">
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-10 w-[1000px]">
         <UserProvider>
           <RegisterMenu />
