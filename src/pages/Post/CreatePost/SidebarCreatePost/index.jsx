214d80460d3160b1fb8b444ed05677c9835f2a35
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-14 11:59:57 +0700

    Refactor SidebarCreatePost component to use loading state in Submit button

--- src/pages/Post/CreatePost/SidebarCreatePost/index.jsx
+++ src/pages/Post/CreatePost/SidebarCreatePost/index.jsx
@@ -1,10 +1,12 @@
 import PrimaryButton from '../../../../components/PrimaryButton';
+import { usePostContext } from '../../../../hooks/usePostContext';
 
 const SidebarCreatePost = () => {
+  const { loadingPost } = usePostContext();
   return (
     <div className="flex flex-col top-5 lg:items-start lg:order-first order-last">
       <div className="w-full font-bold">
-        <PrimaryButton type={'submit'} text={'Submit'} />
+        <PrimaryButton type="submit" text="Submit" loading={loadingPost} />
       </div>
     </div>
   );
