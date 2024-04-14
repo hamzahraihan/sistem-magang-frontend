49605be7cad3fc3bd38e576a20dec79f2e32629d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-14 12:00:15 +0700

    Refactor getRoleId function to update data property names in getRoleId.jsx

--- src/utils/getRoleId.jsx
+++ src/utils/getRoleId.jsx
@@ -1,9 +1,9 @@
 export const getRoleId = (data) => {
   if (data?.dosen_id == null && data?.admin_id == null) {
-    return data?.Mahasiswa.mahasiswa_id;
+    return data?.mahasiswa_id;
   } else if (data?.mahasiswa_id == null && data?.admin_id == null) {
-    return data?.Dosen.dosen_id;
+    return data?.dosen_id;
   } else {
-    return data?.Admin.admin_id;
+    return data?.admin_id;
   }
 };
