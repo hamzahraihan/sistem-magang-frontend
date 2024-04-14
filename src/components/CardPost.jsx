7325b26fa474c6e01b253a7bc76a4533936ff7ac
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-14 11:57:40 +0700

    Update CardPost component with image handling and content sanitization functionality

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -4,46 +4,44 @@ import { formatDate } from '../utils/formatDate';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import DOMPurify from 'dompurify';
-import { pickRole } from '../utils/pickRole';
 import { slugify } from '../utils/slugify';
 import { getRoleId } from '../utils/getRoleId';
 
+// card post use for index/homepage
 const CardPost = (props) => {
   const { post } = props;
 
-  const imageBackground = {
-    backgroundImage: `url(https://drive.google.com/thumbnail?id=${post?.image}&sz=w680)`,
-  };
-
+  // option to sanitize content from ReactQuill input component
   const options = {
     ALLOWED_TAGS: ['p', 'br', 'b', 'i', 'strong', 'em'],
     ALLOWED_ATTR: ['href', 'title', 'alt'],
   };
 
+  // sanitize using DOMPurify
   const sanitizeContent = DOMPurify.sanitize(post?.description, options);
 
   return (
-    <div className="flex flex-col">
+    <div className=" flex flex-col bg-white rounded-3xl shadow-sm">
       <Link to={`/detail-post/${_.kebabCase(post?.title)}`} state={{ post_id: post?.post_id }}>
-        {post.image && <div className="border border-neutral-200 lg:h-96 h-52 rounded-[32px] bg-cover bg-no-repeat bg-center" style={imageBackground}></div>}
+        {post.image && <img src={`https://drive.google.com/thumbnail?id=${post?.image}&sz=w680`} className="border border-neutral-200 lg:h-96 h-52 w-full rounded-3xl object-cover object-center " alt="post-pict" />}
       </Link>
       <div className="p-5 flex flex-col gap-2">
-        <Link to={`/detail-post/${_.kebabCase(post?.title)}`} state={{ post_id: post?.post_id }} className="flex flex-col gap-2">
+        <Link to={`/detail-post/${_.kebabCase(post?.title)}`} state={{ post_id: post?.post_id }} className="flex flex-col">
+          <p className="text-base font-bold">{post?.title}</p>
           <div className="flex gap-2">
-            <p className="flex gap-1 items-center text-neutral-700 text-sm">
+            <p className="flex gap-1 items-center text-neutral-400">
               <ClockIcon />
               {formatDate(post?.updatedAt)}
             </p>
-            <p className="flex gap-1 items-center text-neutral-700 text-sm">
+            <p className="flex gap-1 items-center text-neutral-400">
               <TagIcon />
               {_.capitalize(post?.category_name)}
             </p>
           </div>
-          <p className="text-base font-bold">{post?.title}</p>
-          <p className="line-clamp-3" dangerouslySetInnerHTML={{ __html: sanitizeContent }} />
+          <p className="line-clamp-3 pt-2" dangerouslySetInnerHTML={{ __html: sanitizeContent }} />
         </Link>
-        <Link to={`/profile/${pickRole(post)?.role}/${_.kebabCase(slugify(post))}`} state={{ userId: getRoleId(post) }} className="text-neutral-400 underline">
-          {pickRole(post)?.first_name}
+        <Link to={`/profile/${post?.role}/${_.kebabCase(slugify(post))}`} state={{ userId: getRoleId(post) }} className="text-neutral-400 underline">
+          {post?.author}
         </Link>
       </div>
     </div>
