5897fb3976481a603efd4073500d81355a8bbc83
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-14 11:59:46 +0700

    Refactor Post/Description component to update heading font size and remove unnecessary paragraph class

--- src/pages/Post/Description.jsx
+++ src/pages/Post/Description.jsx
@@ -7,8 +7,8 @@ const Description = () => {
     <div className="flex flex-col gap-2 ">
       <PostDate />
       <div className="flex flex-col gap-2 pb-2">
-        <h1 className="text-xl font-bold leading-8">{postById[0]?.title}</h1>
-        <p className="text-base" dangerouslySetInnerHTML={{ __html: postById[0]?.description }} />
+        <h1 className="text-base font-bold leading-8">{postById[0]?.title}</h1>
+        <p dangerouslySetInnerHTML={{ __html: postById[0]?.description }} />
       </div>
       <div className="border-t border-slate-300 w-full"></div>
     </div>
