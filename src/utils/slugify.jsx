b76afe042754295f0f07a649c98a0ac1d8b77559
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-14 12:00:20 +0700

    Refactor slugify function to use data.author instead of pickRole in slugify.jsx

--- src/utils/slugify.jsx
+++ src/utils/slugify.jsx
@@ -1,8 +1,7 @@
 import _ from 'lodash';
-import { pickRole } from './pickRole';
 
 export const slugify = (data) => {
-  const full_name = `${pickRole(data)?.first_name} ${pickRole(data)?.last_name}`;
+  const full_name = data?.author;
   const slug_name = _.kebabCase(full_name);
   return slug_name;
 };
