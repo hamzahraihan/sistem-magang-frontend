9a3e3d663e383754483d502e6dc5a8998f1f8e61
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-14 12:00:03 +0700

    Refactor SidebarPost component to remove console.log statements and update link URL

--- src/pages/Post/SidebarPost/index.jsx
+++ src/pages/Post/SidebarPost/index.jsx
@@ -10,16 +10,17 @@ import { getRoleId } from '../../../utils/getRoleId';
 
 const SidebarPost = () => {
   const { loadingPost, postById, post } = usePostContext();
+  console.log('🚀 ~ SidebarPost ~ postById:', postById);
   console.log('🚀 ~ SidebarPost ~ post:', post);
 
   return (
-    <div className="lg:flex flex-col top-5 lg:items-start h-[90vh] hidden">
+    <div className="lg:flex flex-col top-5 lg:items-start  hidden">
       <div className="flex flex-col items-center w-full gap-5">
         <h1 className="text-xl font-bold text-start w-full">Tentang Author</h1>
         {pickRole(postById[0])?.image ? (
           <div className="h-28 w-28 bg-slate-500 animate-pulse rounded-full"></div>
         ) : (
-          <Link to={`/profile/${pickRole(postById[0])?.role}/${_.kebabCase(slugify(postById[0]))}`} state={{ userId: getRoleId(postById[0]) }} className="flex items-center gap-2 w-fit ">
+          <Link to={`/profile/${postById[0]?.role}/${_.kebabCase(slugify(postById[0]))}`} state={{ userId: getRoleId(postById[0]) }} className="flex items-center gap-2 w-fit ">
             <Avvvatars value={pickRole(postById[0])?.first_name + pickRole(postById[0])?.last_name} displayValue={_.capitalize(pickRole(postById[0])?.first_name[0]) + _.capitalize(pickRole(postById[0])?.last_name[0])} size={150} />
           </Link>
         )}
@@ -37,7 +38,7 @@ const SidebarPost = () => {
           ) : (
             post
               .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
-              .slice(0, 2)
+              .slice(0, 5)
               .map((item) => <SidebarCard key={item.post_id} post={item} />)
           )}
         </div>
