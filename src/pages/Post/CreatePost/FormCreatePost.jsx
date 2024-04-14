591ab0265370bf35678999c8f0403f0fe7ec2aaa
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-14 11:59:51 +0700

    Refactor FormCreatePost component to update input styles and remove unnecessary code

--- src/pages/Post/CreatePost/FormCreatePost.jsx
+++ src/pages/Post/CreatePost/FormCreatePost.jsx
@@ -7,28 +7,25 @@ const FormCreatePost = () => {
   console.log('🚀 ~ FormCreatePost ~ postInputData:', postInputData);
 
   return (
-    <div className="flex flex-col gap-2 text-base">
+    <div className="flex flex-col gap-2 text-sm">
       <label htmlFor="title">Judul</label>
       <input
-        className="rounded-lg border border-gray-300"
+        className="text-xs rounded-lg border border-gray-300"
         type="text"
         name="title"
         id="title"
         placeholder="Masukkan judul untuk postingan mu "
         required
-        value={postInputData.title}
         onChange={(e) => setPostInputData({ ...postInputData, [e.target.name]: e.target.value })}
       />
 
       <label htmlFor="category_name">Kategori</label>
       <select
-        id="large"
         name="category_name"
-        className="text-base block w-full p-4 text-gray-900 border border-gray-300 rounded-lg "
-        value={postInputData.category_name}
+        className="text-sm block w-full p-2 text-gray-900 border border-gray-300 rounded-lg "
+        value={postInputData.category_name || 'magang'}
         onChange={(e) => setPostInputData({ ...postInputData, [e.target.name]: e.target.value })}
       >
-        <option defaultValue="magang">Pilih Kategori</option>
         <option value="magang">Magang</option>
         <option value="diskusi">Diskusi</option>
         <option value="kendala">Kendala</option>
@@ -36,10 +33,10 @@ const FormCreatePost = () => {
       </select>
 
       <label htmlFor="file_url">Upload Gambar</label>
-      <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer  focus:outline-none " id="file_url" type="file" ref={imageInputRef} />
+      <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-white focus:outline-none " id="file_url" type="file" ref={imageInputRef} />
 
       <label htmlFor="desc">Deskripsi Post</label>
-      <ReactQuill theme="snow" id="description" value={postInputData.description} onChange={(content) => setPostInputData({ ...postInputData, description: content })} className="pb-14" />
+      <ReactQuill theme="snow" id="description" onChange={(content) => setPostInputData({ ...postInputData, description: content })} className="pb-14 bg-white" />
     </div>
   );
 };
