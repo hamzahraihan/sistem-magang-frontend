fe32a4e5dc188033b8f41e8b064af607ebf9e739
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-15 18:57:24 +0700

    Refactor UpdateProfile component to update user profile form fields and add password update functionality

--- src/pages/Profile/Update/index.jsx
+++ src/pages/Profile/Update/index.jsx
@@ -6,15 +6,13 @@ import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { useEffect, useState } from 'react';
 import FormUpdatePassword from './FormUpdatePassword';
-import PrimaryButton from '../../../components/PrimaryButton';
 import useFetchUserByID from '../../../features/user/useFetchUserById';
 import { useUserContext } from '../../../hooks/useUserContext';
 
 const UpdateProfile = () => {
   const [tab, setTab] = useState('profile');
   const { userByID } = useFetchUserByID();
-  const { userLoggedInData } = useUserContext();
-  const { handleUpdateProfile, loadingUpdate } = useUserContext();
+  const { handleUpdateProfile, handleUpdatePassword, loadingUpdate } = useUserContext();
 
   const validFileExt = { files: ['png', 'jpg', 'jpeg'] };
   const isValidFileType = (fileName) => {
@@ -37,6 +35,7 @@ const UpdateProfile = () => {
       nidn: '',
       jurusan: '',
       angkatan: '',
+      old_password: '',
       kelas: '',
       image: '',
       gender: '',
@@ -46,7 +45,12 @@ const UpdateProfile = () => {
       handleUpdateProfile(values);
     },
     validationSchema: yup.object().shape({
-      dosen_id: yup.number().required('Dosen wajib diisi'),
+      dosen_id: yup.number().test('is-dosen-exist', 'Dosen wajib diisi', (value) => {
+        if (userByID.role !== 'mahasiswa') return true;
+        if (value) {
+          return true;
+        }
+      }),
       first_name: yup.string().required('Nama depan wajib diisi'),
       last_name: yup.string().required('Nama belakang wajib diisi'),
       email: yup
@@ -54,32 +58,44 @@ const UpdateProfile = () => {
         .required('Email wajib diisi')
         .email()
         .test('is-student-unsika', 'Email wajib menggunakan @student.unsika.ac.id', (value) => {
+          if (userByID.role !== 'mahasiswa') return true;
           if (value) {
             const [, domain] = value.split('@');
             return domain === 'student.unsika.ac.id';
           }
           return false;
         }),
-      nim: yup.string().required('NIM wajib diisi'),
+      nim: yup.string().test('is-nim-student', 'NIM wajib diisi', (value) => userByID.role !== 'mahasiswa' || value == userByID.nim),
       nidn: yup.string(),
-      angkatan: yup.string().required('Angkatan wajib diisi'),
-      kelas: yup.string().required('Kelas wajib diisi'),
-      // image: yup
-      //   .mixed()
-      //   .test('fileType', `File harus berupa gambar dengan format, ${allowedExt}`, (value) => {
-      //     if (value) {
-      //       return isValidFileType(value.name);
-      //     }
-      //     return true;
-      //   })
-      //   .test('fileSize', 'Ukuran file terlalu besar', (value) => {
-      //     if (value) {
-      //       return value.size <= MAX_FILE_SIZE;
-      //     }
-      //     return true;
-      //   }),
+      angkatan: yup.string().test('is-angkatan-student', 'Angkatan wajib diisi', (value) => userByID.role !== 'mahasiswa' || value == userByID.angkatan),
+      kelas: yup.string().test('is-kelas-student', 'Kelas wajib diisi', (value) => userByID.role !== 'mahasiswa' || value == userByID.kelas),
+      image: yup
+        .mixed()
+        .test('fileType', `File harus berupa gambar dengan format, ${allowedExt}`, (value) => {
+          if (value == userByID.image || !value) return true;
+          console.log('🚀 ~ .test ~ value:', value);
+          return isValidFileType(value?.name);
+        })
+        .test('fileSize', 'Ukuran file terlalu besar', (value) => {
+          if (value == userByID.image || !value) return true;
+          return value?.size <= MAX_FILE_SIZE;
+        }),
       gender: yup.string(),
       phone: yup.string().required('Kontak wajib diisi'),
+    }),
+  });
+
+  const formikPassword = useFormik({
+    initialValues: {
+      password: '',
+      old_password: '',
+    },
+    onSubmit: (values) => {
+      handleUpdatePassword(values);
+    },
+    validationSchema: yup.object().shape({
+      password: yup.string().required('Password wajib diisi'),
+      old_password: yup.string().required('Password lama wajib diisi'),
     }),
   });
 
@@ -112,7 +128,7 @@ const UpdateProfile = () => {
 
   return (
     <div className="col-span-3 pb-10">
-      <form className="grid grid-cols-3 gap-5" onSubmit={formik.handleSubmit}>
+      <div className="grid grid-cols-3 gap-5">
         <div className="lg:order-first flex flex-col gap-4 lg:col-span-2 col-span-3 order-last">
           <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
             <ArrowIcon />
@@ -131,21 +147,10 @@ const UpdateProfile = () => {
               </button>
             ))}
           </div>
-          {tab == 'profile' ? <FormUpdateProfile formik={formik} handleInputForm={handleInputForm} allowedExt={allowedExt} /> : <FormUpdatePassword />}
-
-          {tab == 'profile' && (
-            <div className="block lg:hidden">
-              <PrimaryButton type="submit" text="Ubah profil" loading={loadingUpdate} />
-            </div>
-          )}
-          {tab == 'password' && (
-            <div className="block lg:hidden">
-              <PrimaryButton type="submit" text="Ubah password" />
-            </div>
-          )}
+          {tab == 'profile' ? <FormUpdateProfile user={userByID} formik={formik} handleInputForm={handleInputForm} allowedExt={allowedExt} /> : <FormUpdatePassword formikPassword={formikPassword} />}
         </div>
-        <SidebarUpdate tab={tab} />
-      </form>
+        <SidebarUpdate tab={tab} loading={loadingUpdate} formik={formik} formikPassword={formikPassword} />
+      </div>
     </div>
   );
 };
