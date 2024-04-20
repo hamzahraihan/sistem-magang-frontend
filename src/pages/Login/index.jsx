958fb6a4ee1b5133af58bed8487b5a79920e8aba
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-20 11:59:42 +0700

    Update Login page to use Sidebar component for navigation

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom';
-import LoginMenu from './LoginMenu';
 import UserProvider from '../../context/UserContext';
 import { Toaster } from 'react-hot-toast';
+import Sidebar from './Sidebar';
 
 const index = () => {
   return (
     <div className="flex justify-center items-center p-10 bg-gray-50">
       <div className="grid lg:grid-cols-2 grid-cols-1 h-fit gap-10 w-[1000px]">
         <UserProvider>
-          <LoginMenu />
+          <Sidebar />
           <Outlet />
           <Toaster />
         </UserProvider>
