45a952e16fa0753f2eec35db3191cc87ab20bbbb
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-20 12:00:00 +0700

    Add Sidebar component for login page navigation

---
+++ src/pages/Login/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import InternshipLp from '../../assets/svg/Internship-bro.svg';
+import { useUserContext } from '../../hooks/useUserContext';
+import { useNavigate } from 'react-router-dom';
+
+const Sidebar = () => {
+  const { user, userLoggedInData } = useUserContext();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if ((user && user.length > 0) || userLoggedInData) {
+      navigate('/');
+    }
+  }, [navigate, user, userLoggedInData]);
+  return (
+    <div className="lg:flex flex-col gap-10 justify-center items-center border border-gray-300 h-full p-10 rounded-[64px] hidden bg-white">
+      <img className="w-80" src={InternshipLp} alt="" />
+      <div className="flex flex-col gap-2 text-center w-72 text-xs">
+        <p className="font-bold text-base">Magang Mandiri</p>
+        <p className="text-gray-400">Mulailah Berkontribusi pada Beragam Proyek yang Sesuai dengan Minatmu dan Berikan Dampak Nyata melalui program Magang Mandiri</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
