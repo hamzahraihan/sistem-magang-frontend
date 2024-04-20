2f841ba55559ecc89ad28042ebe941f7f1eaca8d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-20 12:00:08 +0700

    Update NotFound page to include footer with contact information

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -2,13 +2,21 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col gap-3 justify-center items-center h-screen p-2 text-center">
-      <h1 className="text-9xl font-bold">Oops!</h1>
-      <p className="text-base">Halaman yang kamu cari tidak ditemukan atau kamu belum login.</p>
-      <Link to="/" className="bg-primaryColor rounded-[32px] py-2 px-4 shadow-md hover:bg-hoverColor text-white transition-all active:bg-activeColor">
-        Kembali ke beranda
-      </Link>
-    </div>
+    <>
+      <div className="flex flex-col gap-3 justify-center items-center h-[95vh] p-2 text-center">
+        <h1 className="text-9xl font-bold">Oops!</h1>
+        <p className="text-base">Halaman yang kamu cari tidak ditemukan atau kamu belum login.</p>
+        <Link to="/" className="bg-primaryColor rounded-[32px] py-2 px-4 shadow-md hover:bg-hoverColor text-white transition-all active:bg-activeColor">
+          Kembali ke beranda
+        </Link>
+      </div>
+      <footer className="text-center">
+        {new Date().getFullYear()} &copy; hamzahraihan6@gmail.com{' '}
+        <a className="text-blue-700" href="https://github.com/HamzahRaihan">
+          github💖
+        </a>
+      </footer>
+    </>
   );
 };
 
