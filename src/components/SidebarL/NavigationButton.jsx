eda3671269574af66315cc483db71f15aa25e60e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-22 14:55:19 +0700

    Update NavigationButton component to include links for mahasiswa role

--- src/components/SidebarL/NavigationButton.jsx
+++ src/components/SidebarL/NavigationButton.jsx
@@ -1,5 +1,50 @@
 import { NavLink } from 'react-router-dom';
 import { DocumentIcon, HomeIcon, OfficeBrifcase, UploadIcon } from '../Icons';
+import { useUserContext } from '../../hooks/useUserContext';
+
+const NavigationMahasiswa = () => {
+  const { userLoggedInData } = useUserContext();
+  console.log('🚀 ~ NavigationButton ~ userLoggedInData:', userLoggedInData);
+  if (userLoggedInData?.role == 'mahasiswa' || !userLoggedInData) {
+    return (
+      <>
+        <NavLink
+          to="/kegiatan-magang"
+          className={({ isActive }) =>
+            isActive
+              ? 'flex items-center gap-2 p-4 bg-primaryColor active:bg-activeColor hover:bg-hoverColor text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
+              : 'flex items-center gap-2 p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
+          }
+        >
+          <OfficeBrifcase />
+          <p className="lg:flex hidden">Kegiatan Magang</p>
+        </NavLink>
+        <NavLink
+          to="/report"
+          className={({ isActive }) =>
+            isActive
+              ? 'flex items-center gap-2 p-4 bg-primaryColor active:bg-activeColor hover:bg-hoverColor text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
+              : 'flex items-center gap-2 p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
+          }
+        >
+          <UploadIcon />
+          <p className="lg:flex hidden">Upload Laporan</p>
+        </NavLink>
+        <NavLink
+          to="https://drive.google.com/drive/u/1/folders/13_CQ53O0eIKVCQsuSVlHAqvMFFQD7m6-"
+          className={({ isActive }) =>
+            isActive
+              ? 'flex items-center gap-2 p-4 bg-primaryColor active:bg-activeColor hover:bg-hoverColor text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
+              : 'flex items-center gap-2 p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
+          }
+        >
+          <DocumentIcon />
+          <p className="lg:flex hidden">Berkas Magang</p>
+        </NavLink>
+      </>
+    );
+  }
+};
 
 const NavigationButton = () => {
   return (
@@ -15,39 +60,8 @@ const NavigationButton = () => {
         <HomeIcon />
         <p className="lg:flex hidden">Beranda</p>
       </NavLink>
-      <NavLink
-        to="/kegiatan-magang"
-        className={({ isActive }) =>
-          isActive
-            ? 'flex items-center gap-2 p-4 bg-primaryColor active:bg-activeColor hover:bg-hoverColor text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
-            : 'flex items-center gap-2 p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
-        }
-      >
-        <OfficeBrifcase />
-        <p className="lg:flex hidden">Kegiatan Magang</p>
-      </NavLink>
-      <NavLink
-        to="/report"
-        className={({ isActive }) =>
-          isActive
-            ? 'flex items-center gap-2 p-4 bg-primaryColor active:bg-activeColor hover:bg-hoverColor text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
-            : 'flex items-center gap-2 p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
-        }
-      >
-        <UploadIcon />
-        <p className="lg:flex hidden">Upload Laporan</p>
-      </NavLink>
-      <NavLink
-        to="https://drive.google.com/drive/u/1/folders/13_CQ53O0eIKVCQsuSVlHAqvMFFQD7m6-"
-        className={({ isActive }) =>
-          isActive
-            ? 'flex items-center gap-2 p-4 bg-primaryColor active:bg-activeColor hover:bg-hoverColor text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
-            : 'flex items-center gap-2 p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit'
-        }
-      >
-        <DocumentIcon />
-        <p className="lg:flex hidden">Berkas Magang</p>
-      </NavLink>
+      {/* conditional rendering mahasiswa */}
+      <NavigationMahasiswa />
     </nav>
   );
 };
