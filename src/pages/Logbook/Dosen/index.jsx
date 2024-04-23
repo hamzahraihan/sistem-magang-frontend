e2588b9a55414bb3399ef4fec2b234aa43a19842
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-23 16:01:38 +0700

    Update LogbookDosen page and component to include navigation link and content

--- src/pages/Logbook/Dosen/index.jsx
+++ src/pages/Logbook/Dosen/index.jsx
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom';
+import { ArrowIcon } from '../../../components/Icons';
+
 // Logbook view for dosen role
 const LogbookDosen = () => {
-  return <div>LogbookDosen</div>;
+  return (
+    <div className="col-span-3 pb-10">
+      <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
+        <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
+          <ArrowIcon />
+        </Link>
+        <h1 className="text-xl font-bold">Logbook Mahasiswa</h1>
+        <p className="text-base font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates repellendus ipsa neque sit, facere accusamus minus magni ipsum voluptas consequuntur.</p>
+      </div>
+    </div>
+  );
 };
 
 export default LogbookDosen;
