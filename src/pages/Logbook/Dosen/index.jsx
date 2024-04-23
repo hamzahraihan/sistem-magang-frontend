0ff1220bb4a7f55cadce1c6b64e9f201ace35e37
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-23 16:10:18 +0700

    Update LogbookDosen page and component to include sidebar and adjust grid layout

--- src/pages/Logbook/Dosen/index.jsx
+++ src/pages/Logbook/Dosen/index.jsx
@@ -1,16 +1,20 @@
 import { Link } from 'react-router-dom';
 import { ArrowIcon } from '../../../components/Icons';
+import SidebarLogbookDosen from './Sidebar';
 
 // Logbook view for dosen role
 const LogbookDosen = () => {
   return (
     <div className="col-span-3 pb-10">
-      <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
-        <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
-          <ArrowIcon />
-        </Link>
-        <h1 className="text-xl font-bold">Logbook Mahasiswa</h1>
-        <p className="text-base font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates repellendus ipsa neque sit, facere accusamus minus magni ipsum voluptas consequuntur.</p>
+      <div className="grid grid-cols-3 gap-5">
+        <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
+          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+            <ArrowIcon />
+          </Link>
+          <h1 className="text-xl font-bold">Logbook Mahasiswa</h1>
+          <p className="text-base font-bold"></p>
+        </div>
+        <SidebarLogbookDosen />
       </div>
     </div>
   );
