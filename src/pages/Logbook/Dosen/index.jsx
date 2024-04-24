acca624d52e3f1ace6d6a5e54401497aa930266d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-24 13:46:30 +0700

    Add Table component to LogbookDosen page

--- src/pages/Logbook/Dosen/index.jsx
+++ src/pages/Logbook/Dosen/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { ArrowIcon } from '../../../components/Icons';
 import SidebarLogbookDosen from './Sidebar';
+import Table from './Table';
 
 // Logbook view for dosen role
 const LogbookDosen = () => {
@@ -8,11 +9,13 @@ const LogbookDosen = () => {
     <div className="col-span-3 pb-10">
       <div className="grid grid-cols-3 gap-5">
         <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
-          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
             <ArrowIcon />
           </Link>
           <h1 className="text-xl font-bold">Logbook Mahasiswa</h1>
-          <p className="text-base font-bold"></p>
+          <p className="text-base font-bold">
+            <Table />
+          </p>
         </div>
         <SidebarLogbookDosen />
       </div>
