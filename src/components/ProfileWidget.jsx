f85ad11de4eac5b51b163e8b1e6bc02d95c15fa7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-26 14:45:06 +0700

    Refactor ProfileWidget component in ProfileWidget.jsx to include conditional rendering for profile image

--- src/components/ProfileWidget.jsx
+++ src/components/ProfileWidget.jsx
@@ -9,7 +9,11 @@ import { getRoleId } from '../utils/getRoleId';
 const ProfileWidget = ({ data }) => {
   return (
     <Link to={`/profile/${data?.role}/${_.kebabCase(slugify(data))}`} state={{ userId: getRoleId(data) }} className="flex items-center gap-2 w-fit lg:hidden">
-      <Avvvatars value={pickRole(data)?.first_name + pickRole(data)?.last_name} displayValue={_.capitalize(pickRole(data)?.first_name[0]) + _.capitalize(pickRole(data)?.last_name[0])} size={40} on />
+      {!pickRole(data)?.image ? (
+        <Avvvatars value={pickRole(data)?.first_name + pickRole(data)?.last_name} displayValue={_.capitalize(pickRole(data)?.first_name[0]) + _.capitalize(pickRole(data)?.last_name[0])} size={40} />
+      ) : (
+        <img src={`https://drive.google.com/thumbnail?id=${pickRole(data)?.image}&sz=w1000`} className="h-10 w-10 object-cover object-center" alt="profile" />
+      )}
       <div className="flex flex-col">
         <p className="text-sm text-gray-600 ">{data?.author}</p>
         <p className="text-gray-400">{pickRole(data)?.email}</p>
