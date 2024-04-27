515cc966ef311ff99f17429f7b1b82adce2fbd79
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-27 12:38:39 +0700

    Refactor UserComment component in Post page to update styling and add dynamic height for textarea

--- src/pages/Post/UserComment.jsx
+++ src/pages/Post/UserComment.jsx
@@ -4,14 +4,12 @@ const UserComment = () => {
   const dateDummy = new Date();
 
   return (
-    <div>
-      <div className="flex gap-2">
-        <div className="h-10 w-10 bg-teal-500 animate-pulse rounded-full"></div>
-        <div className="p-2 bg-white flex-1 rounded-lg w-fit">
-          <p className="text-md font-bold">Hamzah</p>
-          <p className="text-xs pb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae repudiandae atque quis debitis quaerat aspernatur distinctio, consequatur tenetur! Sint, aspernatur!</p>
-          <p className="text-xs text-neutral-500">{formatDate(dateDummy)}</p>
-        </div>
+    <div className="flex gap-2 ">
+      <div className="h-10 w-10 bg-teal-500 animate-pulse rounded-full"></div>
+      <div className="p-2 shadow-sm bg-white flex-1 rounded-lg w-fit">
+        <p className="text-md font-bold">Hamzah</p>
+        <p className="text-xs pb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae repudiandae atque quis debitis quaerat aspernatur distinctio, consequatur tenetur! Sint, aspernatur!</p>
+        <p className="text-xs text-neutral-500">{formatDate(dateDummy)}</p>
       </div>
     </div>
   );
