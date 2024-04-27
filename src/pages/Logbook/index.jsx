37685f9cc5c16e54e4e8beb20bb0e7aef8af3683
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-27 12:38:03 +0700

    Refactor Logbook component in Logbook page to update Link styling and add background color

--- src/pages/Logbook/index.jsx
+++ src/pages/Logbook/index.jsx
@@ -16,7 +16,7 @@ const Logbook = () => {
     <div className="col-span-3 pb-10">
       <div className="grid grid-cols-3 gap-5">
         <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
-          <Link to={`/kegiatan-magang/detail/${internship_id}`} className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all">
+          <Link to={`/kegiatan-magang/detail/${internship_id}`} className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
             <ArrowIcon />
           </Link>
           <p className="text-base font-bold">
