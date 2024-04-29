dcefe18d25b659fe4ed29d70e252faf4e8c3a9c7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-29 15:10:45 +0700

    Add useFetchComment custom hook to fetch comments for a post

---
+++ src/features/comment/useFetchComment.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useMemo, useState } from 'react';
+import { useCommentContext, useCommentDispatch } from '../../hooks/useCommentContext';
+import { getCommentByPostAPI } from '../../constant/api';
+import { useLocation } from 'react-router-dom';
+
+const useFetchComment = () => {
+  const [loading, setLoading] = useState(false);
+  const { comment } = useCommentContext();
+  const dispatch = useCommentDispatch();
+
+  const { state } = useLocation();
+
+  const postId = useMemo(() => {
+    return state ? state.post_id : null;
+  }, [state]);
+
+  useEffect(() => {
+    const getCommentByPost = async () => {
+      setLoading(true);
+      try {
+        const data = await getCommentByPostAPI(postId);
+        dispatch({ type: 'SET_COMMENT', payload: data });
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    getCommentByPost();
+  }, [postId, dispatch]);
+  return { comment, loading };
+};
+
+export default useFetchComment;
