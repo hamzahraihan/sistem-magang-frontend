2e2a6dd05e1a5beabeeb87f39cee08b680e5f535
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-29 15:11:09 +0700

    Refactor CommentSection component in CommentSection.jsx to use useFetchComment hook and display comments dynamically

--- src/pages/Post/CommentSection.jsx
+++ src/pages/Post/CommentSection.jsx
@@ -1,15 +1,33 @@
+import { Spinner } from '../../components/Icons';
+import useFetchComment from '../../features/comment/useFetchComment';
 import CommentBar from './CommentBar';
 import UserComment from './UserComment';
 
+const Comment = () => {
+  const { comment } = useFetchComment();
+  console.log('🚀 ~ CommentSection ~ comment:', comment);
+  if (comment.length > 0) {
+    return (
+      <>
+        {comment.map((item) => (
+          <UserComment key={item.comment_id} comment={item} />
+        ))}
+      </>
+    );
+  }
+  if (comment.length === 0) {
+    return <div className="flex items-center justify-center h-44 text-sm w-full text-gray-400">Jadi yang pertama berkomentar</div>;
+  }
+};
+
 const CommentSection = () => {
+  const { loading } = useFetchComment();
+
   return (
     <div className="flex flex-col gap-5 pb-10">
       <h1 className="font-bold text-xl">Komentar</h1>
       <CommentBar />
-      <UserComment />
-      <UserComment />
-      <UserComment />
-      <UserComment />
+      {loading ? <Spinner /> : <Comment />}
     </div>
   );
 };
