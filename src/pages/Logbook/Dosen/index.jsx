167e8a563afcc73d05eaff40de7d2c74a657a3e4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-29 15:10:58 +0700

    Update LogbookDosen component in index.jsx to remove SidebarLogbookDosen and adjust grid layout

--- src/pages/Logbook/Dosen/index.jsx
+++ src/pages/Logbook/Dosen/index.jsx
@@ -1,21 +1,17 @@
 import { Link } from 'react-router-dom';
 import { ArrowIcon } from '../../../components/Icons';
-import SidebarLogbookDosen from './Sidebar';
 import Table from './Table';
 
 // Logbook view for dosen role
 const LogbookDosen = () => {
   return (
     <div className="col-span-3 pb-10">
-      <div className="grid grid-cols-3 gap-5">
-        <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
-          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
-            <ArrowIcon />
-          </Link>
-          <h1 className="text-xl font-bold">Logbook Mahasiswa</h1>
-          <Table />
-        </div>
-        <SidebarLogbookDosen />
+      <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
+        <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
+          <ArrowIcon />
+        </Link>
+        <h1 className="text-xl font-bold">Logbook Mahasiswa</h1>
+        <Table />
       </div>
     </div>
   );
