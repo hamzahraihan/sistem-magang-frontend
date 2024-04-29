12433af395b68e72f88626d965ec3b628557c27e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-29 15:10:23 +0700

    Add CommentProvider to App component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,31 @@ import LogbookWeeklyActivityProvider from './context/LogbookWeelyActivityContext
 import { Toaster } from 'react-hot-toast';
 import ReportInternProvider from './context/ReportInternContext.jsx';
 import BottomNavMobile from './components/BottomNavMobile/index.jsx';
+import CommentProvider from './context/CommentContext.jsx';
 
 function App() {
   return (
     <div className="grid text-xs lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:gap-5 md:gap-2 sm:gap-2 gap-2 pt-5 px-3 lg:px-12 md:px-2 sm:px-2 relative lg:mb-0 md:mb-0 sm:mb-0 mb-14 bg-gray-50">
       <UserProvider>
         <PostProvider>
-          <LogbookProvider>
-            <LogbookWeeklyProvider>
-              <LogbookDailyProvider>
-                <LogbookWeeklyActivityProvider>
-                  <InternshipProvider>
-                    <ReportInternProvider>
-                      <SidebarL />
-                      <Outlet />
-                      <Toaster position="top-center" />
-                      <BottomNavMobile />
-                    </ReportInternProvider>
-                  </InternshipProvider>
-                </LogbookWeeklyActivityProvider>
-              </LogbookDailyProvider>
-            </LogbookWeeklyProvider>
-          </LogbookProvider>
+          <CommentProvider>
+            <LogbookProvider>
+              <LogbookWeeklyProvider>
+                <LogbookDailyProvider>
+                  <LogbookWeeklyActivityProvider>
+                    <InternshipProvider>
+                      <ReportInternProvider>
+                        <SidebarL />
+                        <Outlet />
+                        <Toaster position="top-center" />
+                        <BottomNavMobile />
+                      </ReportInternProvider>
+                    </InternshipProvider>
+                  </LogbookWeeklyActivityProvider>
+                </LogbookDailyProvider>
+              </LogbookWeeklyProvider>
+            </LogbookProvider>
+          </CommentProvider>
         </PostProvider>
       </UserProvider>
     </div>
