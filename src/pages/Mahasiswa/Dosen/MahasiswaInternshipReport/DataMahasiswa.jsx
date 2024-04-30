68169ddc5247278c26721f876d98bee18d1cd7e3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-30 13:56:41 +0700

    Add DataMahasiswa component to display student data in MahasiswaInternshipReport

---
+++ src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/DataMahasiswa.jsx
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+
+const DataMahasiswa = () => {
+  return (
+    <div className="bg-white rounded-xl w-full col-span-3 border border-neutral-200 p-5 ">
+      <h1 className="text-xl font-bold">Data Mahasiswa</h1>
+      <div className="grid grid-cols-2 gap-4 mt-10">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <h1 className="text-sm font-bold text-gray-300">Nama</h1>
+            <p className="text-sm font-bold text-gray-500">{_.upperCase('hamzah raihan')}</p>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-sm font-bold text-gray-300">Tempat Magang</h1>
+            <p className="text-sm font-bold text-gray-500">{_.upperCase('PT. Indorama Sejahtera')}</p>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-sm font-bold text-gray-300">Email Mahasiswa</h1>
+            <p className="text-sm font-bold text-gray-500">hamzah@gmail.com</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <h1 className="text-sm font-bold text-gray-300">NIM</h1>
+            <p className="text-sm font-bold text-gray-500">012908301923</p>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-sm font-bold text-gray-300">Angkatan</h1>
+            <p className="text-sm font-bold text-gray-500">2020</p>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-sm font-bold text-gray-300">Jurusan</h1>
+            <p className="text-sm font-bold text-gray-500">{_.upperCase('sistem magang')}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DataMahasiswa;
