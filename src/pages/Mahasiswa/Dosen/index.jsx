681a11dd4a747e9923e3b0721462dc64fd2700d1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-04-30 13:56:29 +0700

    Update Mahasiswa page to display user's full name in Daftar Mahasiswa title

--- src/pages/Mahasiswa/Dosen/index.jsx
+++ src/pages/Mahasiswa/Dosen/index.jsx
@@ -1,16 +1,23 @@
 import { Link } from 'react-router-dom';
 import { ArrowIcon } from '../../../components/Icons';
 import Table from './Table';
+import { useUserContext } from '../../../hooks/useUserContext';
 
 // Logbook view for dosen role
 const Mahasiswa = () => {
+  const { userLoggedInData } = useUserContext();
   return (
     <div className="col-span-3 pb-10">
       <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
         <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
           <ArrowIcon />
         </Link>
-        <h1 className="text-xl font-bold">Logbook Mahasiswa</h1>
+        <div className="flex flex-col">
+          <h1 className="text-xl font-bold">Daftar Mahasiswa</h1>
+          <p className="text-sm">
+            Dosen wali: {userLoggedInData?.first_name} {userLoggedInData?.last_name}
+          </p>
+        </div>
         <Table />
       </div>
     </div>
