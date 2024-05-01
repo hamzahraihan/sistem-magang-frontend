93262dba4bc8689c1734c2c5c58709afe1da0d2b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-01 13:59:44 +0700

    Add SearchTableInput component to handle table search functionality in Table component

---
+++ src/components/Table/SearchTableInput.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const SearchTableInput = ({ value: initValue, onChange, debounce = 200 }) => {
+  const [value, setValue] = useState(initValue);
+  useEffect(() => {
+    setValue(initValue);
+  }, [initValue]);
+
+  // * 0.2 after set value in state
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+    return () => clearTimeout(timeout);
+  }, [value]);
+
+  return <input className="py-2 bg-transparent outline-none border-b-2 border-gray-300 w-1/2 focus:w-full duration-150" placeholder="Search" value={value} onChange={(e) => setValue(e.target.value)} />;
+};
+
+SearchTableInput.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  debounce: PropTypes.number,
+};
+
+export default SearchTableInput;
