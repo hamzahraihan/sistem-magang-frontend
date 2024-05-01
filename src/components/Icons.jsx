8530753b4563cc158c3fc4471133e7a239f0014d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-01 13:59:30 +0700

    Add SortingIcon component to display sorting icon in Icons.jsx

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -209,4 +209,12 @@ export const EditIcon = () => {
   );
 };
 
+export const SortingIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+    </svg>
+  );
+};
+
 export default HomeIcon;
