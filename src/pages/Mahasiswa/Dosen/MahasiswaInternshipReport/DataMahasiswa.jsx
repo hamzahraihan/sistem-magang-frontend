70506617931a627e4f1e70ed38d7a4435a5a8292
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-02 08:35:15 +0700

    Refactor Mahasiswa/Dosen page to update DataMahasiswa component in DataMahasiswa.jsx

--- src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/DataMahasiswa.jsx
+++ src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/DataMahasiswa.jsx
@@ -1,6 +1,11 @@
 import _ from 'lodash';
+import useFetchUserByID from '../../../../features/user/useFetchUserById';
 
 const DataMahasiswa = () => {
+  const { loading, userByID } = useFetchUserByID();
+  const fullName = {
+    value: loading ? <p className="bg-gray-500 h-5 animate-pulse rounded-md w-full"></p> : _.upperCase(` ${userByID.first_name} ${userByID.last_name}`),
+  };
   return (
     <div className="bg-white rounded-xl w-full col-span-3 border border-neutral-200 p-5 ">
       <h1 className="text-xl font-bold">Data Mahasiswa</h1>
@@ -8,29 +13,29 @@ const DataMahasiswa = () => {
         <div className="flex flex-col gap-4">
           <div className="flex flex-col">
             <h1 className="text-sm font-bold text-gray-300">Nama</h1>
-            <p className="text-sm font-bold text-gray-500">{_.upperCase('hamzah raihan')}</p>
+            <p className="text-sm font-bold text-gray-500">{fullName.value}</p>
           </div>
           <div className="flex flex-col">
             <h1 className="text-sm font-bold text-gray-300">Nomor Whatsapp</h1>
-            <p className="text-sm font-bold text-gray-500">089123123</p>
+            {loading ? <p className="bg-gray-500 h-5 animate-pulse rounded-md w-full"></p> : <p className="text-sm font-bold text-gray-500">{userByID.phone}</p>}
           </div>
           <div className="flex flex-col">
             <h1 className="text-sm font-bold text-gray-300">Email Mahasiswa</h1>
-            <p className="text-sm font-bold text-gray-500">hamzah@gmail.com</p>
+            {loading ? <p className="bg-gray-500 h-5 animate-pulse rounded-md w-full"></p> : <p className="text-sm font-bold text-gray-500">{userByID.email}</p>}
           </div>
         </div>
         <div className="flex flex-col gap-4">
           <div className="flex flex-col">
             <h1 className="text-sm font-bold text-gray-300">NIM</h1>
-            <p className="text-sm font-bold text-gray-500">012908301923</p>
+            {loading ? <p className="bg-gray-500 h-5 animate-pulse rounded-md w-full"></p> : <p className="text-sm font-bold text-gray-500">{userByID.nim}</p>}
           </div>
           <div className="flex flex-col">
             <h1 className="text-sm font-bold text-gray-300">Angkatan</h1>
-            <p className="text-sm font-bold text-gray-500">2020</p>
+            {loading ? <p className="bg-gray-500 h-5 animate-pulse rounded-md w-full"></p> : <p className="text-sm font-bold text-gray-500">{userByID.angkatan}</p>}
           </div>
           <div className="flex flex-col">
             <h1 className="text-sm font-bold text-gray-300">Jurusan</h1>
-            <p className="text-sm font-bold text-gray-500">{_.upperCase('sistem magang')}</p>
+            {loading ? <p className="bg-gray-500 h-5 animate-pulse rounded-md w-full"></p> : <p className="text-sm font-bold text-gray-500">{_.upperCase(userByID.jurusan)}</p>}
           </div>
         </div>
       </div>
