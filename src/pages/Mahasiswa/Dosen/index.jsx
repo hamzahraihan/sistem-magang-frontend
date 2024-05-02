1f2befb4894c9505435ef0a34dfa2f994341627b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-02 08:35:10 +0700

    Refactor Mahasiswa/Dosen page to update column headers and add Logbook column

--- src/pages/Mahasiswa/Dosen/index.jsx
+++ src/pages/Mahasiswa/Dosen/index.jsx
@@ -14,27 +14,32 @@ const Mahasiswa = () => {
 
   const columns = [
     columnHelper.accessor('', {
-      id: 'S.No',
+      id: 'No.',
       cell: (info) => info.row.index + 1,
       header: () => <span className="ps-2">No.</span>,
     }),
     columnHelper.accessor('first_name', {
+      id: 'Nama Depan',
       cell: (info) => info.getValue(),
       header: 'Nama depan',
     }),
     columnHelper.accessor('last_name', {
+      id: 'Nama Belakang',
       cell: (info) => info.getValue(),
       header: 'Nama Belakang',
     }),
     columnHelper.accessor('nim', {
+      id: 'NIM',
       cell: (info) => info.getValue(),
       header: 'NIM',
     }),
     columnHelper.accessor('angkatan', {
+      id: 'Angkatan',
       cell: (info) => <div className="rounded-lg p-2 text-center w-fit m-auto ">{info.getValue()}</div>,
       header: <span className="w-full text-center">Angkatan</span>,
     }),
     columnHelper.accessor('mahasiswa_id', {
+      id: 'Logbook',
       cell: (info) => (
         <Link to={`logbook-mahasiswa/${info.getValue()}`} className="flex p-2 bg-gray-300 rounded-lg m-auto text-center w-fit hover:bg-gray-200 active:bg-gray-400 duration-150">
           Lihat detail
@@ -45,8 +50,8 @@ const Mahasiswa = () => {
   ];
 
   const filteredData = mahasiswaDosen.map((item) => {
-    const { first_name, last_name, email, role, nim, jurusan, angkatan, status, kelas, phone } = item;
-    return { first_name, last_name, email, role, nim, jurusan, angkatan, status, kelas, phone };
+    const { mahasiswa_id, first_name, last_name, email, role, nim, jurusan, angkatan, status, kelas, phone } = item;
+    return { mahasiswa_id, first_name, last_name, email, role, nim, jurusan, angkatan, status, kelas, phone };
   });
 
   return (
@@ -55,13 +60,15 @@ const Mahasiswa = () => {
         <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white mb-2">
           <ArrowIcon />
         </Link>
-        <div className="flex flex-col mb-2">
-          <h1 className="text-xl font-bold">Daftar Mahasiswa</h1>
-          <p className="text-sm">
-            Dosen wali: {userLoggedInData?.first_name} {userLoggedInData?.last_name}
-          </p>
+        <div className="bg-white rounded-xl border border-gray-200 p-5">
+          <div className="flex flex-col mb-2">
+            <h1 className="text-xl font-bold">Daftar Mahasiswa</h1>
+            <p className="text-sm">
+              Dosen wali: {userLoggedInData?.first_name} {userLoggedInData?.last_name}
+            </p>
+          </div>
+          <Table columns={columns} data={filteredData} loading={loading} fileName={'laporan_mahasiswa_magang_mandiri'} />
         </div>
-        <Table columns={columns} data={filteredData} loading={loading} fileName={'laporan_mahasiswa_magang_mandiri'} />
       </div>
     </div>
   );
