4f316427a62731cc2f32b8dfc6bc8ec525952c56
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-03 12:46:56 +0700

    Refactor MahasiswaInternshipReport component to add InternshipWeeklyReport component

---
+++ src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/InternshipWeeklyReport/index.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import useFetchInternshipById from '../../../../../features/internship/useFetchInternshipById';
+import { ArrowIcon } from '../../../../../components/Icons';
+import DataLogbook from './DataLogbook';
+
+const InternshipWeeklyReport = () => {
+  const { loading, internshipByID } = useFetchInternshipById();
+  console.log('🚀 ~ InternshipWeeklyReport ~ internshipByID:', internshipByID);
+
+  return (
+    <div className="flex flex-col col-span-3 gap-4">
+      <Link
+        to={`/dashboard/dosen/mahasiswa/logbook-mahasiswa/${internshipByID.mahasiswa_id}`}
+        className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white"
+      >
+        <ArrowIcon />
+      </Link>
+      <DataLogbook data={internshipByID} loading={loading} />
+    </div>
+  );
+};
+
+export default InternshipWeeklyReport;
