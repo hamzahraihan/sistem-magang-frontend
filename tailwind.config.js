8a1ab949904880bae03a015d052bd75be94b73cc
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-05 11:35:56 +0700

    Refactor tailwind.config.js to include flowbite/**/*.js in content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/lib/esm/**/*.js'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/lib/esm/**/*.js', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {
       colors: {
