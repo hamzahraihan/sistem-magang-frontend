6b3b072dad5187a4c84af2e0d1e49798442a3aa8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-05 12:06:20 +0700

    Refactor MahasiswaInternshipReport component to include min-w-full class

--- src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/index.jsx
+++ src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/index.jsx
@@ -5,7 +5,7 @@ import { ArrowIcon } from '../../../../components/Icons';
 
 const MahasiswaInternshipReport = () => {
   return (
-    <div className="flex flex-col col-span-3 gap-4">
+    <div className="flex flex-col col-span-3 gap-4 min-w-full">
       <Link to="/dashboard/dosen/mahasiswa" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
         <ArrowIcon />
       </Link>
