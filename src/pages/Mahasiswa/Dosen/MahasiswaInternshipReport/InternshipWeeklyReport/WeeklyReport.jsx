67824fd3446fdffc3121ba644d5afee4d8a345ad
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-07 12:52:37 +0700

    Refactor WeeklyReport component to fetch user by ID and display logbook data

--- src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/InternshipWeeklyReport/WeeklyReport.jsx
+++ src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/InternshipWeeklyReport/WeeklyReport.jsx
@@ -2,12 +2,14 @@ import { createColumnHelper } from '@tanstack/react-table';
 import Table from '../../../../../components/Table/Table';
 import useFetchWeeklyLogbook from '../../../../../features/logbook/useFetchWeeklyLogbook';
 import { Link } from 'react-router-dom';
+import useFetchUserByID from '../../../../../features/user/useFetchUserById';
 
 const WeeklyReport = () => {
   const { loading, logbookWeekly } = useFetchWeeklyLogbook();
   console.log('🚀 ~ WeeklyReport ~ logbookWeekly:', logbookWeekly);
   const columnHelper = createColumnHelper();
 
+  const { userByID } = useFetchUserByID(logbookWeekly[0]?.mahasiswa_id);
   const columns = [
     columnHelper.accessor('', {
       id: 'Minggu',
@@ -54,21 +56,34 @@ const WeeklyReport = () => {
 
       header: <span className="w-full text-center">Status</span>,
     }),
-    columnHelper.accessor('internship_id', {
-      id: 'Logbook',
+    columnHelper.accessor('logbook_id', {
+      id: 'Aksi',
       cell: (info) => (
-        <Link to={`/dashboard/dosen/mahasiswa/internship-report/${info.getValue()}`} className="flex p-2 bg-gray-300 rounded-lg m-auto text-center w-fit hover:bg-gray-200 active:bg-gray-400 duration-150">
+        <Link to={`/dashboard/dosen/mahasiswa/logbook/${info.getValue()}`} className="flex p-2 bg-gray-200 rounded-lg m-auto text-center w-fit hover:bg-gray-300 active:bg-gray-400 duration-150">
           Lihat detail
         </Link>
       ),
       size: 50,
-      header: <span className="w-full text-center">Logbook</span>,
+      header: <span className="w-full text-center">Aksi</span>,
     }),
   ];
+
+  const filteredData = logbookWeekly.map((item, index) => {
+    const { log_description, Logdailies, status, logbook_id } = item;
+
+    const data = {
+      minggu: index + 1,
+      log_description,
+      Logdailies,
+      status,
+      logbook_id,
+    };
+    return data;
+  });
   return (
     <div className="bg-white rounded-xl w-full col-span-3 border border-neutral-200 p-5 mb-10">
       <h1 className="text-xl font-bold mb-10">Laporan Mingguan Mahasiswa</h1>
-      <Table columns={columns} data={logbookWeekly} loading={loading} fileName={`laporan_mingguan_mahasiswa}`} />
+      <Table columns={columns} data={filteredData} loading={loading} fileName={`${userByID.first_name}_${userByID.last_name}_laporan_mingguan_mahasiswa}`} />
     </div>
   );
 };
