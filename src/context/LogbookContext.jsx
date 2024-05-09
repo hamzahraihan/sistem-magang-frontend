41f37fdd6de799c22201ae5adf38010f2bf829b1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-09 14:18:30 +0700

    refactor: Update LogbookContext to handle logbook status updates

--- src/context/LogbookContext.jsx
+++ src/context/LogbookContext.jsx
@@ -1,5 +1,8 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useUserContext } from '../hooks/useUserContext';
 
 export const LogbookContext = createContext(null);
 
@@ -7,9 +10,37 @@ export const LogbookDispatch = createContext(null);
 
 const LogbookProvider = ({ children }) => {
   const [logbook, dispatch] = useReducer(LogbookReducer, []);
+  const [loading, setLoading] = useState(false);
+  const { accessToken } = useUserContext();
+  const { logbook_id } = useParams();
+
+  const handleStatusLogbook = async ({ lecturer_note, status }) => {
+    console.log('🚀 ~ handleStatusLogbook ~ lecturer_note:', lecturer_note);
+    console.log('🚀 ~ handleStatusLogbook ~ status:', status);
+    setLoading(true);
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_BASE_URL}/logbook/weekly/edit/${logbook_id}`,
+        { status, lecturer_note },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      if (response.status === 201) {
+        alert('ok');
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
 
   return (
-    <LogbookContext.Provider value={{ logbook }}>
+    <LogbookContext.Provider value={{ loading, logbook, handleStatusLogbook }}>
       <LogbookDispatch.Provider value={dispatch}>{children}</LogbookDispatch.Provider>
     </LogbookContext.Provider>
   );
