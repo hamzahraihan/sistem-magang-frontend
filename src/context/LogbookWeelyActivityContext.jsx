b0d53a8f20928ed299a523afd1e3e87b5aa93ee0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-10 14:08:16 +0700

    refactor: Update LogbookWeeklyActivityProvider to handle logbook status updates

--- src/context/LogbookWeelyActivityContext.jsx
+++ src/context/LogbookWeelyActivityContext.jsx
@@ -2,6 +2,9 @@ import { createContext, useReducer, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { TOKEN } from '../constant/key';
+import { useParams } from 'react-router-dom';
+import { useUserContext } from '../hooks/useUserContext';
+import toast from 'react-hot-toast';
 
 export const LogbookWeeklyActivityContext = createContext(null);
 
@@ -9,8 +12,12 @@ export const LogbookWeeklyActivityDispatch = createContext(null);
 
 const LogbookWeeklyActivityProvider = ({ children }) => {
   const [weeklyActivity, dispatch] = useReducer(LogbookWeeklyActivityReducer, []);
+  console.log('🚀 ~ LogbookWeeklyActivityProvider ~ weeklyActivity:', weeklyActivity);
   const [loadingUpdate, setLoadingUpdate] = useState(false);
   const token = localStorage.getItem(TOKEN);
+  const [loading, setLoading] = useState(false);
+  const { accessToken } = useUserContext();
+  const { logbook_id } = useParams();
 
   const handleUpdateWeeklyLog = async ({ logbook_id, log_description, status }) => {
     setLoadingUpdate(true);
@@ -37,8 +44,34 @@ const LogbookWeeklyActivityProvider = ({ children }) => {
     }
   };
 
+  const handleStatusLogbook = async ({ lecturer_note, status }) => {
+    setLoadingUpdate(true);
+    try {
+      const { data } = await axios.put(
+        `${import.meta.env.VITE_BASE_URL}/logbook/weekly/edit/${logbook_id}`,
+        { status, lecturer_note },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      console.log('🚀 ~ handleStatusLogbook ~ data:', data);
+      dispatch({ type: 'EDIT_WEEK_ACTIVITY', payload: data.result });
+      toast.success('Berhasil diubah');
+      setLoadingUpdate(false);
+    } catch (error) {
+      if (error.response.status === 400) {
+        console.error(error);
+        toast.error('Gagal diubah');
+      }
+      setLoading(false);
+    }
+  };
+
   return (
-    <LogbookWeeklyActivityContext.Provider value={{ weeklyActivity, handleUpdateWeeklyLog, loadingUpdate }}>
+    <LogbookWeeklyActivityContext.Provider value={{ loading, weeklyActivity, handleUpdateWeeklyLog, loadingUpdate, handleStatusLogbook }}>
       <LogbookWeeklyActivityDispatch.Provider value={dispatch}>{children}</LogbookWeeklyActivityDispatch.Provider>
     </LogbookWeeklyActivityContext.Provider>
   );
@@ -48,10 +81,10 @@ const LogbookWeeklyActivityReducer = (weeklyActivity, action) => {
   switch (action.type) {
     case 'SET_WEEK_ACTIVITY':
       return action.payload;
-    case 'ADD_WEEK_ACTIVITY':
-      return action.payload;
     case 'EDIT_WEEK_ACTIVITY':
       return action.payload;
+    default:
+      break;
   }
 };
 LogbookWeeklyActivityProvider.propTypes = {
