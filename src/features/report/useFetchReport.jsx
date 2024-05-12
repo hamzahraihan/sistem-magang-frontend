207687b8ec1113fa7f7e55896a8e2f10d8948fc0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-12 08:53:29 +0700

    refactor: Add useFetchReport hook to fetch report data

---
+++ src/features/report/useFetchReport.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useReportInternContext, useReportInternDispatch } from '../../hooks/useReportInternContext';
+import { getAllReportAPI } from '../../constant/api';
+import { useUserContext } from '../../hooks/useUserContext';
+
+const useFetchReport = () => {
+  const [loading, setLoading] = useState();
+  const dispatch = useReportInternDispatch();
+  const { reportIntern } = useReportInternContext();
+  const { accessToken } = useUserContext();
+  useEffect(() => {
+    const getAllReport = async () => {
+      setLoading(true);
+      try {
+        const data = await getAllReportAPI(accessToken);
+        dispatch({ type: 'SET_REPORT_DATA', payload: data });
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getAllReport();
+  }, [dispatch, accessToken]);
+
+  return { loading, reportIntern };
+};
+
+export default useFetchReport;
