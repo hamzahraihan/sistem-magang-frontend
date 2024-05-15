6648bb967d00712ad615a9a083c1e69d1590f0b8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-15 13:48:14 +0700

    feat: Update link in SidebarReport to point to /report page

--- src/pages/Internship/DetailInternship/index.jsx
+++ src/pages/Internship/DetailInternship/index.jsx
@@ -83,7 +83,7 @@ const DetailInternship = () => {
               <p className="flex flex-1 font-bold text-base ">Logbook</p>
               <ArrowIcon />
             </Link>
-            {reportIntern.length === 0 ? (
+            {reportIntern.length === 1 ? (
               <Link
                 to={`/report/upload/${internshipByID?.internship_id}`}
                 state={{ internshipID: internshipByID.internship_id }}
