172fb6b993eea1dbcf79a1125eecfd1ed1016d75
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-19 13:09:23 +0700

    refactor: Update ReportUpdateForm component to include report_id parameter in URL and improve file upload validation

--- src/pages/Report/Update/index.jsx
+++ src/pages/Report/Update/index.jsx
@@ -1,12 +1,12 @@
 import { Link, useParams } from 'react-router-dom';
 import { ArrowIcon } from '../../../components/Icons';
-import SidebarUploadReport from '../Upload/SidebarUploadReport';
 import FormUpdateReport from './FormUpdateReport';
 import * as yup from 'yup';
 import { useReportInternContext } from '../../../hooks/useReportInternContext';
 import { useFormik } from 'formik';
-import _ from 'lodash';
 import useFetchReportById from '../../../features/report/useFetchReportById';
+import { useEffect } from 'react';
+import SidebarUpdateReport from './SidebarUpdateReport';
 
 const ReportUpdateForm = () => {
   const { handleFileUpdate } = useReportInternContext();
@@ -27,9 +27,18 @@ const ReportUpdateForm = () => {
 
   const MAX_FILE_SIZE = 10485760; //max 10 mb file size
 
+  useEffect(() => {
+    formik.setValues({
+      title: reportIntern.title,
+      note: reportIntern.note,
+      intern_complete_file: reportIntern.intern_complete_file,
+      intern_score_file: reportIntern.intern_score_file,
+      intern_final_report: reportIntern.intern_final_report,
+    });
+  }, [reportIntern]);
+
   const formik = useFormik({
     initialValues: {
-      report_id: _.toInteger(report_id),
       title: '',
       note: '',
       intern_complete_file: null,
@@ -40,7 +49,6 @@ const ReportUpdateForm = () => {
       handleFileUpdate(values);
     },
     validationSchema: yup.object().shape({
-      report_id: yup.number().required(),
       title: yup.string().required('Wajib diisi'),
       note: yup.string().required('Wajib diisi'),
       intern_complete_file: yup
@@ -50,7 +58,10 @@ const ReportUpdateForm = () => {
           if (value == reportIntern.intern_complete_file) return true;
           return isValidFileType(value?.name?.toLowerCase());
         })
-        .test('is-valid-size', 'Max allowed size is 10 mb', (value) => value?.size <= MAX_FILE_SIZE),
+        .test('is-valid-size', 'Max allowed size is 10 mb', (value) => {
+          if (value == reportIntern.intern_complete_file) return true;
+          return value?.size <= MAX_FILE_SIZE;
+        }),
       intern_score_file: yup
         .mixed()
         .required('Wajib diisi')
@@ -58,20 +69,27 @@ const ReportUpdateForm = () => {
           if (value == reportIntern.intern_score_file) return true;
           return isValidFileType(value?.name?.toLowerCase());
         })
-        .test('is-valid-size', 'Max allowed size is 10 mb', (value) => value?.size <= MAX_FILE_SIZE),
+        .test('is-valid-size', 'Max allowed size is 10 mb', (value) => {
+          if (value == reportIntern.intern_score_file) return true;
+          return value?.size <= MAX_FILE_SIZE;
+        }),
       intern_final_report: yup
         .mixed()
         .required('Wajib diisi')
         .test('is-valid-type', `Format file salah, file harus berformat .${allowedExt}`, (value) => {
-          if (value == reportIntern.intern_score_file) return true;
+          if (value == reportIntern.intern_final_report) return true;
           return isValidFileType(value?.name?.toLowerCase());
         })
-        .test('is-valid-size', 'Max allowed size is 10 mb', (value) => value?.size <= MAX_FILE_SIZE),
+        .test('is-valid-size', 'Max allowed size is 10 mb', (value) => {
+          if (value == reportIntern.intern_final_report) return true;
+          return value?.size <= MAX_FILE_SIZE;
+        }),
     }),
   });
+
   return (
     <div className="col-span-3 pb-10">
-      <form className="grid grid-cols-3 gap-5">
+      <form className="grid grid-cols-3 gap-5" onSubmit={formik.handleSubmit}>
         <div className="lg:order-first flex flex-col gap-4 lg:col-span-2 col-span-3 order-last">
           <Link to={`/report/detail/${report_id}`} className="lg:flex hidden items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
             <ArrowIcon />
@@ -79,7 +97,7 @@ const ReportUpdateForm = () => {
           <h1 className="text-xl font-bold">Ubah laporan</h1>
           <FormUpdateReport formik={formik} getAllowedExt={getAllowedExt} />
         </div>
-        <SidebarUploadReport />
+        <SidebarUpdateReport />
       </form>
     </div>
   );
