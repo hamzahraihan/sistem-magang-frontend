2fb0db2c21e54b65640ce09458db25af53928fc7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-20 15:22:36 +0700

    refactor: Improve DataLogbook component layout and add file upload functionality

--- src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/InternshipWeeklyReport/DataLogbook.jsx
+++ src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/InternshipWeeklyReport/DataLogbook.jsx
@@ -3,6 +3,7 @@ import { formatDate } from '../../../../../utils/formatDate';
 import { Button } from 'flowbite-react';
 import { useState } from 'react';
 import ModalInternshipDocs from '../../../../../components/ModalInternshipDocument/ModalInternshipDocs';
+import { FileIcon } from '../../../../../components/Icons';
 
 const DataLogbook = ({ data, loading }) => {
   const [openModal, setOpenModal] = useState(false);
@@ -12,9 +13,32 @@ const DataLogbook = ({ data, loading }) => {
     setOpenModal(true);
     setModalType(type);
   };
+
+  let statusColor;
+  let statusText;
+  switch (data.status) {
+    case 'Belum diterima':
+      statusColor = 'bg-gray-300';
+      statusText = 'Belum diterima';
+      break;
+    case 'Disetujui':
+      statusColor = 'bg-green-500 text-white';
+      statusText = 'Sudah disetujui';
+      break;
+    case 'Tidak disetujui':
+      statusColor = 'bg-red-500 text-white';
+      statusText = 'Tidak disetujui';
+      break;
+  }
+
   return (
     <div className="bg-white rounded-xl w-full col-span-3 border border-neutral-200 p-5 ">
-      <h1 className="text-xl font-bold">Data Logbook</h1>
+      <div className="flex justify-between">
+        <h1 className="text-xl font-bold">Data Logbook</h1>
+        <div className="flex flex-col">
+          <div className={`p-3 rounded-xl font-bold ${statusColor}`}>{statusText}</div>
+        </div>
+      </div>
       <div className="grid grid-cols-2 gap-4 mt-10">
         <div className="flex flex-col gap-4">
           <div className="flex flex-col">
@@ -51,6 +75,7 @@ const DataLogbook = ({ data, loading }) => {
           </div>
         </div>
 
+        <h1 className="text-sm text-gray-400">Dokumentasi Magang Mandiri</h1>
         <Button color={null} className="border border-gray-400 hover:bg-hoverColor active:bg-primaryColor hover:text-white bg-white col-span-2" onClick={() => handleOpenModal('lecture_docu')}>
           Surat bersedia dosen magang
         </Button>
@@ -62,6 +87,45 @@ const DataLogbook = ({ data, loading }) => {
         <Button color={null} className="border border-gray-400 hover:bg-hoverColor active:bg-primaryColor hover:text-white bg-white col-span-2" onClick={() => handleOpenModal('instance_docu')}>
           Surat magang dari perusahaan
         </Button>
+        <div className="flex flex-col">
+          <div className="flex items-baseline gap-2">
+            <p className="text-sm text-gray-500 font-bold">Surat Selesai Magang:</p>
+            <button className="w-fit bg-primaryColor text-white p-2 rounded-xl hover:bg-hoverColor duration-150 " type="button" onClick={() => handleOpenModal('intern_complete_file')}>
+              <FileIcon />
+            </button>
+          </div>
+
+          <div className="flex items-baseline gap-2">
+            <p className="text-sm text-gray-500 font-bold"> Laporan akhir magang:</p>
+            <button className="w-fit bg-primaryColor text-white p-2 rounded-xl hover:bg-hoverColor duration-150 " type="button" onClick={() => handleOpenModal('intern_final_report')}>
+              <FileIcon />
+            </button>
+          </div>
+
+          <div className="flex items-baseline gap-2">
+            <p className="text-sm text-gray-500 font-bold">Penilaian dari perusahaan:</p>
+            <button className="w-fit bg-primaryColor text-white p-2 rounded-xl hover:bg-hoverColor duration-150 " type="button" onClick={() => handleOpenModal('intern_score_file')}>
+              <FileIcon />
+            </button>
+          </div>
+        </div>
+
+        <form className="flex flex-col col-span-2">
+          <label htmlFor="lecturer_note" className="text-sm text-gray-400">
+            Catatan <span className="text-red-600">*Wajib diisi bila dokumentasi magang tidak valid</span>
+          </label>
+          <textarea id="lecturer_note" name="lecturer_note" className="rounded-lg bg-gray-200 border-0 text-xs" rows={5} />
+
+          <div className="flex lg:flex-row flex-col gap-2 w-full mt-4">
+            <button type="button" className="flex items-center justify-center h-10 lg:w-20 w-full bg-green-500 text-white rounded-md hover:bg-green-600 active:bg-green-700 duration-150 disabled:bg-green-200 disabled:cursor-default">
+              {'Validasi'}
+            </button>
+
+            <button type="submit" className="flex items-center justify-center h-10 lg:w-24 w-full bg-red-600 text-white rounded-md hover:bg-red-700 active:bg-red-800 duration-150 disabled:bg-red-200 disabled:cursor-default">
+              {'Perlu direvisi'}
+            </button>
+          </div>
+        </form>
 
         {openModal && <ModalInternshipDocs id={data} isOpen={openModal} closeModal={() => setOpenModal(false)} modalType={modalType} />}
       </div>
@@ -70,7 +134,7 @@ const DataLogbook = ({ data, loading }) => {
 };
 
 DataLogbook.propTypes = {
-  data: PropTypes.array,
+  data: PropTypes.any,
   loading: PropTypes.bool,
 };
 
