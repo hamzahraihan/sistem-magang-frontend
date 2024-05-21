182ff1ff3f0560a105d7f989c98d32011b4ac4e8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-21 14:33:40 +0700

    refactor: Add FormInternshipValidation component for internship weekly report validation

---
+++ src/pages/Mahasiswa/Dosen/MahasiswaInternshipReport/InternshipWeeklyReport/FormInternshipValidation.jsx
@@ -0,0 +1,36 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const FormInternshipValidation = () => {
+  const formik = useFormik({
+    initialValues: {
+      lecturer_note: '',
+      status: '',
+    },
+    onSubmit: () => {},
+    validationSchema: yup.object().shape({
+      lecturer_note: yup.string().required('Wajib diisi bila perlu direvisi'),
+      status: yup.string().required(),
+    }),
+  });
+  return (
+    <form className="flex flex-col col-span-2" onSubmit={formik.handleSubmit}>
+      <label htmlFor="lecturer_note" className="text-sm text-gray-400">
+        Catatan <span className="text-red-600">*Wajib diisi bila dokumentasi magang tidak valid</span>
+      </label>
+      <textarea id="lecturer_note" name="lecturer_note" className="rounded-lg bg-gray-200 border-0 text-xs" rows={5} />
+
+      <div className="flex lg:flex-row flex-col gap-2 w-full mt-4">
+        <button type="button" className="flex items-center justify-center h-10 lg:w-20 w-full bg-green-500 text-white rounded-md hover:bg-green-600 active:bg-green-700 duration-150 disabled:bg-green-200 disabled:cursor-default">
+          {'Validasi'}
+        </button>
+
+        <button type="submit" className="flex items-center justify-center h-10 lg:w-24 w-full bg-red-600 text-white rounded-md hover:bg-red-700 active:bg-red-800 duration-150 disabled:bg-red-200 disabled:cursor-default">
+          {'Perlu direvisi'}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default FormInternshipValidation;
