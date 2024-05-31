aa8265116eb2450ce06d7a72aa67f40ea6a65514
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-05-31 12:39:35 +0700

    feat: Add InternshipWeeklyReport component for displaying weekly internship report

---
+++ src/pages/Dosen/Mahasiswa/MahasiswaInternshipReport/InternshipWeeklyReport/index.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import { ArrowIcon } from '../../../../../components/Icons';
+import DataLogbook from './DataInternship';
+import WeeklyReport from './WeeklyReport';
+import useFetchInternship from '../../../../../features/internship/useFetchInternship';
+
+const InternshipWeeklyReport = () => {
+  const { loading, internship } = useFetchInternship();
+  console.log('🚀 ~ InternshipWeeklyReport ~ internship:', internship);
+
+  return (
+    <div className="flex flex-col col-span-3 gap-4">
+      <Link
+        to={`/dashboard/dosen/mahasiswa/logbook-mahasiswa/${internship.mahasiswa_id}`}
+        className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white"
+      >
+        <ArrowIcon />
+      </Link>
+      <DataLogbook data={internship} loading={loading} />
+      <WeeklyReport />
+    </div>
+  );
+};
+
+export default InternshipWeeklyReport;
