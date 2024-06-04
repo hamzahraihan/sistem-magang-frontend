eb96a7fb9cc0da9fd97dafd701fabe4ee96a20b2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-04 13:46:43 +0700

    refactor: Add DosenProvider to Admin dashboard for managing dosen data

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import MahasiswaProvider from '../../context/MahasiswaContext';
+import DosenProvider from '../../context/DosenContext';
 
 const DashboardAdmin = () => {
   return (
     <div className="col-span-3 pb-10">
       <MahasiswaProvider>
-        <Outlet />
+        <DosenProvider>
+          <Outlet />
+        </DosenProvider>
       </MahasiswaProvider>
     </div>
   );
