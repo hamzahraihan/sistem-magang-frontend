49982e9686a5adf6c96e56a623a02ccc50496e4d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-05 12:52:00 +0700

    refactor: Update useFetchMahasiswaByDosen to include dosen_id parameter

--- src/features/user/useFetchMahasiswaByDosen.jsx
+++ src/features/user/useFetchMahasiswaByDosen.jsx
@@ -2,18 +2,19 @@ import { useEffect, useState } from 'react';
 import { useUserContext } from '../../hooks/useUserContext';
 import { getMahasiswaByDosenAPI } from '../../constant/api';
 import { TOKEN } from '../../constant/key';
+import { useParams } from 'react-router-dom';
 
 const useFetchMahasiswaByDosen = () => {
   const [mahasiswaDosen, setMahasiswaDosen] = useState([]);
   const [loading, setLoading] = useState(false);
   const { userLoggedInData } = useUserContext();
-
+  const { dosen_id } = useParams();
   useEffect(() => {
     const token = localStorage.getItem(TOKEN);
     const getMahasiswaByDosen = async () => {
       setLoading(true);
       try {
-        const data = await getMahasiswaByDosenAPI(userLoggedInData?.id, token);
+        const data = await getMahasiswaByDosenAPI(dosen_id ?? userLoggedInData?.id, token);
         setMahasiswaDosen(data);
         setLoading(false);
       } catch (error) {
@@ -21,7 +22,7 @@ const useFetchMahasiswaByDosen = () => {
       }
     };
     getMahasiswaByDosen();
-  }, [userLoggedInData?.id]);
+  }, [userLoggedInData, dosen_id]);
   return { mahasiswaDosen, loading };
 };
 
