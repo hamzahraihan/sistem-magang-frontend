c5e566a683189600bed6dda340af8c619f8423ce
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-05 12:51:52 +0700

    refactor: Add loading state to handleCreateDosenAccount function

--- src/context/DosenContext.jsx
+++ src/context/DosenContext.jsx
@@ -1,31 +1,50 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { useUserContext } from '../hooks/useUserContext';
+import toast from 'react-hot-toast';
 
 export const DosenContext = createContext(null);
 export const DosenDispatch = createContext(null);
 
 const DosenProvider = ({ children }) => {
   const [dosen, dispatch] = useReducer(DosenReducer, []);
+  const [loading, setLoading] = useState(false);
   const { accessToken } = useUserContext();
 
-  const handleCreateDosenAccount = async (values) => {
+  const handleCreateDosenAccount = async ({ first_name, last_name, email, nidn, password, gender, phone }) => {
+    setLoading(true);
     try {
-      const { data } = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/dosen`, values, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
+      await axios.post(
+        `${import.meta.env.VITE_BASE_URL}/users/dosen`,
+        {
+          first_name,
+          last_name,
+          email,
+          role: 'dosen',
+          nidn,
+          password,
+          gender,
+          phone,
         },
-      });
-      dispatch({ type: 'ADD_DOSEN', payload: data.result });
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      toast.success('Akun berhasil ditambahkan');
+      setLoading(false);
     } catch (error) {
+      toast.error(error);
       console.log(error);
+      setLoading(false);
     }
   };
 
   return (
-    <DosenContext.Provider value={{ dosen, handleCreateDosenAccount }}>
+    <DosenContext.Provider value={{ dosen, handleCreateDosenAccount, loading }}>
       <DosenDispatch.Provider value={dispatch}>{children}</DosenDispatch.Provider>
     </DosenContext.Provider>
   );
