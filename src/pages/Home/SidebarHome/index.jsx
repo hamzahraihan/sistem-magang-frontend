0f7fef1498b5b5865c0743344f215d10917cd48f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-06 14:49:01 +0700

    feat: Add CategoryProvider to manage category context

--- src/pages/Home/SidebarHome/index.jsx
+++ src/pages/Home/SidebarHome/index.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { usePostContext } from '../../../hooks/usePostContext';
 import { Link } from 'react-router-dom';
 import CatergoryMobile from '../../../components/CatergoryMobile';
+import CategoryProvider from '../../../context/CategoryContext';
 
 const SidebarHome = () => {
   const [openCategory, setOpenCategory] = useState(false);
@@ -21,28 +22,30 @@ const SidebarHome = () => {
   };
 
   return (
-    <div className="lg:col-span-1 col-span-3 flex flex-col items-start h-full lg:order-last order-first w-full">
-      <form className="flex lg:flex-col items-center gap-4 w-full">
-        <Searchbar />
-        <div className="flex items-center relative lg:hidden">
-          <button type="button" className="cursor-pointer text-gray-500" onClick={() => handleMobileCategory()}>
-            <AdjustIcon />
-          </button>
-          {openCategory && <CatergoryMobile setSearchParams={setSearchParams} />}
-        </div>
-        <div className="lg:block hidden w-full">
-          <CatagoriesSelect />
-        </div>
-        <div className="text-gray-500 lg:hidden">
-          <Link to="/create-post">
-            <PlusIcon />
-          </Link>
-        </div>
-        <div className="lg:block w-full hidden">
-          <PrimaryButton text="Buat Post" />
-        </div>
-      </form>
-    </div>
+    <CategoryProvider>
+      <div className="lg:col-span-1 col-span-3 flex flex-col items-start h-full lg:order-last order-first w-full">
+        <form className="flex lg:flex-col items-center gap-4 w-full">
+          <Searchbar />
+          <div className="flex items-center relative lg:hidden">
+            <button type="button" className="cursor-pointer text-gray-500" onClick={() => handleMobileCategory()}>
+              <AdjustIcon />
+            </button>
+            {openCategory && <CatergoryMobile setSearchParams={setSearchParams} />}
+          </div>
+          <div className="lg:block hidden w-full">
+            <CatagoriesSelect />
+          </div>
+          <div className="text-gray-500 lg:hidden">
+            <Link to="/create-post">
+              <PlusIcon />
+            </Link>
+          </div>
+          <div className="lg:block w-full hidden">
+            <PrimaryButton text="Buat Post" />
+          </div>
+        </form>
+      </div>
+    </CategoryProvider>
   );
 };
 
