7c1a550b5935e3e9d701ba5238ceea67e36fad10
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-08 14:41:56 +0700

    refactor: Add CategoryProvider and update Admin index.jsx to use it

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import MahasiswaProvider from '../../context/MahasiswaContext';
 import DosenProvider from '../../context/DosenContext';
+import CategoryProvider from '../../context/CategoryContext';
 
 const DashboardAdmin = () => {
   return (
     <div className="col-span-3 pb-10">
       <MahasiswaProvider>
         <DosenProvider>
-          <Outlet />
+          <CategoryProvider>
+            <Outlet />
+          </CategoryProvider>
         </DosenProvider>
       </MahasiswaProvider>
     </div>
