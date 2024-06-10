411e019deb6223f5f4ee9c26e94cfa99545e7de8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-10 10:51:03 +0700

    refactor: Update CatergoryMobile component to use useFetchCategory hook and improve category button rendering

--- src/components/CatergoryMobile.jsx
+++ src/components/CatergoryMobile.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
+import useFetchCategory from '../features/category/useFetchCategory';
 
-const CatergoryMobile = ({ setSearchParams }) => {
+const CatergoryMobile = ({ setOpenCategory, setSearchParams }) => {
+  const { category } = useFetchCategory();
   return (
     <div className="absolute top-7 -right-5 rounded-lg bg-white shadow-md">
       <div className="flex flex-col p-2 text-sm whitespace-nowrap">
@@ -9,54 +11,33 @@ const CatergoryMobile = ({ setSearchParams }) => {
           type="button"
           className="p-2 hover:bg-gray-200 active:bg-gray-100 rounded-md transition-all"
           value=""
-          onClick={(e) =>
+          onClick={(e) => {
+            setOpenCategory(false);
             setSearchParams((prev) => {
               prev.set('category_name', e.target.value);
               return prev;
-            })
-          }
+            });
+          }}
         >
           Semua
         </button>
-        <button
-          type="button"
-          className="p-2 hover:bg-gray-200 active:bg-gray-100 rounded-md transition-all"
-          value="bertanya"
-          onClick={(e) =>
-            setSearchParams((prev) => {
-              prev.set('category_name', e.target.value);
-              return prev;
-            })
-          }
-        >
-          Bertanya
-        </button>
-        <button
-          type="button"
-          className="p-2 hover:bg-gray-200 active:bg-gray-100 rounded-md transition-all"
-          value="magang"
-          onClick={(e) =>
-            setSearchParams((prev) => {
-              prev.set('category_name', e.target.value);
-              return prev;
-            })
-          }
-        >
-          Magang
-        </button>
-        <button
-          type="button"
-          className="p-2 hover:bg-gray-200 active:bg-gray-100 rounded-md transition-all"
-          value="kendala"
-          onClick={(e) =>
-            setSearchParams((prev) => {
-              prev.set('category_name', e.target.value);
-              return prev;
-            })
-          }
-        >
-          Kendala
-        </button>
+        {category.map((item) => (
+          <button
+            key={item.category}
+            type="button"
+            className="p-2 hover:bg-gray-200 active:bg-gray-100 rounded-md transition-all"
+            value={item.category}
+            onClick={(e) => {
+              setOpenCategory(false);
+              setSearchParams((prev) => {
+                prev.set('category_name', e.target.value);
+                return prev;
+              });
+            }}
+          >
+            {item.category}
+          </button>
+        ))}
       </div>
     </div>
   );
@@ -64,6 +45,7 @@ const CatergoryMobile = ({ setSearchParams }) => {
 
 CatergoryMobile.propTypes = {
   setSearchParams: PropTypes.func,
+  setOpenCategory: PropTypes.func,
 };
 
 export default CatergoryMobile;
