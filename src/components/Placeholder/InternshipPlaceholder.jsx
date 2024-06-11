ec6589d97511a92d925a669bf69cb5b6d6c02eb4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-11 13:42:16 +0700

    refactor: Optimize InternshipPlaceholder component rendering

--- src/components/Placeholder/InternshipPlaceholder.jsx
+++ src/components/Placeholder/InternshipPlaceholder.jsx
@@ -1,47 +1,20 @@
 const InternshipPlaceholder = () => {
+  const items = Array.from({ length: 5 });
   return (
-    <>
-      <div className="flex border items-center border-gray-200 rounded-xl p-4 transition-all">
-        <div className="flex flex-1 gap-1 flex-col">
-          <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
-          <p className="bg-gray-600 animate-pulse h-3 w-96 rounded-lg"></p>
-          <div className="flex gap-2">
-            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
-            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+    <div className="flex flex-col gap-2 min-w-full">
+      {items.map((_, index) => (
+        <div key={index} className="flex border items-center border-gray-200 rounded-xl p-4 transition-all bg-white">
+          <div className="flex flex-1 gap-1 flex-col">
+            <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
+            <p className="bg-gray-600 animate-pulse h-3 w-4/5 rounded-lg"></p>
+            <div className="flex gap-2">
+              <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+              <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex border items-center border-gray-200 rounded-xl p-4 transition-all">
-        <div className="flex flex-1 gap-1 flex-col">
-          <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
-          <p className="bg-gray-600 animate-pulse h-3 w-96 rounded-lg"></p>
-          <div className="flex gap-2">
-            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
-            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
-          </div>
-        </div>
-      </div>
-      <div className="flex border items-center border-gray-200 rounded-xl p-4 transition-all">
-        <div className="flex flex-1 gap-1 flex-col">
-          <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
-          <p className="bg-gray-600 animate-pulse h-3 w-96 rounded-lg"></p>
-          <div className="flex gap-2">
-            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
-            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
-          </div>
-        </div>
-      </div>
-      <div className="flex border items-center border-gray-200 rounded-xl p-4 transition-all">
-        <div className="flex flex-1 gap-1 flex-col">
-          <p className="bg-gray-600 animate-pulse h-3 w-40 rounded-lg"></p>
-          <p className="bg-gray-600 animate-pulse h-3 w-96 rounded-lg"></p>
-          <div className="flex gap-2">
-            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
-            <p className="bg-gray-600 animate-pulse h-3 w-full rounded-lg"></p>
-          </div>
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 };
 
