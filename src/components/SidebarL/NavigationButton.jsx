da4cbc8e0d1101803151ae4e53a729d70881fe09
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-23 22:31:01 +0700

    refactor: Update SidebarL NavigationButton component to include GuideIcon for displaying a guide icon

--- src/components/SidebarL/NavigationButton.jsx
+++ src/components/SidebarL/NavigationButton.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import { DocumentIcon, HomeIcon, LecturerIcon, NewsIcon, OfficeBrifcase, UploadIcon, UserIcon } from '../Icons';
+import { DocumentIcon, GuideIcon, HomeIcon, LecturerIcon, NewsIcon, OfficeBrifcase, UploadIcon, UserIcon } from '../Icons';
 import { useUserContext } from '../../hooks/useUserContext';
 
 const activeNavButton = 'flex items-center gap-2 p-4 bg-primaryColor active:bg-activeColor hover:bg-hoverColor text-white rounded-[24px] transition-all ease-in-out duration-150 cursor-pointer lg:w-full md:w-full w-fit';
@@ -20,9 +20,18 @@ const Navigation = () => {
           <UploadIcon />
           <p className="lg:flex hidden">Upload Laporan</p>
         </NavLink>
-        <NavLink to="https://drive.google.com/drive/u/1/folders/13_CQ53O0eIKVCQsuSVlHAqvMFFQD7m6-" className={({ isActive }) => (isActive ? activeNavButton : notActiveNavButton)}>
+        <NavLink to="https://drive.google.com/drive/u/1/folders/13_CQ53O0eIKVCQsuSVlHAqvMFFQD7m6-" target="_blank" className={({ isActive }) => (isActive ? activeNavButton : notActiveNavButton)} rel="noopener noreferrer">
           <DocumentIcon />
           <p className="lg:flex hidden">Berkas Magang</p>
+        </NavLink>
+        <NavLink
+          to="https://docs.google.com/presentation/d/1vZLuBXoQ84byioTcmqQkeYT6NGPn47R18V_48OF6g9A/edit?usp=sharing"
+          target="_blank"
+          className={({ isActive }) => (isActive ? activeNavButton : notActiveNavButton)}
+          rel="noopener noreferrer"
+        >
+          <GuideIcon />
+          <p className="lg:flex hidden">Panduan Magang</p>
         </NavLink>
       </>
     );
