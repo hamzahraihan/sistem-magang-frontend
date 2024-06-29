9d6aa910bc5cbe89bbde467e32e87c96872d516f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-29 12:21:20 +0700

    feat: Add SidebarInternshipRequirements component for displaying internship requirements sidebar

---
+++ src/pages/Internship/InternshipRequirements/SidebarInternshipRequirements/index.jsx
@@ -0,0 +1,12 @@
+const SidebarInternshipRequirements = () => {
+  return (
+    <div className="lg:flex lg:col-span-1 col-span-3 flex-col top-5 lg:items-start hidden">
+      <div className="flex flex-col w-full gap-5">
+        <h1 className="text-xl font-bold">Berkas Magang</h1>
+        <p className="text-neutral-500">Mahasiswa dapat mengunduh berbagai dokumen yang diperlukan untuk magang.</p>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarInternshipRequirements;
