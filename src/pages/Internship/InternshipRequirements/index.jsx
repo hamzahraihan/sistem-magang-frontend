943a62136113d29f18e4b6fe0675f9a01af5ddd4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-29 12:20:32 +0700

    feat: Add InternshipRequirements page
    The code changes include adding a new page for internship requirements. This will allow users to view the requirements for internships.

---
+++ src/pages/Internship/InternshipRequirements/index.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const InternshipRequirements = () => {
+  return (
+    <div className="col-span-3 pb-10">
+      <Outlet />
+    </div>
+  );
+};
+
+export default InternshipRequirements;
