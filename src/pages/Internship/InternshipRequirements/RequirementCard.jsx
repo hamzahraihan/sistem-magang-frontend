437e0c0c4946e7bd46454cd2e085c0a23fda3c95
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-06-29 12:20:47 +0700

    feat: Add RequirementCard component for displaying internship requirements
    The code changes include adding a new component called RequirementCard. This component is responsible for displaying the requirements for internships. It accepts props for the vector image, title, and link. This component will be used in the InternshipRequirements page to render each requirement card.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature and providing a clear and concise description of the changes made.

---
+++ src/pages/Internship/InternshipRequirements/RequirementCard.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const RequirementCard = ({ vector, title, link }) => {
+  return (
+    <Link to={link} className="group flex flex-col gap-2 border border-gray-300 p-3 rounded-xl h-full w-full hover:bg-primaryColor active:bg-activeColor  hover:border-white duration-150 bg-white relative">
+      <img className="lg:group-hover:blur-sm duration-100" src={vector} alt="form-vector" />
+      <div className="lg:absolute lg:block inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 duration-150 rounded-xl hidden">
+        <div className="flex justify-center items-center h-full ">
+          <p className="text-white font-bold text-sm text-center">{title}</p>
+        </div>
+      </div>
+      <div className="lg:hidden text-center group-hover:text-white duration-100 font-bold z-30">{title}</div>
+    </Link>
+  );
+};
+
+RequirementCard.propTypes = {
+  vector: PropTypes.string,
+  title: PropTypes.string,
+  link: PropTypes.string,
+};
+
+export default RequirementCard;
