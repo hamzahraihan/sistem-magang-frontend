40177211619181d13252eaa955151d81c53c2d22
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-01 20:42:37 +0700

    feat: Add useFetchRequestInternship hook for fetching internship requests
    The code changes involve adding a new hook called useFetchRequestInternship. This hook is responsible for fetching the internship requests from the API and managing the loading state. It is used in the Internship component to retrieve the internship requests and display them to the user. This enhancement improves the functionality of the application by providing a reusable and efficient way to fetch and manage internship requests.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature and providing a clear and concise description of the changes made.

---
+++ src/features/internship/useFetchRequestIntenship.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { getRequestInternshipAPI } from '../../constant/api';
+import toast from 'react-hot-toast';
+import { useUserContext } from '../../hooks/useUserContext';
+
+const useFetchRequestInternship = () => {
+  const [loading, setLoading] = useState(false);
+  const [requestInternship, setRequestInternship] = useState([]);
+
+  const { userLoggedInData } = useUserContext();
+
+  useEffect(() => {
+    const getAllLetterInternship = async () => {
+      setLoading(true);
+      try {
+        const data = await getRequestInternshipAPI();
+        setRequestInternship(data);
+        setLoading(false);
+      } catch (error) {
+        if (error.response.status === 403) {
+          toast.error('Anda tidak mempunyai hak akses. Pastikan Anda sudah login dengan akun dan role yang benar.');
+        }
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    getAllLetterInternship();
+  }, [userLoggedInData]);
+
+  return { requestInternship, loading };
+};
+
+export default useFetchRequestInternship;
