5b7a0f8cebb236c944f20d94a701454a823b52c0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-01 20:43:53 +0700

    feat: Add lazy loading for images in RequirementMenu
    The code changes involve adding lazy loading functionality for images in the RequirementMenu component. This enhancement improves the page load performance by loading images only when they are visible on the screen. The lazy loading technique helps reduce the initial load time and improves the overall user experience.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature and providing a clear and concise description of the changes made.

--- src/pages/Internship/InternshipRequirements/RequirementMenu.jsx
+++ src/pages/Internship/InternshipRequirements/RequirementMenu.jsx
@@ -1,13 +1,66 @@
 import { Link } from 'react-router-dom';
-import { ArrowIcon } from '../../../components/Icons';
+import { ArrowIcon, DownloadIcon } from '../../../components/Icons';
 import SidebarInternshipRequirements from './SidebarInternshipRequirements';
 import FormsVector from '../../../assets/svg/forms.svg';
 import DownloadVector from '../../../assets/svg/filesdownload.svg';
 import RequirementCard from './RequirementCard';
+import Table from '../../../components/Table/Table';
+import { createColumnHelper } from '@tanstack/react-table';
+import { weekDay } from '../../../utils/formatDate';
+import useFetchRequestInternshipByMahasiswa from '../../../features/internship/useFetchRequestInternshipByMahasiswa';
 
 const RequirementMenu = () => {
+  const { requestInternship, loading } = useFetchRequestInternshipByMahasiswa();
+
+  const columnHelper = createColumnHelper();
+
+  const columns = [
+    columnHelper.accessor('', {
+      id: 'No.',
+      cell: (info) => info.row.index + 1,
+      header: () => <span className="ps-2">No.</span>,
+      size: 20,
+    }),
+    columnHelper.accessor('instance', {
+      id: 'Perusahaan / Instansi',
+      cell: (info) => info.getValue(),
+      header: 'Perusahaan / Instansi',
+    }),
+    columnHelper.accessor('type_internship', {
+      id: 'Tipe Magang',
+      cell: (info) => <span>Magang {info.getValue()}</span>,
+      header: 'Tipe Magang',
+    }),
+    columnHelper.accessor('instance_contact', {
+      id: 'Kontak',
+      cell: (info) => info.getValue(),
+      header: 'Kontak',
+    }),
+    columnHelper.accessor('internship_letter_file', {
+      id: 'Surat Magang',
+      cell: (info) => {
+        if (info.getValue()) {
+          return (
+            <Link to={`https://drive.google.com/file/d/${info.getValue()}/view`} target="_blank" rel="noopener noreferrer">
+              <div className="w-fit p-2 rounded-md bg-yellow-300 hover:bg-yellow-400 duration-150">
+                <DownloadIcon />
+              </div>
+            </Link>
+          );
+        }
+        return <div className="p-2 rounded-md w-fit bg-gray-300">Menunggu</div>;
+      },
+      header: 'Surat Magang',
+    }),
+    columnHelper.accessor('createdAt', {
+      id: 'Tanggal',
+      cell: (info) => <span>{weekDay(info.getValue())}</span>,
+      header: 'Tanggal',
+    }),
+  ];
+
   return (
-    <div className="grid grid-cols-3 gap-5">
+    <div className="grid grid-cols-3 gap-5 min-w-full ">
       <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
         <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
           <ArrowIcon />
@@ -18,7 +71,9 @@ const RequirementMenu = () => {
           <RequirementCard vector={DownloadVector} link="https://drive.google.com/drive/u/1/folders/13_CQ53O0eIKVCQsuSVlHAqvMFFQD7m6-" title="Unduh Berkas Magang" />
         </div>
         <h1 className="font-bold text-xl">Surat Permohonan Magang</h1>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita, sint ea perspiciatis commodi porro ratione dolor nam error placeat!
+        <div className="overflow-auto">
+          <Table columns={columns} data={requestInternship} loading={loading} fileName="laporan_surat_permohonan_magang" />
+        </div>
       </div>
       <SidebarInternshipRequirements />
     </div>
