717fcd4fdc50bfad81d7040e6871743bd659db45
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-01 20:42:43 +0700

    feat: Add useFetchRequestInternshipById hook for fetching internship requests by ID

---
+++ src/features/internship/useFetchRequestInternshipById.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { getRequestInternshipByIdAPI } from '../../constant/api';
+import toast from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
+import { TOKEN } from '../../constant/key';
+
+const useFetchRequestInternshipById = () => {
+  const [loading, setLoading] = useState(false);
+  const [requestInternship, setRequestInternship] = useState([]);
+
+  const { letter_id } = useParams();
+
+  useEffect(() => {
+    const getLetterById = async () => {
+      const token = localStorage.getItem(TOKEN);
+      setLoading(true);
+      try {
+        const data = await getRequestInternshipByIdAPI(letter_id, token);
+        setRequestInternship(data);
+        setLoading(false);
+      } catch (error) {
+        if (error.response.status === 403) {
+          toast.error('Anda tidak mempunyai hak akses. Pastikan Anda sudah login dengan akun dan role yang benar.');
+        }
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    getLetterById();
+  }, [letter_id]);
+
+  return { requestInternship, loading };
+};
+
+export default useFetchRequestInternshipById;
