658a7949938caf6e4be32edf6138e7d05d929074
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-06 12:00:57 +0700

    feat: Improve InternshipCard layout and add status indicator
    Refactor InternshipCard component in InternshipCard.jsx to improve the layout and add a status indicator for each internship item. The layout changes include adjusting the positioning of the internship details and adding a container for the status indicator. The status indicator is dynamically styled based on the internship status, with different background colors and text colors for different statuses. This enhancement improves the visual presentation of the InternshipCard and provides clear visibility of the internship status.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature and providing a clear and concise description of the changes made.

--- src/pages/Internship/InternshipCard.jsx
+++ src/pages/Internship/InternshipCard.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import InternshipPlaceholder from '../../components/Placeholder/InternshipPlaceholder';
 import useFetchInternship from '../../features/internship/useFetchInternship';
-import { ArrowIcon } from '../../components/Icons';
 import { formatDate } from '../../utils/formatDate';
 import _ from 'lodash';
 
 const InternshipCard = () => {
   const { loading, internship } = useFetchInternship();
   console.log('🚀 ~ InternshipCard ~ internship:', internship);
+
   return (
     <>
       {loading ? (
@@ -20,15 +20,17 @@ const InternshipCard = () => {
             className="flex border items-center border-gray-200 rounded-xl p-4 hover:bg-primaryColor active:bg-activeColor hover:text-white hover:border-white transition-all bg-white"
             key={item.internship_id}
           >
-            <div className="flex flex-1 flex-col">
-              <p className="text-bold">Magang Mandiri</p>
-              <p className="font-bold text-base">{_.capitalize(item.instance)}</p>
-              <p>
-                Periode Magang: {formatDate(item.start_intern)} - {formatDate(item.end_intern)}
-              </p>
-            </div>
-            <div>
-              <ArrowIcon />
+            <div className="flex justify-evenly w-full">
+              <div className="flex-1">
+                <p className="text-bold">Magang Mandiri</p>
+                <p className="font-bold text-base">{_.capitalize(item.instance)}</p>
+                <div className="flex flex-row justify-between">
+                  <p>
+                    Periode Magang: {formatDate(item.start_intern)} - {formatDate(item.end_intern)}
+                  </p>
+                </div>
+              </div>
+              <div className={`mt-auto p-2 h-fit rounded-md text-black ${item.status == 'Belum diterima' ? 'bg-gray-300' : 'bg-green-400 text-white'}`}>{item.status}</div>
             </div>
           </Link>
         ))
