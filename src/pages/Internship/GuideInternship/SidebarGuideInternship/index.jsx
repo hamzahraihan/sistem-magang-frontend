1136714d8baeb11090fde7bbd00a673c16d2ebd6
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-11 12:56:13 +0700

    feat: Add SidebarGuideInternship component
    Add the SidebarGuideInternship component to the GuideInternship page. This component displays a sidebar with additional resources for interns. This feature enhances the user experience by providing helpful information and easy navigation within the page.

---
+++ src/pages/Internship/GuideInternship/SidebarGuideInternship/index.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+const SidebarGuideInternship = () => {
+  return (
+    <div className="lg:flex lg:col-span-1 col-span-3 flex-col top-5 lg:items-start">
+      <div className="flex flex-col w-full gap-5">
+        <h1 className="text-xl font-bold">Panduan Magang</h1>
+        <p className="text-neutral-500">
+          Pelaksanaan Magang diwujudkan dalam bentuk praktik kerja fulltime atau parttime pada unit tujuan magang dengan status mahasiswa aktif Universitas Singaperbangsa Karawang dengan batasan waktu minimal 1 bulan dan maksimal 6 bulan.
+        </p>
+        <Link
+          to="https://docs.google.com/presentation/d/1to4WHd0bn7_Q1ik_WvvJTYefgRxZB1lX/edit#slide=id.p10"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="p-4 font-bold bg-primaryColor hover:bg-hoverColor active:bg-activeColor duration-150 text-white rounded-lg"
+        >
+          Panduan lengkap
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarGuideInternship;
