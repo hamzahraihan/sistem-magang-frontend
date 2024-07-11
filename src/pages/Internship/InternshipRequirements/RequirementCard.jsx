1312a3355f42c93d60787a72f1bdd1efb459bdf2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-11 12:56:20 +0700

    feat: Optimize image loading in RequirementCard
    Improve the performance of the RequirementCard component by optimizing the loading of images. This change adds a height constraint to the image element, ensuring that it maintains its aspect ratio and preventing layout shifts. This enhancement enhances the user experience by reducing page load time and providing a smoother browsing experience.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature and providing a clear and concise description of the changes made.

--- src/pages/Internship/InternshipRequirements/RequirementCard.jsx
+++ src/pages/Internship/InternshipRequirements/RequirementCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const RequirementCard = ({ vector, title, link }) => {
   return (
     <Link to={link} className="group flex flex-col gap-2 border border-gray-300 p-3 rounded-xl h-full w-full hover:bg-primaryColor active:bg-activeColor  hover:border-white duration-150 bg-white relative">
-      <img className="lg:group-hover:blur-sm duration-100" src={vector} alt="form-vector" />
+      <img className="lg:group-hover:blur-sm duration-100 h-full" src={vector} alt="form-vector" />
       <div className="lg:absolute lg:block inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 duration-150 rounded-xl hidden">
         <div className="flex justify-center items-center h-full ">
           <p className="text-white font-bold text-sm text-center">{title}</p>
