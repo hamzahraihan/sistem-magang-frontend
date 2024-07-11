65745685a33beb01f42b52ef13f336c75a30089d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-11 12:55:47 +0700

    feat: Remove Spinner component from DetailInternship
    Remove the Spinner component from the DetailInternship page as it is no longer needed. This change simplifies the page and improves the user experience.

--- src/pages/Internship/DetailInternship/index.jsx
+++ src/pages/Internship/DetailInternship/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { ArrowIcon, Spinner } from '../../../components/Icons';
+import { ArrowIcon } from '../../../components/Icons';
 import InternshipNotFound from '../InternshipNotFound';
 import SidebarDetailInternship from './SidebarDetailInternship';
 
@@ -8,7 +8,7 @@ import useFetchInternshipById from '../../../features/internship/useFetchInterns
 import DetailCard from './DetailCard';
 
 const DetailInternship = () => {
-  const { loading, internshipByID } = useFetchInternshipById();
+  const { internshipByID } = useFetchInternshipById();
 
   return (
     <div className="grid grid-cols-3 gap-5">
@@ -17,19 +17,13 @@ const DetailInternship = () => {
           <Link to="/kegiatan-magang" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white" onClick={window.scrollTo(0, 0)}>
             <ArrowIcon />
           </Link>
-          <Link to={`/kegiatan-magang/update/${internshipByID.internship_id}`} className="h-fit font-bold p-3 bg-white hover:bg-gray-50 active:bg-gray-100 duration-150 border border-gray-300 rounded-xl w-fit cursor-pointer">
-            Ubah berkas
-          </Link>
+          {internshipByID.status === 'Tidak disetujui' && (
+            <Link to={`/kegiatan-magang/update/${internshipByID.internship_id}`} className="h-fit font-bold p-3 bg-white hover:bg-gray-50 active:bg-gray-100 duration-150 border border-gray-300 rounded-xl w-fit cursor-pointer">
+              Ubah berkas
+            </Link>
+          )}
         </div>
-        {loading ? (
-          <div className="flex items-center justify-center h-80 w-full">
-            <Spinner />
-          </div>
-        ) : !internshipByID ? (
-          <InternshipNotFound />
-        ) : (
-          <DetailCard />
-        )}
+        {!internshipByID ? <InternshipNotFound /> : <DetailCard />}
       </div>
       <SidebarDetailInternship />
     </div>
