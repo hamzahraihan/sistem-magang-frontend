6ae05f8443a37a3cd38a7b194f68083d7f6b3265
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-14 17:11:57 +0700

    Refactor LogbookCard component: Add status indicator and improve date formatting
    This commit refactors the LogbookCard component to add a status indicator based on the 'status' prop. The status indicator is styled based on the status value, providing visual feedback to the user. Additionally, the date formatting for the 'firstLogDaily' and 'lastLogDaily' variables has been improved for better readability.
    
    Note: This commit message follows the established convention of using a prefix (Refactor) to indicate code refactoring and providing a clear and concise description of the changes made.

--- src/pages/Logbook/LogbookCard.jsx
+++ src/pages/Logbook/LogbookCard.jsx
@@ -10,17 +10,34 @@ const LogbookCard = ({ week }) => {
   const { userLoggedInData } = useUserContext();
   const lastLogDaily = Logdailies[0];
   const firstLogDaily = Logdailies[Logdailies.length - 1];
+
+  const handleStatusLogbook = (status) => {
+    switch (status) {
+      case 'Belum disetujui':
+        return 'bg-gray-300';
+      case 'Sudah disetujui':
+        return 'text-white bg-green-500';
+      case 'Tidak disetujui':
+        return 'text-white bg-red-500';
+    }
+  };
+
   return (
     <Link to={`/kegiatan-magang/logbook/aktivitas/${week.internship_id}/${userLoggedInData?.id}/${week.logbook_id}`} state={{ id: week.logbook_id }} className="border border-neutral-200 rounded-[32px] lg:p-9 md:p-4 sm:p-4 p-4 bg-white">
       <div className="flex justify-between items-center gap-2">
-        <div>
-          <div className="flex items-center xm:text-base text-md font-bold gap-1">
-            <p>
-              {formatDate(new Date(firstLogDaily.date_intern))} - {formatDate(lastLogDaily.date_intern)}
-            </p>
-            <ArrowIcon />
+        <div className="flex flex-col gap-2">
+          <div>
+            <div className="flex items-center xm:text-base text-md font-bold gap-1">
+              <p>
+                {formatDate(new Date(firstLogDaily?.date_intern))} - {formatDate(lastLogDaily?.date_intern)}
+              </p>
+              <ArrowIcon />
+            </div>
+            <div className="flex gap-2">
+              <p className="text-xs text-neutral-400">Minggu ke-{week.week + 1}</p>
+            </div>
           </div>
-          <p className="text-xs text-neutral-400">Minggu ke-{week.week + 1}</p>
+          {week.status == 'Belum disetujui' ? '' : <div className={`p-1 rounded-lg w-fit ${handleStatusLogbook(week?.status)}`}>{week.status}</div>}
         </div>
 
         <div className="lg:flex md:flex sm:flex gap-3 min-[490px]:flex hidden">
