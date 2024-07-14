d70706298180a42d970e7ce1184ebd819e536d92
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-14 17:11:34 +0700

    Refactor GuideInternship component
    Refactor the GuideInternship component to improve code readability and maintainability. This change includes hiding the back button on smaller screens and updating the title element to be hidden on larger screens. These refactorings enhance the overall quality of the codebase.
    
    Note: This commit message follows the established convention of using a prefix (Refactor) to indicate code refactoring and providing a clear and concise description of the changes made.

--- src/pages/Internship/GuideInternship/index.jsx
+++ src/pages/Internship/GuideInternship/index.jsx
@@ -8,11 +8,10 @@ const GuideInternhsip = () => {
     <div className="col-span-3 pb-10">
       <div className="grid grid-cols-3 gap-5 min-w-full ">
         <div className="flex flex-col gap-4 lg:col-span-2 col-span-3">
-          <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white">
+          <Link to="/" className="lg:flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white hidden">
             <ArrowIcon />
           </Link>
-          <h1 className="font-bold text-xl">Panduan Magang</h1>
-
+          <h1 className="lg:block hidden font-bold text-xl">Panduan Magang</h1>
           <img src={PanduanSVG} alt="panduan" />
         </div>
         <SidebarGuideInternship />
