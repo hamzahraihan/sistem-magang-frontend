ea2e9652a8bf0129aa14b26b31ee615bf9839e1c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-14 17:11:16 +0700

    Refactor FormCreateInternship component
    Remove unnecessary 'dosen_id' field and associated code in the FormCreateInternship component. This change simplifies the code and improves maintainability.
    
    Note: This commit message follows the established convention of using a prefix (Refactor) to indicate code refactoring and providing a clear and concise description of the changes made.

--- src/pages/Internship/CreateInternship/FormCreateInternship.jsx
+++ src/pages/Internship/CreateInternship/FormCreateInternship.jsx
@@ -1,28 +1,11 @@
 import { useInternshipContext } from '../../../hooks/useInternshipContext';
 import PropTypes from 'prop-types';
-import { useUserContext } from '../../../hooks/useUserContext';
 
 const FormCreateInternship = ({ formik, allowedExt }) => {
   const { internFileInputRef, campusFileInputRef } = useInternshipContext();
-  const { dosenData } = useUserContext();
-
-  const handleInputValue = (event) => {
-    const { target } = event;
-    formik.setFieldValue(target.name, target.value);
-  };
 
   return (
     <>
-      <label htmlFor="dosen_id">Dosen Wali</label>
-      <select id="dosen_id" name="dosen_id" className="text-sm p-3 border border-gray-200 rounded-xl " onChange={handleInputValue} required>
-        <option defaultValue={null}>Pilih dosen wali</option>
-        {dosenData.map((item) => (
-          <option key={item.dosen_id} value={item.dosen_id}>
-            {item.first_name} {item.last_name}
-          </option>
-        ))}
-      </select>
-
       <label htmlFor="campus_approval">Surat permohonan magang dari kampus</label>
       <input
         className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer  focus:outline-none "
