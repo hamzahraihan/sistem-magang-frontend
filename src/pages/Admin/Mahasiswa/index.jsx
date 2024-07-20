9d9f8b34a17c84724ac6d26e460043051f220c70
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-20 13:01:54 +0700

    feat: Add icons and statistics for Mahasiswa status
    This commit adds icons and statistics for different Mahasiswa status in the AdminMahasiswa page. It includes displaying the total number of Mahasiswa, the number of Mahasiswa who have completed their internship, the number of Mahasiswa who are currently interning, and the number of Mahasiswa who have not started their internship yet. This enhancement provides a better overview of the Mahasiswa status and improves the user experience.
    
    Note: This commit message follows the established convention of using a prefix (feat) to indicate a new feature and providing a clear and concise description of the changes made.

--- src/pages/Admin/Mahasiswa/index.jsx
+++ src/pages/Admin/Mahasiswa/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { ArrowIcon } from '../../../components/Icons';
+import { ArrowIcon, CheckIcon, LogbookIcon, UserIcon } from '../../../components/Icons';
 import Table from '../../../components/Table/Table';
 import useFetchMahasiswa from '../../../features/user/useFetchMahasiswa';
 import { createColumnHelper } from '@tanstack/react-table';
@@ -99,6 +99,42 @@ const AdminMahasiswa = () => {
         <Link to="/" className="flex items-center justify-center rotate-180 border border-neutral-300 rounded-full h-10 w-10 hover:bg-neutral-100 transition-all bg-white mb-2">
           <ArrowIcon />
         </Link>
+        <div className="grid lg:grid-cols-4 grid-cols-2 gap-2">
+          <div className="flex items-center gap-2 p-2 bg-white border border-gray-200 rounded-xl mb-3">
+            <div className="p-3 bg-primaryColor text-white rounded-full">
+              <UserIcon />
+            </div>
+            <div className="flex flex-col text-sm">
+              <p className="text-gray-400">Total Mahasiswa</p>
+              <p className="font-bold">{mahasiswa.length}</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 p-2 bg-white border border-gray-200 rounded-xl mb-3">
+            <div className="p-3 bg-green-600 text-white rounded-full">
+              <CheckIcon />
+            </div>
+            <div className="flex flex-col text-sm">
+              <p className="text-gray-400">Mahasiswa Selesai Magang</p>
+              <p className="font-bold">{mahasiswa.filter((item) => item.status == 'Selesai magang').length}</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 p-2 bg-white border border-gray-200 rounded-xl mb-3">
+            <div className="p-3 bg-yellow-300 text-white rounded-full">
+              <LogbookIcon />
+            </div>
+            <div className="flex flex-col text-sm">
+              <p className="text-gray-400">Mahasiswa Sedang Magang</p>
+              <p className="font-bold">{mahasiswa.filter((item) => item.status == 'Sedang magang').length}</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 p-2 bg-white border border-gray-200 rounded-xl mb-3">
+            <div className="flex flex-shrink-0 justify-center items-center p-3 bg-gray-400 text-white rounded-full text-xl font-bold w-12 h-12">?</div>
+            <div className="flex flex-col text-sm">
+              <p className="text-gray-400">Mahasiswa Belum Magang</p>
+              <p className="font-bold">{mahasiswa.filter((item) => item.status == 'Belum magang').length}</p>
+            </div>
+          </div>
+        </div>
         <div className="bg-white rounded-xl border border-gray-200 p-5 overflow-auto w-full">
           <div className="flex justify-between mb-2">
             <h1 className="text-xl font-bold">Daftar Mahasiswa</h1>
