f02a2f59cfd54e74c51e1eb0cf40d1d614a3887a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-07-21 13:26:40 +0700

    refactor: Update useFetchDosen to include authorization token
    This commit updates the useFetchDosen hook to include an authorization token in the API call to getAllDosen. By adding the token, the API call is now secure and restricted to authorized users.
    
    Note: This commit message follows the established convention of using a prefix (refactor) to indicate code refactoring and providing a clear and concise description of the changes made.

--- src/features/user/useFetchDosen.jsx
+++ src/features/user/useFetchDosen.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getAllDosenAPI } from '../../constant/api';
 import { useDosenContext, useDosenDispatch } from '../../hooks/useDosenContext';
+import { TOKEN } from '../../constant/key';
 
 const useFetchDosen = () => {
   const [loading, setLoading] = useState(false);
@@ -8,10 +9,11 @@ const useFetchDosen = () => {
   const dispatch = useDosenDispatch();
 
   useEffect(() => {
+    const token = localStorage.getItem(TOKEN);
     const getAllDosen = async () => {
       setLoading(true);
       try {
-        const data = await getAllDosenAPI();
+        const data = await getAllDosenAPI(token);
         dispatch({ type: 'SET_DOSEN_DATA', payload: data });
         setLoading(false);
       } catch (error) {
